ea69b889c7f70ceaa85624d91c97101858e2bc5a
Richard Davey <rich@photonstorm.com>
2021-04-27 11:57:39 +0100

    Fixed reference

--- src/math/euler/FromQuaternion.ts
+++ src/math/euler/FromQuaternion.ts
@@ -1,4 +1,4 @@
-import { FromQuat, Matrix4 } from '../mat4';
+import { Mat4FromQuat, Matrix4 } from '../mat4';
 
 import { Euler } from './Euler';
 import { FromRotationMatrix } from './FromRotationMatrix';
@@ -8,7 +8,7 @@ const tempMat4 = new Matrix4();
 
 export function FromQuaternion (e: Euler, q: Quaternion, order: string = e.order): Euler
 {
-    FromQuat(q, tempMat4);
+    Mat4FromQuat(q, tempMat4);
 
     return FromRotationMatrix(e, tempMat4, order);
 }
