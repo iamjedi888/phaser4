c3ed0b856ca5a0895445e7be43a8faedffcf4e37
Richard Davey <rich@photonstorm.com>
2021-05-06 18:03:55 +0100

    Moved UpdateVertices

---
+++ src/gameobjects/components/transform/UpdateVertices.ts
@@ -0,0 +1,16 @@
+import { GetVertices } from './GetVertices';
+import { IGameObject } from '../../IGameObject';
+
+export function UpdateVertices <T extends IGameObject> (gameObject: T): T
+{
+    const vertices = gameObject.vertices;
+
+    const { x0, y0, x1, y1, x2, y2, x3, y3 } = GetVertices(gameObject.transform);
+
+    vertices[0].setPosition(x0, y0);
+    vertices[1].setPosition(x1, y1);
+    vertices[2].setPosition(x2, y2);
+    vertices[3].setPosition(x3, y3);
+
+    return gameObject;
+}
