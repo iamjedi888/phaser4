4c749d90cd364694ef7bd05d03174d8bb9e57000
Richard Davey <rich@photonstorm.com>
2021-05-06 18:04:16 +0100

    Expose new functions

--- src/gameobjects/components/transform/index.ts
+++ src/gameobjects/components/transform/index.ts
@@ -1,11 +1,17 @@
 import { GetVertices } from './GetVertices';
+import { GetVerticesFromValues } from './GetVerticesFromValues';
+import { PreRenderVertices } from './PreRenderVertices';
 import { TransformComponent } from './TransformComponent';
 import { UpdateLocalTransform } from './UpdateLocalTransform';
+import { UpdateVertices } from './UpdateVertices';
 import { UpdateWorldTransform } from './UpdateWorldTransform';
 
 export {
     GetVertices,
+    GetVerticesFromValues,
+    PreRenderVertices,
     TransformComponent,
     UpdateLocalTransform,
-    UpdateWorldTransform
+    UpdateWorldTransform,
+    UpdateVertices
 };
