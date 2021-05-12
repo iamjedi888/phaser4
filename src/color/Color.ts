30cba3fbe0a977ad156587faf82f719f4bd1cf10
Richard Davey <rich@photonstorm.com>
2021-05-12 14:52:07 +0100

    Update Color.ts

--- src/color/Color.ts
+++ src/color/Color.ts
@@ -1,111 +1,90 @@
 import { IColor } from './IColor';
-import { RGBAToFloat } from './RGBAToFloat';
-import { RGBToFloat } from './RGBToFloat';
 
 export class Color implements IColor
 {
-    //  Using a Uint8ClampedArray means we guarantee values are in the 0-255 range
-    private _rgba: Uint8ClampedArray;
-    private _gl: Float32Array;
-    private _color: number;
-    private _color32: number;
+    rgba: Uint8ClampedArray;
 
     constructor (red: number = 0, green: number = 0, blue: number = 0, alpha: number = 1)
     {
-        this._rgba = new Uint8ClampedArray(4);
-        this._gl = new Float32Array(4);
-
-        this.set(red, green, blue, alpha);
+        //  Using a Uint8ClampedArray means we guarantee values are in the 0-255 range
+        this.rgba = new Uint8ClampedArray([ red, green, blue, alpha ]);
     }
 
-    set (red: number, green: number, blue: number, alpha?: number): this
+    set (red: number, green: number, blue: number, alpha: number = this.alpha): this
     {
-        if (alpha === undefined)
-        {
-            this._rgba.set([ red, green, blue ]);
-        }
-        else
-        {
-            this._rgba.set([ red, green, blue, alpha ]);
-        }
-
-        return this.update();
-    }
-
-    fromArray (rgba: number[]): this
-    {
-        this._rgba.set(rgba);
-
-        return this.update();
-    }
-
-    getColor (): number
-    {
-        return this._color;
-    }
-
-    getColor32 (): number
-    {
-        return this._color32;
-    }
-
-    private update (): this
-    {
-        const [ r, g, b, a ] = this._rgba;
-
-        this._gl.set([ r / 255, g / 255, b / 255, a / 255 ]);
-
-        this._color = RGBToFloat(r, g, b);
-        this._color32 = RGBAToFloat(r, g, b, a);
+        this.rgba.set([ red, green, blue, alpha ]);
 
         return this;
     }
 
+    setColor (color: number): this
+    {
+        const rgba = this.rgba;
+
+        //  The color value has an alpha component
+        const alpha = (color > 16777215) ? color >>> 24 : 255;
+
+        rgba.set([
+            color >> 16 & 0xFF,
+            color >> 8 & 0xFF,
+            color & 0xFF,
+            alpha
+        ]);
+
+        return this;
+    }
+
+    getColor (includeAlpha: boolean = false): number
+    {
+        const [ r, g, b, a ] = this.rgba;
+
+        if (includeAlpha)
+        {
+            return a << 24 | r << 16 | g << 8 | b;
+        }
+        else
+        {
+            return r << 16 | g << 8 | b;
+        }
+    }
+
     get red (): number
     {
-        return this._rgba[0];
+        return this.rgba[0];
     }
 
     set red (value: number)
     {
-        this._rgba[0] = value;
-
-        this.update();
+        this.rgba[0] = value;
     }
 
     get green (): number
     {
-        return this._rgba[1];
+        return this.rgba[1];
     }
 
     set green (value: number)
     {
-        this._rgba[1] = value;
-
-        this.update();
+        this.rgba[1] = value;
     }
 
     get blue (): number
     {
-        return this._rgba[2];
+        return this.rgba[2];
     }
 
     set blue (value: number)
     {
-        this._rgba[2] = value;
-
-        this.update();
+        this.rgba[2] = value;
     }
 
     get alpha (): number
     {
-        return this._rgba[3];
+        return this.rgba[3];
     }
 
     set alpha (value: number)
     {
-        this._rgba[3] = value;
-
-        this.update();
+        this.rgba[3] = value;
     }
 }
