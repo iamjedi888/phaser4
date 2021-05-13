dae1382751fa3d015f834dc765cbbfd6bea41c32
Richard Davey <rich@photonstorm.com>
2021-05-13 12:03:01 +0100

    Much more flexible - frames array is optional and can extract from a Texture too

--- src/textures/GetFrames.ts
+++ src/textures/GetFrames.ts
@@ -1,14 +1,29 @@
 import { Frame } from './Frame';
+import { GetTexture } from './GetTexture';
 import { ITexture } from './ITexture';
 
-export function GetFrames (texture: ITexture, frames: string[] | number[]): Frame[]
+export function GetFrames (texture: string | ITexture, frames?: string[] | number[]): Frame[]
 {
+    if (typeof(texture) === 'string')
+    {
+        texture = GetTexture(texture);
+    }
+
     const output: Frame[] = [];
 
-    frames.forEach((key: string | number) =>
+    for (const frame of texture.frames.values())
     {
-        output.push(texture.getFrame(key));
-    });
+        //  Don't include the base frame if this is a multi-frame Texture
+        if (frame.key === '__BASE' && texture.frames.size > 1)
+        {
+            continue;
+        }
+
+        if (!frames || frames.indexOf(frame.key as never) !== -1)
+        {
+            output.push(frame);
+        }
+    }
 
     return output;
 }
