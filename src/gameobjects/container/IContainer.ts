f75a66c9a6dc7c5e71ba9755dbce0ebea66f72f5
Richard Davey <rich@photonstorm.com>
2021-05-18 21:53:05 +0100

    Removed Transform properties and methods that are now in Game Object

--- src/gameobjects/container/IContainer.ts
+++ src/gameobjects/container/IContainer.ts
@@ -1,34 +1,8 @@
 import { IGameObject } from '../IGameObject';
-import { Vec2 } from '../../math/vec2/Vec2';
 
 export interface IContainer extends IGameObject
 {
-    x: number;
-    y: number;
-    width: number;
-    height: number;
-    rotation: number;
-    scaleX: number;
-    scaleY: number;
-    skewX: number;
-    skewY: number;
-    originX: number;
-    originY: number;
     alpha: number;
 
     // setAlpha (value?: number): this;
-
-    setSize (width: number, height?: number): this;
-    setPosition (x: number, y: number): this;
-    setOrigin (x: number, y?: number): this;
-    setSkew (x: number, y?: number): this;
-    setScale (x: number, y?: number): this;
-    setRotation (value: number): this;
-
-    getSize (out?: Vec2): Vec2;
-    getPosition (out?: Vec2): Vec2;
-    getOrigin (out?: Vec2): Vec2;
-    getSkew (out?: Vec2): Vec2;
-    getScale (out?: Vec2): Vec2;
-    getRotation (): number;
 }
