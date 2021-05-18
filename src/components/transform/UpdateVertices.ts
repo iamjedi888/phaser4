a5e0d0d484b30fe8c37bcecb16d6277973b0f4bd
Richard Davey <rich@photonstorm.com>
2021-05-18 22:09:13 +0100

    Take parameters directly

--- src/components/transform/UpdateVertices.ts
+++ src/components/transform/UpdateVertices.ts
@@ -1,16 +1,14 @@
 import { GetVertices } from './GetVertices';
-import { IGameObject } from '../../gameobjects/IGameObject';
+import { Matrix2D } from '../../math/mat2d/Matrix2D';
+import { Rectangle } from '../../geom/rectangle/Rectangle';
+import { Vertex } from '../Vertex';
 
-export function UpdateVertices <T extends IGameObject> (gameObject: T): T
+export function UpdateVertices (vertices: Vertex[], worldTransform: Matrix2D, transformExtent: Rectangle): void
 {
-    const vertices = gameObject.vertices;
-
-    const { x0, y0, x1, y1, x2, y2, x3, y3 } = GetVertices(gameObject.worldTransform, gameObject.transformExtent);
+    const { x0, y0, x1, y1, x2, y2, x3, y3 } = GetVertices(worldTransform, transformExtent);
 
     vertices[0].setPosition(x0, y0);
     vertices[1].setPosition(x1, y1);
     vertices[2].setPosition(x2, y2);
     vertices[3].setPosition(x3, y3);
-
-    return gameObject;
 }
