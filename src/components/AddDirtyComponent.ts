eb89f9728c1c7bf943386909d1470ff92e0d0d1c
Richard Davey <rich@photonstorm.com>
2021-05-20 12:24:32 +0100

    Create AddDirtyComponent.ts

---
+++ src/components/AddDirtyComponent.ts
@@ -0,0 +1,20 @@
+import { DirtyComponent } from './DirtyComponent';
+import { GameObjectWorld } from './GameObjectWorld';
+import { addComponent } from 'bitecs';
+
+export function AddDirtyComponent (id: number): void
+{
+    addComponent(GameObjectWorld, DirtyComponent, id);
+
+    DirtyComponent.frame[id] = 0;
+    DirtyComponent.transform[id] = 1;
+    DirtyComponent.update[id] = 1;
+    DirtyComponent.childCache[id] = 0;
+    DirtyComponent.postRender[id] = 0;
+    DirtyComponent.colors[id] = 1;
+    DirtyComponent.bounds[id] = 1;
+    DirtyComponent.texture[id] = 0;
+    DirtyComponent.textureFrame[id] = 0;
+    DirtyComponent.alpha[id] = 0;
+    DirtyComponent.child[id] = 0;
+}
