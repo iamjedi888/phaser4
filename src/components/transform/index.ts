faa040803ea28147ac021ddddaddaaa08464a9b1
Richard Davey <rich@photonstorm.com>
2021-05-20 17:47:52 +0100

    Expose new functions

--- src/components/transform/index.ts
+++ src/components/transform/index.ts
@@ -1,19 +1,33 @@
+import { AddTransform2DComponent } from './AddTransform2DComponent';
+import { Extent2DComponent } from './Extent2DComponent';
 import { GetVertices } from './GetVertices';
 import { GetVerticesFromValues } from './GetVerticesFromValues';
 import { GlobalToLocal } from './GlobalToLocal';
+import { LocalMatrix2DComponent } from './LocalMatrix2DComponent';
 import { LocalToGlobal } from './LocalToGlobal';
 import { PreRenderVertices } from './PreRenderVertices';
-import { UpdateLocalTransform } from './UpdateLocalTransform';
+import { SetExtent } from './SetExtent';
+import { Transform2DComponent } from './Transform2DComponent';
+import { UpdateExtent } from './UpdateExtent';
+import { UpdateTransform2DSystem } from './UpdateTransform2DSystem';
 import { UpdateVertices } from './UpdateVertices';
 import { UpdateWorldTransform } from './UpdateWorldTransform';
+import { WorldMatrix2DComponent } from './WorldMatrix2DComponent';
 
 export {
+    AddTransform2DComponent,
+    Extent2DComponent,
     GetVertices,
     GetVerticesFromValues,
     GlobalToLocal,
+    LocalMatrix2DComponent,
     LocalToGlobal,
     PreRenderVertices,
-    UpdateLocalTransform,
+    SetExtent,
+    Transform2DComponent,
+    UpdateExtent,
+    UpdateTransform2DSystem,
+    UpdateVertices,
     UpdateWorldTransform,
-    UpdateVertices
+    WorldMatrix2DComponent
 };
