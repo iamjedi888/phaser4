b7130c0d9066db85ad905492e86a3240d61422e5
Richard Davey <rich@photonstorm.com>
2021-05-20 12:24:35 +0100

    Create AddPermissionsComponent.ts

---
+++ src/components/AddPermissionsComponent.ts
@@ -0,0 +1,14 @@
+import { GameObjectWorld } from './GameObjectWorld';
+import { PermissionsComponent } from './PermissionsComponent';
+import { addComponent } from 'bitecs';
+
+export function AddPermissionsComponent (id: number): void
+{
+    addComponent(GameObjectWorld, PermissionsComponent, id);
+
+    PermissionsComponent.willUpdate[id] = 1;
+    PermissionsComponent.willUpdateChildren[id] = 1;
+    PermissionsComponent.willRender[id] = 1;
+    PermissionsComponent.willRenderChildren[id] = 1;
+    PermissionsComponent.willCacheChildren[id] = 0;
+}
