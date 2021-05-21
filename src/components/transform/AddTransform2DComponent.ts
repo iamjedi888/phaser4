687c5848b1dda80d7792e84a5711a5b2d89283ae
Richard Davey <rich@photonstorm.com>
2021-05-21 01:21:26 +0100

    Removed zero defaults

--- src/components/transform/AddTransform2DComponent.ts
+++ src/components/transform/AddTransform2DComponent.ts
@@ -12,28 +12,21 @@ export function AddTransform2DComponent (id: number, x: number = 0, y: number = 
     addComponent(GameObjectWorld, LocalMatrix2DComponent, id);
     addComponent(GameObjectWorld, WorldMatrix2DComponent, id);
 
-    //  Defaults
+    //  Component defaults to zero, so we only need to set the other values
 
     Transform2DComponent.x[id] = x;
     Transform2DComponent.y[id] = y;
-    Transform2DComponent.rotation[id] = 0;
     Transform2DComponent.scaleX[id] = 1;
     Transform2DComponent.scaleY[id] = 1;
-    Transform2DComponent.skewX[id] = 0;
-    Transform2DComponent.skewY[id] = 0;
     Transform2DComponent.originX[id] = originX;
     Transform2DComponent.originY[id] = originY;
 
     LocalMatrix2DComponent.a[id] = 1;
-    LocalMatrix2DComponent.b[id] = 0;
-    LocalMatrix2DComponent.c[id] = 0;
     LocalMatrix2DComponent.d[id] = 1;
     LocalMatrix2DComponent.tx[id] = x;
     LocalMatrix2DComponent.ty[id] = y;
 
     WorldMatrix2DComponent.a[id] = 1;
-    WorldMatrix2DComponent.b[id] = 0;
-    WorldMatrix2DComponent.c[id] = 0;
     WorldMatrix2DComponent.d[id] = 1;
     WorldMatrix2DComponent.tx[id] = x;
     WorldMatrix2DComponent.ty[id] = y;
