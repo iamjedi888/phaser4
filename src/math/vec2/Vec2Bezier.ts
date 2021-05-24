637a98c7facc80c28be05ecdf00125b890f78045
Francisco Pereira <gammafp@gmail.com>
2021-05-24 14:29:39 +0200

    fix Vec2Bezier and Vec2Hermite

--- src/math/vec2/Vec2Bezier.ts
+++ src/math/vec2/Vec2Bezier.ts
@@ -5,7 +5,7 @@ import { Vec2 } from './Vec2';
 export function Vec2Bezier (a: IVec2Like, b: IVec2Like, c: IVec2Like, d: IVec2Like, t: number, out: Vec2 = new Vec2()): Vec2
 {
     return out.set(
-        Bezier(t, a.x, b.x, c.x, d.x),
-        Bezier(t, a.y, b.y, c.y, d.y)
+        Bezier(a.x, b.x, c.x, d.x, t),
+        Bezier(a.y, b.y, c.y, d.y, t)
     );
 }
--- src/math/vec2/Vec2Hermite.ts
+++ src/math/vec2/Vec2Hermite.ts
@@ -5,7 +5,7 @@ import { Vec2 } from './Vec2';
 export function Vec2Hermite (a: IVec2Like, b: IVec2Like, c: IVec2Like, d: IVec2Like, t: number, out: Vec2 = new Vec2()): Vec2
 {
     return out.set(
-        Hermite(t, a.x, b.x, c.x, d.x),
-        Hermite(t, a.y, b.y, c.y, d.y)
+        Hermite(a.x, b.x, c.x, d.x, t),
+        Hermite(a.y, b.y, c.y, d.y, t)
     );
 }
