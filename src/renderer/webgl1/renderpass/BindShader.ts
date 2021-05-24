9a6f676d99fdc57a9a3acf5abc264b8a45d69bb9
Richard Davey <rich@photonstorm.com>
2021-05-24 16:32:17 +0100

    Only bind shader if not already bound

--- src/renderer/webgl1/renderpass/BindShader.ts
+++ src/renderer/webgl1/renderpass/BindShader.ts
@@ -3,15 +3,29 @@ import { ShaderStackEntry } from '../shaders/ShaderStackEntry';
 
 export function BindShader (renderPass: IRenderPass, entry?: ShaderStackEntry): void
 {
+    let prevShader;
+
     if (!entry)
     {
         entry = renderPass.currentShader;
     }
-
-    const success = entry.shader.bind(renderPass, entry.textureID);
-
-    if (success)
+    else
     {
-        entry.shader.setAttributes(renderPass);
+        prevShader = renderPass.currentShader.shader;
+    }
+
+    if (!entry.shader.isActive)
+    {
+        const success = entry.shader.bind(renderPass, entry.textureID);
+
+        if (success)
+        {
+            entry.shader.setAttributes(renderPass);
+
+            if (prevShader && prevShader !== entry.shader)
+            {
+                prevShader.isActive = false;
+            }
+        }
     }
 }
