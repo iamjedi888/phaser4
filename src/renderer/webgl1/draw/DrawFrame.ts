b97eb13b6d9ab4f18f45529a71dcc38eec2d7c3d
Richard Davey <rich@photonstorm.com>
2021-05-24 10:01:51 +0100

    Added scale support

--- src/renderer/webgl1/draw/DrawFrame.ts
+++ src/renderer/webgl1/draw/DrawFrame.ts
@@ -5,7 +5,7 @@ import { ITexture } from '../../../textures/ITexture';
 import { PackColor } from '../colors/PackColor';
 import { SetTexture } from '../renderpass/SetTexture';
 
-export function DrawFrame (renderPass: IRenderPass, texture: ITexture, frame: string | number | IFrame, x: number, y: number, alpha: number = 1): void
+export function DrawFrame (renderPass: IRenderPass, texture: ITexture, frame: string | number | IFrame, x: number, y: number, alpha: number = 1, scaleX: number = 1, scaleY: number = 1): void
 {
     const { F32, U32, offset } = GetVertexBufferEntry(renderPass, 1);
 
@@ -14,6 +14,9 @@ export function DrawFrame (renderPass: IRenderPass, texture: ITexture, frame: st
     frame = texture.getFrame(frame);
 
     const textureIndex = SetTexture(renderPass, texture);
+
+    const displayWidth = frame.width * scaleX;
+    const displayHeight = frame.height * scaleY;
 
     //  top left
     F32[offset + 0] = x;
@@ -25,22 +28,22 @@ export function DrawFrame (renderPass: IRenderPass, texture: ITexture, frame: st
 
     //  bottom left
     F32[offset + 6] = x;
-    F32[offset + 7] = y + frame.height;
+    F32[offset + 7] = y + displayHeight;
     F32[offset + 8] = frame.u0;
     F32[offset + 9] = frame.v1;
     F32[offset + 10] = textureIndex;
     U32[offset + 11] = packedColor;
 
     //  bottom right
-    F32[offset + 12] = x + frame.width;
-    F32[offset + 13] = y + frame.height;
+    F32[offset + 12] = x + displayWidth;
+    F32[offset + 13] = y + displayHeight;
     F32[offset + 14] = frame.u1;
     F32[offset + 15] = frame.v1;
     F32[offset + 16] = textureIndex;
     U32[offset + 17] = packedColor;
 
     //  top right
-    F32[offset + 18] = x + frame.width;
+    F32[offset + 18] = x + displayWidth;
     F32[offset + 19] = y;
     F32[offset + 20] = frame.u1;
     F32[offset + 21] = frame.v0;
