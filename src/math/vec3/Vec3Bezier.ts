514700611dd49bbcfe44e83d518c79255d2020d3
Francisco Pereira <gammafp@gmail.com>
2021-05-26 16:58:41 +0200

    fix Vec3Bezier and Vec3Hermite

--- src/math/vec3/Vec3Bezier.ts
+++ src/math/vec3/Vec3Bezier.ts
@@ -5,8 +5,8 @@ import { Vec3 } from './Vec3';
 export function Vec3Bezier (a: IVec3Like, b: IVec3Like, c: IVec3Like, d: IVec3Like, t: number, out: Vec3 = new Vec3()): Vec3
 {
     return out.set(
-        Bezier(t, a.x, b.x, c.x, d.x),
-        Bezier(t, a.y, b.y, c.y, d.y),
-        Bezier(t, a.z, b.z, c.z, d.z)
+        Bezier(a.x, b.x, c.x, d.x, t),
+        Bezier(a.y, b.y, c.y, d.y, t),
+        Bezier(a.z, b.z, c.z, d.z, t)
     );
 }
--- src/math/vec3/Vec3Hermite.ts
+++ src/math/vec3/Vec3Hermite.ts
@@ -5,8 +5,8 @@ import { Vec3 } from './Vec3';
 export function Vec3Hermite (a: IVec3Like, b: IVec3Like, c: IVec3Like, d: IVec3Like, t: number, out: Vec3 = new Vec3()): Vec3
 {
     return out.set(
-        Hermite(t, a.x, b.x, c.x, d.x),
-        Hermite(t, a.y, b.y, c.y, d.y),
-        Hermite(t, a.z, b.z, c.z, d.z)
+        Hermite(a.x, b.x, c.x, d.x, t),
+        Hermite(a.y, b.y, c.y, d.y, t),
+        Hermite(a.z, b.z, c.z, d.z, t)
     );
 }
