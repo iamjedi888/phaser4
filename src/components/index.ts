0dc5848a5f11b1cce5e54573d893ef5c619a998a
Richard Davey <rich@photonstorm.com>
2021-05-27 11:26:05 +0100

    Export more components

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,12 +1,20 @@
 import * as Bounds from './bounds';
+import * as Dirty from './dirty';
+import * as GameObject from './gameobject';
 import * as Input from './input';
+import * as Permissions from './permissions';
 import * as Transform from './transform';
+import * as Vertices from './vertices';
 
 import { Vertex } from './Vertex';
 
 export {
     Bounds,
+    Dirty,
+    GameObject,
     Input,
+    Permissions,
     Transform,
+    Vertices,
     Vertex
 };
