89b2e682033f8c5b6e9483ed224624021f0bc7a3
Richard Davey <rich@photonstorm.com>
2021-05-27 17:57:12 +0100

    Create entity zero

--- src/components/GameObjectWorld.ts
+++ src/components/GameObjectWorld.ts
@@ -1,5 +1,10 @@
-import { createWorld } from 'bitecs';
+import { addEntity, createWorld } from 'bitecs';
 
 const world = createWorld();
+
+//  Lock out entity zero, so nothing else can ever be assigned to it
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const zeroEntity = addEntity(world);
 
 export const GameObjectWorld = world;
