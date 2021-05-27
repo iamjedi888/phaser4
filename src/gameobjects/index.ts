89ebdaf0e1dfa3723537dfb8e08cc524d8cdb011
Richard Davey <rich@photonstorm.com>
2021-05-27 11:25:58 +0100

    Export the tree and cache

--- src/gameobjects/index.ts
+++ src/gameobjects/index.ts
@@ -4,6 +4,8 @@ import { AnimatedSprite } from './animatedsprite/AnimatedSprite';
 import { Container } from './container/Container';
 import { EffectLayer } from './effectlayer/EffectLayer';
 import { GameObject } from './GameObject';
+import { GameObjectCache } from './GameObjectCache';
+import { GameObjectTree } from './GameObjectTree';
 import { Layer } from './layer/Layer';
 import { Rectangle } from './rectangle/Rectangle';
 import { RenderLayer } from './renderlayer/RenderLayer';
@@ -17,6 +19,8 @@ export {
     Container,
     EffectLayer,
     GameObject,
+    GameObjectCache,
+    GameObjectTree,
     Layer,
     Rectangle,
     RenderLayer,
