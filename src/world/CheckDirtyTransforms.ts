aedee75374bd507384c7e1363d28722365e2ef79
Richard Davey <rich@photonstorm.com>
2021-06-03 17:45:21 +0100

    Create CheckDirtyTransforms.ts

---
+++ src/world/CheckDirtyTransforms.ts
@@ -0,0 +1,14 @@
+import { GetWorldID } from '../components/hierarchy';
+
+export function CheckDirtyTransforms (worldID: number, list: number[]): boolean
+{
+    for (let i = 0; i < list.length; i++)
+    {
+        if (GetWorldID(list[i]) === worldID)
+        {
+            return true;
+        }
+    }
+
+    return false;
+}
