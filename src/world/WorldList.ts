c02e2ba91ed86c704c1635923667348250b503e7
Richard Davey <rich@photonstorm.com>
2021-06-03 23:41:08 +0100

    Added WorldList and removed WorldRenderList

---
+++ src/world/WorldList.ts
@@ -0,0 +1,4 @@
+import { IBaseWorld } from './IBaseWorld';
+import { IScene } from '../scenes/IScene';
+
+export const WorldList: Map<IScene, IBaseWorld[]> = new Map();
--- src/world/WorldRenderList.ts
+++
@@ -1,3 +0,0 @@
-import { IGameObject } from '../gameobjects/IGameObject';
-
-export const WorldRenderList: Set<IGameObject> = new Set();
