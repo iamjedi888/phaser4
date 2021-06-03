fc0a2061b6eca553b41811f1079a038f349ef733
Richard Davey <rich@photonstorm.com>
2021-06-03 17:45:17 +0100

    Create RebuildWorldTransforms.ts

---
+++ src/world/RebuildWorldTransforms.ts
@@ -0,0 +1,45 @@
+import { WillRender, WillRenderChildren } from '../components/permissions';
+
+import { GameObjectTree } from '../gameobjects';
+import { GetNumChildren } from '../components/hierarchy';
+import { IBaseWorld } from './IBaseWorld';
+import { UpdateWorldTransform } from '../components/transform';
+import { WillTransformChildren } from '../components/permissions/WillTransformChildren';
+
+export function RebuildWorldTransforms (world: IBaseWorld, parent: number, transformList: number[], forceUpdate: boolean): void
+{
+    if (WillRender(parent))
+    {
+        if (!forceUpdate && transformList.indexOf(parent) > -1)
+        {
+            forceUpdate = true;
+        }
+
+        if (forceUpdate)
+        {
+            UpdateWorldTransform(parent);
+        }
+
+        const children = GameObjectTree.get(parent);
+
+        for (let i = 0; i < children.length; i++)
+        {
+            const nodeID = children[i];
+
+            if (WillRender(nodeID))
+            {
+                if (GetNumChildren(nodeID) > 0)
+                {
+                    if (WillRenderChildren(nodeID) && WillTransformChildren(nodeID))
+                    {
+                        RebuildWorldTransforms(world, nodeID, transformList, forceUpdate);
+                    }
+                }
+                else if (forceUpdate || transformList.indexOf(nodeID) > -1)
+                {
+                    UpdateWorldTransform(nodeID);
+                }
+            }
+        }
+    }
+}
