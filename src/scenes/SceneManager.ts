d5f7d0e7cfcd07a3fe2a1d5d22f9599834a86a92
Richard Davey <rich@photonstorm.com>
2021-06-07 18:16:35 +0100

    Collate Render Stats

--- src/scenes/SceneManager.ts
+++ src/scenes/SceneManager.ts
@@ -1,6 +1,6 @@
 import { Changed, addEntity, defineQuery } from 'bitecs';
 
-import { AddSceneRenderDataComponent } from './AddSceneRenderDataComponent';
+import { AddRenderStatsComponent } from './AddRenderStatsComponent';
 import { Game } from '../Game';
 import { GameInstance } from '../GameInstance';
 import { GameObjectWorld } from '../GameObjectWorld';
@@ -8,7 +8,8 @@ import { GetScenes } from '../config/scenes';
 import { IScene } from './IScene';
 import { LocalMatrix2DComponent } from '../components/transform';
 import { Once } from '../events';
-import { ResetSceneRenderData } from './ResetSceneRenderData';
+import { RenderStatsComponent } from './RenderStatsComponent';
+import { ResetRenderStats } from './ResetRenderStats';
 import { SceneManagerInstance } from './SceneManagerInstance';
 import { UpdateLocalTransform2DSystem } from '../components/transform/UpdateLocalTransform2DSystem';
 import { UpdateVertexPositionSystem } from '../components/vertices/UpdateVertexPositionSystem';
@@ -36,7 +37,7 @@ export class SceneManager
 
         SceneManagerInstance.set(this);
 
-        AddSceneRenderDataComponent(this.id);
+        AddRenderStatsComponent(this.id);
 
         Once(this.game, 'boot', () => this.boot());
     }
@@ -46,8 +47,11 @@ export class SceneManager
         GetScenes().forEach(scene => new scene());
     }
 
-    update (delta: number, time: number): void
+    update (delta: number, time: number, gameFrame: number): void
     {
+        let sceneTotal = 0;
+        let worldTotal = 0;
+
         for (const scene of this.scenes.values())
         {
             const worlds = WorldList.get(scene);
@@ -57,20 +61,24 @@ export class SceneManager
                 world.beforeUpdate(delta, time);
                 world.update(delta, time);
                 world.afterUpdate(delta, time);
+
+                worldTotal++;
             }
+
+            sceneTotal++;
         }
 
         //  Process all dirty 2D transforms and update the local matrix across all Worlds and Scenes
-        UpdateLocalTransform2DSystem(GameObjectWorld);
+        const localTransforms = UpdateLocalTransform2DSystem(GameObjectWorld);
+
+        ResetRenderStats(this.id, gameFrame, sceneTotal, worldTotal, localTransforms.length);
     }
 
     //  Run through all Scenes and Worlds within them, telling them to prepare to render
     //  The renderer itself tells them to actually render
     preRender (gameFrame: number): void
     {
-        ResetSceneRenderData(this.id, gameFrame);
-
-        const dirtyTransforms: number[] = this.changedMatrixQuery(GameObjectWorld);
+        const dirtyTransforms = this.changedMatrixQuery(GameObjectWorld);
 
         let dirtyWorld = false;
 
@@ -88,11 +96,23 @@ export class SceneManager
         }
 
         //  Update all vertices across the whole game, ready for rendering
-        UpdateVertexPositionSystem(GameObjectWorld);
+        const verts = UpdateVertexPositionSystem(GameObjectWorld);
+
+        RenderStatsComponent.numDirtyVertices[this.id] = verts.length;
 
         if (dirtyWorld)
         {
             this.flush = true;
         }
+    }
+
+    updateWorldStats (numGameObjects: number, numRendered: number, numDisplayLists: number, numWorldTransforms: number): void
+    {
+        const id = this.id;
+
+        RenderStatsComponent.numGameObjects[id] += numGameObjects;
+        RenderStatsComponent.numGameObjectsRendered[id] += numRendered;
+        RenderStatsComponent.numDirtyWorldLists[id] += numDisplayLists;
+        RenderStatsComponent.numDirtyWorldTransforms[id] += numWorldTransforms;
     }
 }
