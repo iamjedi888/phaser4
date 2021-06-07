c4822d08d6b22cc34b4c0e30b03f44658ee97725
Richard Davey <rich@photonstorm.com>
2021-06-07 18:15:42 +0100

    Be explicit

--- src/components/hierarchy/SetParentID.ts
+++ src/components/hierarchy/SetParentID.ts
@@ -1,6 +1,6 @@
 import { HierarchyComponent } from './HierarchyComponent';
 
-export function SetParentID (id: number, parentID: number): void
+export function SetParentID (childID: number, parentID: number): void
 {
-    HierarchyComponent.parentID[id] = parentID;
+    HierarchyComponent.parentID[childID] = parentID;
 }
