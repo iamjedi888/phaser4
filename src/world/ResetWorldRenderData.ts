5acfcca1dcaa1f346e7a10923307f8af251f0514
Richard Davey <rich@photonstorm.com>
2021-06-07 17:12:03 +0100

    Store numWorld in here until bitecs 0.3.9 released

--- src/world/ResetWorldRenderData.ts
+++ src/world/ResetWorldRenderData.ts
@@ -1,9 +1,23 @@
 import { RenderDataComponent } from './RenderDataComponent';
 
+let numWorldTransforms: number = 0;
+
 export function ResetWorldRenderData (id: number, gameFrame: number): void
 {
+    numWorldTransforms = 0;
+
     RenderDataComponent.gameFrame[id] = gameFrame;
     RenderDataComponent.dirtyFrame[id] = 0;
     RenderDataComponent.numRendered[id] = 0;
     RenderDataComponent.numRenderable[id] = 0;
+}
+
+export function UpdateNumWorldTransforms (): void
+{
+    numWorldTransforms++;
+}
+
+export function GetNumWorldTransforms (): number
+{
+    return numWorldTransforms;
 }
