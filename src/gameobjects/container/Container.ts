f00ad166815e01d23a97c716f274e998d7889d66
Richard Davey <rich@photonstorm.com>
2021-06-11 15:35:54 +0100

    Revised interface

--- src/gameobjects/container/Container.ts
+++ src/gameobjects/container/Container.ts
@@ -1,4 +1,4 @@
-import { AddTransform2DComponent, Extent2DComponent, Transform2DComponent, UpdateExtent } from '../../components/transform/';
+import { AddTransform2DComponent, Origin, Position, Scale, Size, Skew, Transform2DComponent } from '../../components/transform/';
 import { GetDefaultOriginX, GetDefaultOriginY } from '../../config/defaultorigin';
 
 import { GameObject } from '../GameObject';
@@ -6,271 +6,56 @@ import { IContainer } from './IContainer';
 import { IGameObject } from '../IGameObject';
 import { Rectangle } from '../../geom/rectangle/Rectangle';
 import { SetDirtyAlpha } from '../../components/dirty/';
-import { Vec2 } from '../../math/vec2/Vec2';
 
 export class Container extends GameObject implements IContainer
 {
     protected _alpha: number = 1;
 
+    position: Position;
+    scale: Scale;
+    skew: Skew;
+    origin: Origin;
+    size: Size;
+
     constructor (x: number = 0, y: number = 0)
     {
         super();
 
-        AddTransform2DComponent(this.id, x, y, GetDefaultOriginX(), GetDefaultOriginY());
-    }
-
-    /*
-    updateTransform (flag: number, value: number): void
-    {
-        if (this.transformData[flag] !== value)
-        {
-            this.transformData[flag] = value;
-
-            this.updateLocalTransform();
-            this.updateWorldTransform();
-        }
-    }
-
-    updateLocalTransform (): void
-    {
-        this.setDirty(DIRTY_CONST.TRANSFORM, DIRTY_CONST.BOUNDS);
-
-        UpdateLocalTransform(this.localTransform, this.transformData);
-    }
-
-    updateTransform (): void
-    {
-        this.setDirty(DIRTY_CONST.TRANSFORM, DIRTY_CONST.BOUNDS);
-
-        //  To take advantage of Changed batching we didn't ought to do this here, but on render
-        UpdateTransform2DSystem(GameObjectWorld);
-
         const id = this.id;
 
-        this.localTransform.set(
-            Matrix2DComponent.a[id],
-            Matrix2DComponent.b[id],
-            Matrix2DComponent.c[id],
-            Matrix2DComponent.d[id],
-            Matrix2DComponent.tx[id],
-            Matrix2DComponent.ty[id]
-            );
+        AddTransform2DComponent(id, x, y, GetDefaultOriginX(), GetDefaultOriginY());
 
-            this.updateWorldTransform();
-        }
-    */
-
-    updateWorldTransform (): void
-    {
-        /*
-        this.setDirty(DIRTY_CONST.TRANSFORM, DIRTY_CONST.BOUNDS);
-
-        const parentWorldTransform = (this.parent) ? this.parent.worldTransform : undefined;
-
-        UpdateWorldTransform(this.localTransform, this.worldTransform, this.passthru, parentWorldTransform);
-
-        if (this.numChildren)
-        {
-            //  Sweep all children - by this point our local and world transforms are correct
-            const children = this.children;
-
-            for (let i = 0; i < children.length; i++)
-            {
-                const child = children[i];
-
-                child.updateWorldTransform();
-            }
-        }
-        */
+        this.position = new Position(id, x, y);
+        this.scale = new Scale(id);
+        this.skew = new Skew(id);
+        this.size = new Size(id);
+        this.origin = new Origin(id, GetDefaultOriginX(), GetDefaultOriginY());
     }
 
     getBounds (): Rectangle
     {
-        return this.bounds.get();
-    }
-
-    setSize (width: number, height: number = width): this
-    {
-        UpdateExtent(this.id, width, height);
-
-        return this;
-    }
-
-    setPosition (x: number, y: number): this
-    {
-        this.x = x;
-        this.y = y;
-
-        return this;
-    }
-
-    setSkew (x: number, y: number = x): this
-    {
-        this.skewX = x;
-        this.skewY = y;
-
-        return this;
-    }
-
-    setScale (x: number, y: number = x): this
-    {
-        this.scaleX = x;
-        this.scaleY = y;
-
-        return this;
-    }
-
-    setRotation (value: number): this
-    {
-        this.rotation = value;
-
-        return this;
-    }
-
-    setOrigin (x: number, y: number = x): this
-    {
-        const id = this.id;
-
-        Transform2DComponent.originX[id] = x;
-        Transform2DComponent.originY[id] = y;
-
-        UpdateExtent(id, this.width, this.height);
-
-        return this;
-    }
-
-    getSize (out: Vec2 = new Vec2()): Vec2
-    {
-        return out.set(Extent2DComponent.width[this.id], Extent2DComponent.height[this.id]);
-    }
-
-    getPosition (out: Vec2 = new Vec2()): Vec2
-    {
-        return out.set(this.x, this.y);
-    }
-
-    getOrigin (out: Vec2 = new Vec2()): Vec2
-    {
-        return out.set(this.originX, this.originY);
-    }
-
-    getSkew (out: Vec2 = new Vec2()): Vec2
-    {
-        return out.set(this.skewX, this.skewY);
-    }
-
-    getScale (out: Vec2 = new Vec2()): Vec2
-    {
-        return out.set(this.scaleX, this.scaleY);
-    }
-
-    getRotation (): number
-    {
-        return this.rotation;
-    }
-
-    set width (value: number)
-    {
-        UpdateExtent(this.id, value, this.height);
-    }
-
-    get width (): number
-    {
-        return Extent2DComponent.width[this.id];
-    }
-
-    set height (value: number)
-    {
-        UpdateExtent(this.id, this.width, value);
-    }
-
-    get height (): number
-    {
-        return Extent2DComponent.height[this.id];
+        // return this.bounds.get();
+        return new Rectangle();
     }
 
     set x (value: number)
     {
-        Transform2DComponent.x[this.id] = value;
+        this.position.x = value;
     }
 
     get x (): number
     {
-        return Transform2DComponent.x[this.id];
+        return this.position.x;
     }
 
     set y (value: number)
     {
-        Transform2DComponent.y[this.id] = value;
+        this.position.y = value;
     }
 
     get y (): number
     {
-        return Transform2DComponent.y[this.id];
-    }
-
-    set originX (value: number)
-    {
-        Transform2DComponent.originX[this.id] = value;
-
-        UpdateExtent(this.id, this.width, this.height);
-    }
-
-    get originX (): number
-    {
-        return Transform2DComponent.originX[this.id];
-    }
-
-    set originY (value: number)
-    {
-        Transform2DComponent.originY[this.id] = value;
-
-        UpdateExtent(this.id, this.width, this.height);
-    }
-
-    get originY (): number
-    {
-        return Transform2DComponent.originY[this.id];
-    }
-
-    set skewX (value: number)
-    {
-        Transform2DComponent.skewX[this.id] = value;
-    }
-
-    get skewX (): number
-    {
-        return Transform2DComponent.skewX[this.id];
-    }
-
-    set skewY (value: number)
-    {
-        Transform2DComponent.skewY[this.id] = value;
-    }
-
-    get skewY (): number
-    {
-        return Transform2DComponent.skewY[this.id];
-    }
-
-    set scaleX (value: number)
-    {
-        Transform2DComponent.scaleX[this.id] = value;
-    }
-
-    get scaleX (): number
-    {
-        return Transform2DComponent.scaleX[this.id];
-    }
-
-    set scaleY (value: number)
-    {
-        Transform2DComponent.scaleY[this.id] = value;
-    }
-
-    get scaleY (): number
-    {
-        return Transform2DComponent.scaleY[this.id];
+        return this.position.y;
     }
 
     set rotation (value: number)
@@ -312,10 +97,5 @@ export class Container extends GameObject implements IContainer
     destroy (reparentChildren?: IGameObject): void
     {
         super.destroy(reparentChildren);
-
-        //  TODO - Clear components
-
-        // this.bounds.destroy();
-        // this.input.destroy();
     }
 }
--- src/gameobjects/container/IContainer.ts
+++ src/gameobjects/container/IContainer.ts
@@ -1,39 +1,22 @@
+import { Origin, Position, Scale, Size, Skew } from '../../components/transform';
+
 import { IGameObject } from '../IGameObject';
-import { Matrix2D } from '../../math/mat2d/Matrix2D';
 import { Rectangle } from '../../geom/rectangle/Rectangle';
-import { Vec2 } from '../../math/vec2/Vec2';
 
 export interface IContainer extends IGameObject
 {
+    position: Position;
+    scale: Scale;
+    skew: Skew;
+    origin: Origin;
+    size: Size;
+
     x: number;
     y: number;
-    width: number;
-    height: number;
     rotation: number;
-    scaleX: number;
-    scaleY: number;
-    skewX: number;
-    skewY: number;
-    originX: number;
-    originY: number;
-
     alpha: number;
 
     // getBounds (): Rectangle;
-
-    setSize (width: number, height?: number): this;
-    setPosition (x: number, y: number): this;
-    setOrigin (x: number, y?: number): this;
-    setSkew (x: number, y?: number): this;
-    setScale (x: number, y?: number): this;
-    setRotation (value: number): this;
-
-    getSize (out?: Vec2): Vec2;
-    getPosition (out?: Vec2): Vec2;
-    getOrigin (out?: Vec2): Vec2;
-    getSkew (out?: Vec2): Vec2;
-    getScale (out?: Vec2): Vec2;
-    getRotation (): number;
 
     // setAlpha (value?: number): this;
 }
