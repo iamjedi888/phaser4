b319bd3529f693dc6d4531c461863ada418b1385
Richard Davey <rich@photonstorm.com>
2021-06-11 14:46:33 +0100

    Exposed functions

--- src/components/transform/index.ts
+++ src/components/transform/index.ts
@@ -5,8 +5,13 @@ import { GetVerticesFromValues } from './GetVerticesFromValues';
 import { GlobalToLocal } from './GlobalToLocal';
 import { LocalMatrix2DComponent } from './LocalMatrix2DComponent';
 import { LocalToGlobal } from './LocalToGlobal';
+import { Origin } from './Origin';
+import { Position } from './Position';
 import { PreRenderVertices } from './PreRenderVertices';
+import { Scale } from './Scale';
 import { SetExtent } from './SetExtent';
+import { Size } from './Size';
+import { Skew } from './Skew';
 import { Transform2DComponent } from './Transform2DComponent';
 import { UpdateExtent } from './UpdateExtent';
 import { UpdateLocalTransform2DSystem } from './UpdateLocalTransform2DSystem';
@@ -23,8 +28,13 @@ export {
     GlobalToLocal,
     LocalMatrix2DComponent,
     LocalToGlobal,
+    Origin,
+    Position,
     PreRenderVertices,
+    Scale,
     SetExtent,
+    Size,
+    Skew,
     Transform2DComponent,
     UpdateExtent,
     UpdateLocalTransform2DSystem,
