3adc427ec1f567fe2932b68d57a7e363b8bcfab8
Richard Davey <rich@photonstorm.com>
2021-06-14 18:22:16 +0100

    Update parent list

--- src/components/hierarchy/SetParentID.ts
+++ src/components/hierarchy/SetParentID.ts
@@ -1,6 +1,11 @@
 import { HierarchyComponent } from './HierarchyComponent';
+import { UpdateNumChildren } from './UpdateNumChildren';
 
 export function SetParentID (childID: number, parentID: number): void
 {
     HierarchyComponent.parentID[childID] = parentID;
+
+    UpdateNumChildren(parentID);
+
+    //  Emit add event?
 }
