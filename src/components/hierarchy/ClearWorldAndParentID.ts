4fbdc291a0aeb47e0585abd0e643dc63c33348d6
Richard Davey <rich@photonstorm.com>
2021-06-14 18:22:05 +0100

    Clear fully and reset lists

--- src/components/hierarchy/ClearWorldAndParentID.ts
+++ src/components/hierarchy/ClearWorldAndParentID.ts
@@ -1,7 +1,23 @@
-import { HierarchyComponent } from './HierarchyComponent';
+import { GetParentID, GetWorldID, HierarchyComponent, UpdateNumChildren } from './';
+
+import { GameObjectCache } from '../../gameobjects/GameObjectCache';
+import { GameObjectWorld } from '../../GameObjectWorld';
+import { IBaseWorld } from '../../world/IBaseWorld';
+import { SetDirtyDisplayList } from '../dirty';
+import { removeComponent } from 'bitecs';
 
 export function ClearWorldAndParentID (id: number): void
 {
+    const worldID = GetWorldID(id);
+    const parentID = GetParentID(id);
+    const world = GameObjectCache.get(worldID) as IBaseWorld;
+
     HierarchyComponent.worldID[id] = 0;
     HierarchyComponent.parentID[id] = 0;
+
+    removeComponent(GameObjectWorld, world.tag, id);
+
+    UpdateNumChildren(parentID);
+
+    SetDirtyDisplayList(worldID);
 }
