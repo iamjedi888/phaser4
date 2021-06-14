d20c6a60a9a6bf1d1ac9228a824200562933ad2c
Richard Davey <rich@photonstorm.com>
2021-06-14 18:22:32 +0100

    Just uses AddChildAt

--- src/display/AddChild.ts
+++ src/display/AddChild.ts
@@ -1,39 +1,7 @@
-import { GetWorldID, SetParentID, SetWorldAndParentID, UpdateNumChildren } from '../components/hierarchy';
-
-import { GameObjectCache } from '../gameobjects';
-import { GameObjectTree } from '../gameobjects/GameObjectTree';
-import { IBaseWorld } from '../world/IBaseWorld';
+import { AddChildAt } from './AddChildAt';
 import { IGameObject } from '../gameobjects/IGameObject';
-import { IsValidParent } from './IsValidParent';
-import { RemoveChild } from './RemoveChild';
-import { SetDirtyDisplayList } from '../components/dirty';
-import { SetWorld } from './SetWorld';
 
 export function AddChild <P extends IGameObject, C extends IGameObject> (parent: P, child: C): C
 {
-    if (IsValidParent(parent, child))
-    {
-        const childID = child.id;
-        const parentID = parent.id;
-        const worldID = GetWorldID(parentID);
-        const world = GameObjectCache.get(worldID) as IBaseWorld;
-
-        RemoveChild(child.getParent(), child);
-
-        GameObjectTree.get(parentID).push(childID);
-
-        // SetWorldAndParentID(childID, worldID, parentID);
-
-        SetParentID(childID, parentID);
-
-        SetWorld(world, child);
-
-        SetDirtyDisplayList(worldID);
-
-        //  Emit add event?
-
-        UpdateNumChildren(parentID);
-    }
-
-    return child;
+    return AddChildAt(parent, child);
 }
