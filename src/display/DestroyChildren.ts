20d79e1d9890dd0df87c12cc463a11f1ee879f7f
Richard Davey <rich@photonstorm.com>
2021-06-15 13:23:15 +0100

    Update DestroyChildren.ts

--- src/display/DestroyChildren.ts
+++ src/display/DestroyChildren.ts
@@ -1,7 +1,7 @@
 import { IGameObject } from '../gameobjects/IGameObject';
 import { RemoveChildrenBetween } from './RemoveChildrenBetween';
 
-export function DestroyChildren (parent: IGameObject, beginIndex: number = 0, endIndex?: number): void
+export function DestroyChildren <T extends IGameObject> (parent: T, beginIndex: number = 0, endIndex?: number): void
 {
     const removed = RemoveChildrenBetween(parent, beginIndex, endIndex);
 
