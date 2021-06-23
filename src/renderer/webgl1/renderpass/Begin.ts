87d5af9783ec76e5230a1d859388d78bef201046
Richard Davey <rich@photonstorm.com>
2021-06-23 21:10:31 +0100

    Update Begin.ts

--- src/renderer/webgl1/renderpass/Begin.ts
+++ src/renderer/webgl1/renderpass/Begin.ts
@@ -1,6 +1,8 @@
 import { IBaseCamera } from '../../../camera/IBaseCamera';
 import { IRenderPass } from './IRenderPass';
 
+//  Call at the start of each World rendering
+
 export function Begin (renderPass: IRenderPass, camera2D: IBaseCamera): void
 {
     renderPass.current2DCamera = camera2D;
