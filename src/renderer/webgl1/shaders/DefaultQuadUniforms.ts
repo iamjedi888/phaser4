535f9b5356228bad55b5860b9a559b8c59d1395d
Richard Davey <rich@photonstorm.com>
2021-06-23 13:51:50 +0100

    Better object type

--- src/renderer/webgl1/shaders/DefaultQuadUniforms.ts
+++ src/renderer/webgl1/shaders/DefaultQuadUniforms.ts
@@ -1,4 +1,4 @@
-export const DefaultQuadUniforms =
+export const DefaultQuadUniforms: Record<string, Number | Float32List> =
 {
     uProjectionMatrix: new Float32Array(),
     uCameraMatrix: new Float32Array(),
