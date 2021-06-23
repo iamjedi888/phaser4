9c32df8075121164c6d48beb3286208b0704af00
Richard Davey <rich@photonstorm.com>
2021-06-23 13:51:40 +0100

    Removed pre-defined properties

--- src/renderer/webgl1/shaders/DefaultQuadAttributes.ts
+++ src/renderer/webgl1/shaders/DefaultQuadAttributes.ts
@@ -1,14 +1,11 @@
 import * as GL_CONST from '../GL_CONST';
 
-// size = 8
-// size = 8, offset = position
-// size = 4, offset = position + tex coord
-// size = 4, offset = position + tex coord + index
+import { ShaderAttributeEntry } from './CreateAttributes';
 
-export const DefaultQuadAttributes =
+export const DefaultQuadAttributes: Record<string, ShaderAttributeEntry> =
 {
-    aVertexPosition: { size: 2, type: GL_CONST.FLOAT, normalized: false, offset: 0 },
-    aTextureCoord: { size: 2, type: GL_CONST.FLOAT, normalized: false, offset: 8 },
-    aTextureId: { size: 1, type: GL_CONST.FLOAT, normalized: false, offset: 16 },
-    aTintColor: { size: 4, type: GL_CONST.UNSIGNED_BYTE, normalized: true, offset: 20 }
+    aVertexPosition: { size: 2 },
+    aTextureCoord: { size: 2 },
+    aTextureId: { size: 1 },
+    aTintColor: { size: 4, type: GL_CONST.UNSIGNED_BYTE, normalized: true }
 };
