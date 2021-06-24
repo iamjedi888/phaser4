878c6a64962a97949ee9c70a91113350c73e6f93
Richard Davey <rich@photonstorm.com>
2021-06-24 23:58:03 +0100

    Update SetWillCacheChildren.ts

--- src/components/permissions/SetWillCacheChildren.ts
+++ src/components/permissions/SetWillCacheChildren.ts
@@ -1,11 +1,14 @@
 import { IGameObject } from '../../gameobjects/IGameObject';
 import { PermissionsComponent } from './PermissionsComponent';
+import { SetRenderType } from '../hierarchy/SetRenderType';
 
 export function SetWillCacheChildren (value: boolean, ...children: IGameObject[]): IGameObject[]
 {
     children.forEach(child =>
     {
         PermissionsComponent.willCacheChildren[child.id] = Number(value);
+
+        SetRenderType(child.id);
     });
 
     return children;
