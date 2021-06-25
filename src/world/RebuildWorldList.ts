fba75bd41e77af87a77a0ed1787e5fad618433c7
Richard Davey <rich@photonstorm.com>
2021-06-25 17:21:29 +0100

    Handle cached layers

--- src/world/RebuildWorldList.ts
+++ src/world/RebuildWorldList.ts
@@ -1,7 +1,8 @@
-import { GetNumChildren, GetPostRenderType, GetRenderType } from '../components/hierarchy';
-import { WillRender, WillRenderChildren } from '../components/permissions';
+import { WillCacheChildren, WillRender, WillRenderChildren } from '../components/permissions';
 
 import { GameObjectTree } from '../gameobjects';
+import { GetNumChildren } from '../components/hierarchy';
+import { HasDirtyChildCache } from '../components/dirty';
 import { IBaseWorld } from './IBaseWorld';
 
 //  Rebuilds the World.renderList - a list of all entities that need to render.
@@ -15,7 +16,7 @@ export function RebuildWorldList (world: IBaseWorld, parent: number): void
     {
         if (world.id !== parent)
         {
-            world.addToRenderList(parent, GetRenderType(parent));
+            world.addToRenderList(parent, 0);
         }
 
         const children = GameObjectTree.get(parent);
@@ -28,9 +29,18 @@ export function RebuildWorldList (world: IBaseWorld, parent: number): void
             {
                 if (GetNumChildren(nodeID) > 0 && WillRenderChildren(nodeID))
                 {
-                    RebuildWorldList(world, nodeID);
+                    //  Is a Child Cache, like a Render Layer, or doesn't cache children, like a Container
+                    if (!WillCacheChildren(nodeID) || HasDirtyChildCache(nodeID))
+                    {
+                        RebuildWorldList(world, nodeID);
+                    }
+                    else
+                    {
+                        world.addToRenderList(nodeID, 0);
+                        world.addToRenderList(nodeID, 1);
+                    }
                 }
-                else
+                else if (!WillCacheChildren(nodeID))
                 {
                     world.addToRenderList(nodeID, 0);
                     world.addToRenderList(nodeID, 1);
@@ -40,7 +50,7 @@ export function RebuildWorldList (world: IBaseWorld, parent: number): void
 
         if (world.id !== parent)
         {
-            world.addToRenderList(parent, GetPostRenderType(parent));
+            world.addToRenderList(parent, 1);
         }
     }
 }
