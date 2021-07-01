a06c3824c2abaf49fcf7c74c9e9be860cb939266
Richard Davey <rich@photonstorm.com>
2021-07-01 17:28:25 +0100

    Added optional parameters

--- src/components/vertices/AddVertex.ts
+++ src/components/vertices/AddVertex.ts
@@ -3,11 +3,17 @@ import { addComponent, addEntity } from 'bitecs';
 import { VertexComponent } from './VertexComponent';
 import { VertexWorld } from './VertexWorld';
 
-export function AddVertex (): number
+export function AddVertex (x: number = 0, y: number = 0, z: number = 0, u: number = 0, v: number = 0): number
 {
     const vertexID = addEntity(VertexWorld);
 
     addComponent(VertexWorld, VertexComponent, vertexID);
+
+    VertexComponent.x[vertexID] = x;
+    VertexComponent.y[vertexID] = y;
+    VertexComponent.z[vertexID] = z;
+    VertexComponent.u[vertexID] = u;
+    VertexComponent.v[vertexID] = v;
 
     VertexComponent.alpha[vertexID] = 1;
     VertexComponent.tint[vertexID] = 0xffffff;
