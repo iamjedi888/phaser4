73eac8d4d11b0e9f15d05877f1feb3eaae03e4d5
Richard Davey <rich@photonstorm.com>
2021-07-02 11:56:52 +0100

    Work out time and res automatically and allow override and time scaling

--- src/renderer/webgl1/shaders/FXShader.ts
+++ src/renderer/webgl1/shaders/FXShader.ts
@@ -1,30 +1,63 @@
 import { DefaultQuadAttributes } from './DefaultQuadAttributes';
+import { IFXShaderConfig } from './IFXShaderConfig';
 import { IRenderPass } from '../renderpass/IRenderPass';
 import { IShader } from './IShader';
-import { IShaderConfig } from './IShaderConfig';
 import { QuadShader } from './QuadShader';
 
-//  A Quad Shader with built in uTime and uResolution uniforms
+//  A Quad Shader with built in time and resolution uniforms.
+//  It will automatically look for uniforms called "Uime" and "uResolution"
+//  and if it doesn't find those, it will look for "time" and "resolution".
+//  If you use other uniform names then pass them in the config object.
 
 export class FXShader extends QuadShader implements IShader
 {
-    constructor (config: IShaderConfig = {})
+    private timeVar: string;
+    private resolutionVar: string;
+
+    timeScale: number = 1;
+
+    constructor (config: IFXShaderConfig = {})
     {
-        const shaderConfig = config;
+        config.attributes = config?.attributes || DefaultQuadAttributes;
 
-        shaderConfig.attributes = (!shaderConfig.attributes) ? DefaultQuadAttributes : shaderConfig.attributes;
+        config.renderToFramebuffer = true;
 
-        shaderConfig.renderToFramebuffer = true;
+        super(config);
 
-        super(shaderConfig);
+        const {
+            timeUniform = 'uTime',
+            resolutionUniform = 'uResolution'
+        } = config;
+
+        const uniforms = [ ... this.uniformSetters.keys() ];
+
+        this.timeVar = uniforms.includes(timeUniform) ? timeUniform : 'time';
+        this.resolutionVar = uniforms.includes(resolutionUniform) ? resolutionUniform : 'resolution';
+
+        if (!uniforms.includes(this.timeVar))
+        {
+            this.timeVar = undefined;
+        }
+
+        if (!uniforms.includes(this.resolutionVar))
+        {
+            this.resolutionVar = undefined;
+        }
     }
 
     bind (renderPass: IRenderPass): boolean
     {
         const renderer = renderPass.renderer;
 
-        this.uniforms.set('uTime', performance.now());
-        this.uniforms.set('uResolution', [ renderer.width, renderer.height ]);
+        if (this.timeVar)
+        {
+            this.uniforms.set(this.timeVar, performance.now() * this.timeScale);
+        }
+
+        if (this.resolutionVar)
+        {
+            this.uniforms.set(this.resolutionVar, [ renderer.width, renderer.height ]);
+        }
 
         return super.bind(renderPass);
     }
