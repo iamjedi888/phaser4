e4924cdfa78608e16a894843ca7d8c87dcedc114
Richard Davey <rich@photonstorm.com>
2021-07-02 18:10:23 +0100

    Testing compressed texture formats

--- src/renderer/webgl1/textures/CreateGLTexture.ts
+++ src/renderer/webgl1/textures/CreateGLTexture.ts
@@ -1,9 +1,9 @@
 import { IGLTextureBinding } from './IGLTextureBinding';
 import { gl } from '../GL';
 
-export function CreateGLTexture <T extends IGLTextureBinding> (binding: T): WebGLTexture
+export function CreateGLTexture <T extends IGLTextureBinding> (binding: T, data?: Uint8Array): WebGLTexture
 {
-    const { parent, flipY, unpackPremultiplyAlpha, minFilter, magFilter, wrapS, wrapT, generateMipmap, isPOT } = binding;
+    const { parent, compressed, format, flipY, unpackPremultiplyAlpha, minFilter, magFilter, wrapS, wrapT, generateMipmap, isPOT } = binding;
 
     const source = parent.image;
 
@@ -25,10 +25,33 @@ export function CreateGLTexture <T extends IGLTextureBinding> (binding: T): WebG
         width = source.width;
         height = source.height;
     }
+    else if (compressed)
+    {
+        // void gl.compressedTexImage2D(target, level, internalformat, width, height, border, ArrayBufferView? pixels);
+
+        // https://developer.mozilla.org/en-US/docs/Web/API/WebGLRenderingContext/compressedTexImage2D
+
+        const formats = {
+            COMPRESSED_RGB_S3TC_DXT1_EXT: 0x83F0,
+            COMPRESSED_RGBA_S3TC_DXT1_EXT: 0x83F1,
+            COMPRESSED_RGBA_S3TC_DXT3_EXT: 0x83F2,
+            COMPRESSED_RGBA_S3TC_DXT5_EXT: 0x83F3,
+
+            COMPRESSED_RGB_PVRTC_4BPPV1_IMG: 0x8C00,
+            COMPRESSED_RGB_PVRTC_2BPPV1_IMG: 0x8C01,
+            COMPRESSED_RGBA_PVRTC_4BPPV1_IMG: 0x8C02,
+            COMPRESSED_RGBA_PVRTC_2BPPV1_IMG: 0x8C03
+        };
+
+        gl.compressedTexImage2D(gl.TEXTURE_2D, 0, formats[ format ], width, height, 0, data);
+    }
     else
     {
         gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, width, height, 0, gl.RGBA, gl.UNSIGNED_BYTE, null);
     }
+
+    //  Could we use https://developer.mozilla.org/en-US/docs/Web/API/WebGLRenderingContext/texSubImage2D instead?
+    //  Then it may allow for wrapping, repeating, etc on a sub-image
 
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, minFilter);
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, magFilter);
