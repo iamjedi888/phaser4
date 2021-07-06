8f2d8132bb5ee51bb65d650fe891d72136f72f7a
Richard Davey <rich@photonstorm.com>
2021-07-06 13:48:07 +0100

    Removed old format list

--- src/renderer/webgl1/textures/CreateGLTexture.ts
+++ src/renderer/webgl1/textures/CreateGLTexture.ts
@@ -3,9 +3,7 @@ import { gl } from '../GL';
 
 export function CreateGLTexture <T extends IGLTextureBinding> (binding: T, data?: Uint8Array): WebGLTexture
 {
-    let { generateMipmap, minFilter } = binding;
-
-    const { parent, compressed, format, flipY, unpackPremultiplyAlpha, magFilter, wrapS, wrapT, isPOT } = binding;
+    const { generateMipmap, minFilter, parent, compressed, internalFormat, flipY, unpackPremultiplyAlpha, magFilter, wrapS, wrapT, isPOT } = binding;
 
     const source = parent.image;
 
@@ -29,32 +27,9 @@ export function CreateGLTexture <T extends IGLTextureBinding> (binding: T, data?
     }
     else if (compressed)
     {
-        // void gl.compressedTexImage2D(target, level, internalformat, width, height, border, ArrayBufferView? pixels);
-
-        // https://developer.mozilla.org/en-US/docs/Web/API/WebGLRenderingContext/compressedTexImage2D
-
-        const formats = {
-            COMPRESSED_RGB_S3TC_DXT1_EXT: 33776,
-            COMPRESSED_RGBA_S3TC_DXT1_EXT: 33777,
-            COMPRESSED_RGBA_S3TC_DXT3_EXT: 33778,
-            COMPRESSED_RGBA_S3TC_DXT5_EXT: 33779,
-
-            COMPRESSED_SRGB_ALPHA_S3TC_DXT1_EXT: 35917,
-            COMPRESSED_SRGB_ALPHA_S3TC_DXT3_EXT: 35918,
-            COMPRESSED_SRGB_ALPHA_S3TC_DXT5_EXT: 35919,
-            COMPRESSED_SRGB_S3TC_DXT1_EXT: 35916,
-
-            COMPRESSED_RGB_PVRTC_4BPPV1_IMG: 0x8C00,
-            COMPRESSED_RGB_PVRTC_2BPPV1_IMG: 0x8C01,
-            COMPRESSED_RGBA_PVRTC_4BPPV1_IMG: 0x8C02,
-            COMPRESSED_RGBA_PVRTC_2BPPV1_IMG: 0x8C03
-        };
-
-        gl.compressedTexImage2D(gl.TEXTURE_2D, 0, formats[ format ], width, height, 0, data);
+        gl.compressedTexImage2D(gl.TEXTURE_2D, 0, internalFormat, width, height, 0, data);
 
         //  If you don't set minFilter to LINEAR then the compressed textures don't work!
-        minFilter = gl.LINEAR;
-        generateMipmap = false;
     }
     else
     {
