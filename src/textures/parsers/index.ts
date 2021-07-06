b34daec00bd3c5c959be511ba26ba069601e9a30
Richard Davey <rich@photonstorm.com>
2021-07-06 17:25:17 +0100

    Expose new parsers

--- src/textures/parsers/index.ts
+++ src/textures/parsers/index.ts
@@ -1,9 +1,13 @@
 import { AtlasParser } from './AtlasParser';
 import { BitmapTextParser } from './BitmapTextParser';
+import { KTXParser } from './KTXParser';
+import { PVRParser } from './PVRParser';
 import { SpriteSheetParser } from './SpriteSheetParser';
 
 export {
     AtlasParser,
     BitmapTextParser,
+    KTXParser,
+    PVRParser,
     SpriteSheetParser
 };
