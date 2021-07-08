9157595a71f0dcf33a88c5061347c3ea2dbb8415
Richard Davey <rich@photonstorm.com>
2021-07-08 20:24:26 +0100

    Fixed all calls, added setRGB, fixed box alignment

--- src/gameobjects/directdraw/DirectDraw.ts
+++ src/gameobjects/directdraw/DirectDraw.ts
@@ -15,65 +15,125 @@ export class DirectDraw extends GameObject
 {
     readonly type: string = 'DirectDraw';
 
+    //  In the 0 to 1 range
+    red: number = 1;
+    green: number = 1;
+    blue: number = 1;
     alpha: number = 1;
 
+    smoothness: number = 8;
+
     private renderPass: IRenderPass;
+    private _color: number;
 
     constructor ()
     {
         super();
     }
 
-    circle (x: number, y: number, radius: number, color: number, alpha: number = 1): this
+    set color (value: number)
     {
-        FillArc(this.renderPass, x, y, radius, 0, 0, false, color, alpha);
+        if (value !== undefined && value !== this._color)
+        {
+            this.red = (value >> 16 & 0xFF) / 255;
+            this.green = (value >> 8 & 0xFF) / 255;
+            this.blue = (value & 0xFF) / 255;
+
+            this._color = value;
+        }
+    }
+
+    //  Values in the range 0 to 255
+    setRGB (red: number, green: number, blue: number, alpha: number = 1): this
+    {
+        this.red = red / 255;
+        this.green = green / 255;
+        this.blue = blue / 255;
+        this.alpha = alpha;
 
         return this;
     }
 
-    plot (x: number, y: number, color: number, alpha: number = 1): this
+    arc (x: number, y: number, radius: number, startAngle: number = 0, endAngle: number = 6.283185307179586, anticlockwise: boolean = false, includeCenter: boolean = false, color?: number): this
     {
-        FillRect(this.renderPass, x, y, 1, 1, color, this.alpha * alpha);
+        this.color = color;
+
+        FillArc(
+            this.renderPass,
+            x, y, radius,
+            startAngle, endAngle, this.smoothness,
+            anticlockwise, includeCenter,
+            this.red, this.green, this.blue, this.alpha
+        );
 
         return this;
     }
 
-    box (x: number, y: number, width: number, height: number, color: number, thickness: number = 1, alpha: number = 1): this
+    circle (x: number, y: number, radius: number, color?: number): this
     {
-        //   TODO - The box should be centered on x/y/w/h if thickness > 1
+        this.color = color;
 
-        //  top
-        this.line(x, y, x + width, y, thickness, color, alpha);
-
-        //  bottom
-        this.line(x, y + height - thickness, x + width, y + height - thickness, thickness, color, alpha);
-
-        //  left
-        this.line(x + thickness, y + thickness, x, y + height - thickness - thickness, thickness, color, alpha);
-
-        // right
-        this.line(x + width - thickness, y + thickness, x + width, y + height - thickness - thickness, thickness, color, alpha);
+        FillArc(
+            this.renderPass,
+            x, y, radius,
+            0, Math.PI * 2, this.smoothness,
+            false, false,
+            this.red, this.green, this.blue, this.alpha
+        );
 
         return this;
     }
 
-    rect (x: number, y: number, width: number, height: number, color: number, alpha: number = 1): this
+    plot (x: number, y: number, color?: number): this
     {
-        FillRect(this.renderPass, x, y, width, height, color, this.alpha * alpha);
+        this.color = color;
+
+        FillRect(this.renderPass, x, y, 1, 1, this.red, this.green, this.blue, this.alpha);
 
         return this;
     }
 
-    triangle (x1: number, y1: number, x2: number, y2: number, x3: number, y3: number, color: number, alpha: number = 1): this
+    //  Draws a box (like rect but un-filled) from x/y to x+width,y+height
+    //  If the line thickness is > 1 it is positioned _within_ the bounds of the box, not outside or overlapping it
+    box (x: number, y: number, width: number, height: number, thickness: number = 1, color?: number): this
     {
-        FillTriangle(this.renderPass, x1, y1, x2, y2, x3, y3, color, this.alpha * alpha);
+        this.color = color;
+
+        const tw = thickness * 0.5;
+
+        //  top, bottom, left, right
+
+        this.line(x, y + tw, x + width, y + tw, thickness);
+        this.line(x, y + height - tw, x + width, y + height - tw, thickness);
+        this.line(x + tw, y + thickness, x + tw, y + height - thickness, thickness);
+        this.line(x + width - tw, y + thickness, x + width - tw, y + height - thickness, thickness);
 
         return this;
     }
 
-    line (x0: number, y0: number, x1: number, y1: number, width: number, color: number, alpha: number = 1): this
+    rect (x: number, y: number, width: number, height: number, color?: number): this
     {
-        FillLine(this.renderPass, x0, y0, x1, y1, width, color, this.alpha * alpha);
+        this.color = color;
+
+        FillRect(this.renderPass, x, y, width, height, this.red, this.green, this.blue, this.alpha);
+
+        return this;
+    }
+
+    triangle (x1: number, y1: number, x2: number, y2: number, x3: number, y3: number, color?: number): this
+    {
+        this.color = color;
+
+        FillTriangle(this.renderPass, x1, y1, x2, y2, x3, y3, this.red, this.green, this.blue, this.alpha);
+
+        return this;
+    }
+
+    line (x1: number, y1: number, x2: number, y2: number, width: number, color?: number): this
+    {
+        this.color = color;
+
+        FillLine(this.renderPass, x1, y1, x2, y2, width, this.red, this.green, this.blue, this.alpha);
 
         return this;
     }
