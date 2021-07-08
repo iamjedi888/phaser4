f2451bd53ca322d0d66b2037754cb1252896063d
Richard Davey <rich@photonstorm.com>
2021-07-08 20:23:49 +0100

    Create index.ts

---
+++ src/renderer/webgl1/draw/index.ts
@@ -0,0 +1,37 @@
+import { AddVertexToBatch } from './AddVertexToBatch';
+import { BatchQuad } from './BatchQuad';
+import { BatchSingleQuad } from './BatchSingleQuad';
+import { BatchTexturedQuad } from './BatchTexturedQuad';
+import { BatchTexturedQuadBuffer } from './BatchTexturedQuadBuffer';
+import { BatchTriangle } from './BatchTriangle';
+import { DrawFrame } from './DrawFrame';
+import { DrawImage } from './DrawImage';
+import { DrawImagePart } from './DrawImagePart';
+import { DrawQuad } from './DrawQuad';
+import { DrawTexturedQuad } from './DrawTexturedQuad';
+import { FillArc } from './FillArc';
+import { FillLine } from './FillLine';
+import { FillRect } from './FillRect';
+import { FillTriangle } from './FillTriangle';
+import { GetQuadBuffer } from './GetQuadBuffer';
+import { GetTriBuffer } from './GetTriBuffer';
+
+export {
+    AddVertexToBatch,
+    BatchSingleQuad,
+    BatchTexturedQuad,
+    BatchTexturedQuadBuffer,
+    BatchQuad,
+    BatchTriangle,
+    DrawFrame,
+    DrawImage,
+    DrawImagePart,
+    DrawQuad,
+    DrawTexturedQuad,
+    FillArc,
+    FillLine,
+    FillRect,
+    FillTriangle,
+    GetQuadBuffer,
+    GetTriBuffer
+};
