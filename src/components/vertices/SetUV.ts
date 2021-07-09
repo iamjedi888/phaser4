2e438a998234c26bb715f4a01db3315071d2dc3d
Richard Davey <rich@photonstorm.com>
2021-07-09 14:40:42 +0100

    Update to new format

--- src/components/vertices/SetUV.ts
+++ src/components/vertices/SetUV.ts
@@ -1,7 +1,24 @@
-import { VertexComponent } from './VertexComponent';
+import { QuadVertexComponent } from './QuadVertexComponent';
 
-export function SetUV (id: number, u: number, v: number): void
+export function SetUV (id: number, u0: number, v0: number,u1: number, v1: number): void
 {
-    VertexComponent.u[id] = u;
-    VertexComponent.v[id] = v;
+    const data = QuadVertexComponent.values[id];
+
+    data[2] = u0;
+    data[3] = v0;
+
+    data[11] = u0;
+    data[12] = v1;
+
+    data[20] = u1;
+    data[21] = v1;
+
+    data[29] = u0;
+    data[30] = v0;
+
+    data[38] = u1;
+    data[39] = v1;
+
+    data[47] = u1;
+    data[48] = v0;
 }
