4e707fa0cc13c024f633c0c26317a359bf3c9dc6
Richard Davey <rich@photonstorm.com>
2021-07-09 01:43:42 +0100

    Fixed for new batch

--- src/renderer/webgl1/draw/DrawQuad.ts
+++ src/renderer/webgl1/draw/DrawQuad.ts
@@ -1,48 +1,24 @@
+import { BatchTexturedQuad } from './BatchTexturedQuad';
 import { GetVertexBufferEntry } from '../renderpass/GetVertexBufferEntry';
 import { IFrame } from '../../../textures/IFrame';
 import { IRenderPass } from '../renderpass/IRenderPass';
 import { ITexture } from '../../../textures/ITexture';
-import { PackColor } from '../colors/PackColor';
 
-export function DrawQuad (renderPass: IRenderPass, texture: ITexture, frame: string | number | IFrame, x0: number, y0: number, x1: number, y1: number, x2: number, y2: number, x3: number, y3: number, alpha: number = 1): void
+export function DrawQuad <T extends ITexture> (renderPass: IRenderPass, texture: T, frame: string | number | IFrame, x0: number, y0: number, x1: number, y1: number, x2: number, y2: number, x3: number, y3: number, alpha: number = 1): void
 {
-    const { F32, U32, offset } = GetVertexBufferEntry(renderPass, 1);
-
-    const packedColor = PackColor(0xffffff, alpha);
+    const { F32, offset } = GetVertexBufferEntry(renderPass, 2);
 
     frame = texture.getFrame(frame);
 
     const textureIndex = renderPass.textures.set(texture);
 
-    //  top left
-    F32[offset + 0] = x0;
-    F32[offset + 1] = y0;
-    F32[offset + 2] = frame.u0;
-    F32[offset + 3] = frame.v0;
-    F32[offset + 4] = textureIndex;
-    U32[offset + 5] = packedColor;
-
-    //  bottom left
-    F32[offset + 6] = x1;
-    F32[offset + 7] = y1;
-    F32[offset + 8] = frame.u0;
-    F32[offset + 9] = frame.v1;
-    F32[offset + 10] = textureIndex;
-    U32[offset + 11] = packedColor;
-
-    //  bottom right
-    F32[offset + 12] = x2;
-    F32[offset + 13] = y2;
-    F32[offset + 14] = frame.u1;
-    F32[offset + 15] = frame.v1;
-    F32[offset + 16] = textureIndex;
-    U32[offset + 17] = packedColor;
-
-    //  top right
-    F32[offset + 18] = x3;
-    F32[offset + 19] = y3;
-    F32[offset + 20] = frame.u1;
-    F32[offset + 21] = frame.v0;
-    F32[offset + 22] = textureIndex;
-    U32[offset + 23] = packedColor;
+    BatchTexturedQuad(
+        F32, offset, textureIndex,
+        x0, y0,
+        x1, y1,
+        x2, y2,
+        x3, y3,
+        frame.u0, frame.v0, frame.u1, frame.v1,
+        1, 1, 1, alpha
+    );
 }
