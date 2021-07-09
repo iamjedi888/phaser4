19a73978aac892943cb2f69140c78c968e5dab8c
Richard Davey <rich@photonstorm.com>
2021-07-09 01:44:12 +0100

    Recoded

--- src/renderer/webgl1/draw/BatchTexturedQuad.ts
+++ src/renderer/webgl1/draw/BatchTexturedQuad.ts
@@ -1,49 +1,85 @@
-import { AddVertexToBatch } from './AddVertexToBatch';
-import { GetVertexBufferEntry } from '../renderpass/GetVertexBufferEntry';
-import { IRenderPass } from '../renderpass/IRenderPass';
-import { ITexture } from '../../../textures/ITexture';
-import { QuadVertexComponent } from '../../../components/vertices/QuadVertexComponent';
+import { GetQuadBuffer } from './GetQuadBuffer';
 
-export function BatchTexturedQuad <T extends ITexture> (texture: T, id: number, renderPass: IRenderPass): void
+const buffer = GetQuadBuffer();
+
+export function BatchTexturedQuad (
+    F32: Float32Array,
+    offset: number,
+    textureIndex: number,
+    x1: number, y1: number,
+    x2: number, y2: number,
+    x3: number, y3: number,
+    x4: number, y4: number,
+    u0: number, v0: number, u1: number, v1: number,
+    r: number, g: number, b: number, a: number): number
 {
-    const textureIndex = renderPass.textures.set(texture);
+    //  top left
+    buffer[0] = x1;
+    buffer[1] = y1;
+    buffer[2] = u0;
+    buffer[3] = v0;
+    buffer[4] = textureIndex;
+    buffer[5] = r;
+    buffer[6] = g;
+    buffer[7] = b;
+    buffer[8] = a;
 
-    const { F32, U32, offset } = GetVertexBufferEntry(renderPass, 1);
+    //  bottom left
+    buffer[9] = x2;
+    buffer[10] = y2;
+    buffer[11] = u0;
+    buffer[12] = v1;
+    buffer[13] = textureIndex;
+    buffer[14] = r;
+    buffer[15] = g;
+    buffer[16] = b;
+    buffer[17] = a;
 
-    let vertOffset = AddVertexToBatch(QuadVertexComponent.tl[id], offset, textureIndex, F32, U32);
-    vertOffset = AddVertexToBatch(QuadVertexComponent.bl[id], vertOffset, textureIndex, F32, U32);
-    vertOffset = AddVertexToBatch(QuadVertexComponent.br[id], vertOffset, textureIndex, F32, U32);
-    AddVertexToBatch(QuadVertexComponent.tr[id], vertOffset, textureIndex, F32, U32);
+    //  bottom right
+    buffer[18] = x3;
+    buffer[19] = y3;
+    buffer[20] = u1;
+    buffer[21] = v1;
+    buffer[22] = textureIndex;
+    buffer[23] = r;
+    buffer[24] = g;
+    buffer[25] = b;
+    buffer[26] = a;
+
+    //  top left
+    buffer[27] = x1;
+    buffer[28] = y1;
+    buffer[29] = u0;
+    buffer[30] = v0;
+    buffer[31] = textureIndex;
+    buffer[32] = r;
+    buffer[33] = g;
+    buffer[34] = b;
+    buffer[35] = a;
+
+    //  bottom right
+    buffer[36] = x3;
+    buffer[37] = y3;
+    buffer[38] = u1;
+    buffer[39] = v1;
+    buffer[40] = textureIndex;
+    buffer[41] = r;
+    buffer[42] = g;
+    buffer[43] = b;
+    buffer[44] = a;
+
+    //  top right
+    buffer[45] = x4;
+    buffer[46] = y4;
+    buffer[47] = u1;
+    buffer[48] = v0;
+    buffer[49] = textureIndex;
+    buffer[50] = r;
+    buffer[51] = g;
+    buffer[52] = b;
+    buffer[53] = a;
+
+    F32.set(buffer, offset);
+
+    return offset + 54;
 }
-
-/*
-    vertexData array structure:
-
-    0 = topLeft.x
-    1 = topLeft.y
-    2 = frame.u0
-    3 = frame.v0
-    4 = textureIndex
-    5 = topLeft.packedColor
-
-    6 = bottomLeft.x
-    7 = bottomLeft.y
-    8 = frame.u0
-    9 = frame.v1
-    10 = textureIndex
-    11 = bottomLeft.packedColor
-
-    12 = bottomRight.x
-    13 = bottomRight.y
-    14 = frame.u1
-    15 = frame.v1
-    16 = textureIndex
-    17 = bottomRight.packedColor
-
-    18 = topRight.x
-    19 = topRight.y
-    20 = frame.u1
-    21 = frame.v0
-    22 = textureIndex
-    23 = topRight.packedColor
-*/
