88ab3b2a65e056c2680919f1a343c2c208e2be3d
Richard Davey <rich@photonstorm.com>
2021-07-13 02:41:41 +0100

    Removed all the Color Matrix code now handled by the stack

--- src/renderer/webgl1/shaders/MultiTextureQuadShader.ts
+++ src/renderer/webgl1/shaders/MultiTextureQuadShader.ts
@@ -1,5 +1,3 @@
-import { Color } from '../../../components/color/Color';
-import { Flush } from '../renderpass';
 import { GetMaxTextures } from '../../../config/maxtextures/GetMaxTextures';
 import { IRenderPass } from '../renderpass/IRenderPass';
 import { IShaderConfig } from './IShaderConfig';
@@ -8,17 +6,6 @@ import { QuadShader } from './QuadShader';
 
 export class MultiTextureQuadShader extends QuadShader
 {
-    private defaultColorMatrix = new Float32Array([
-        1, 0, 0, 0,
-        0, 1, 0, 0,
-        0, 0, 1, 0,
-        0, 0, 0, 1
-    ]);
-
-    private defaultColorOffset = new Float32Array(4);
-
-    private isDefaultColorMatrix: boolean = true;
-
     constructor (config: IShaderConfig = {})
     {
         config.fragmentShader = config?.fragmentShader || MULTI_QUAD_FRAG;
@@ -40,27 +27,5 @@ export class MultiTextureQuadShader extends QuadShader
         this.uniforms.set('uTexture', renderPass.textures.textureIndex);
 
         return super.bind(renderPass);
-    }
-
-    setColorMatrix (color: Color, renderPass: IRenderPass): void
-    {
-        if (color.useColorMatrix)
-        {
-            Flush(renderPass);
-
-            this.setUniform('uColorMatrix', color.colorMatrix);
-            this.setUniform('uColorOffset', color.colorOffset);
-
-            this.isDefaultColorMatrix = false;
-        }
-        else if (!this.isDefaultColorMatrix)
-        {
-            Flush(renderPass);
-
-            this.setUniform('uColorMatrix', this.defaultColorMatrix);
-            this.setUniform('uColorOffset', this.defaultColorOffset);
-
-            this.isDefaultColorMatrix = true;
-        }
     }
 }
