4730bcece04c520cfcf053ee7788db3b42a45fd5
Richard Davey <rich@photonstorm.com>
2021-07-13 12:57:16 +0100

    Expose all of the new functions

--- src/colormatrix/index.ts
+++ src/colormatrix/index.ts
@@ -1,15 +1,43 @@
 import { BlackWhite } from './BlackWhite';
 import { Brightness } from './Brightness';
+import { Brown } from './Brown';
+import { Contrast } from './Contrast';
+import { DefaultColorMatrix } from './DefaultColorMatrix';
 import { Desaturate } from './Desaturate';
+import { DesaturateLuminance } from './DesaturateLuminance';
 import { Grayscale } from './Grayscale';
 import { Hue } from './Hue';
+import { Kodachrome } from './Kodachrome';
+import { LSD } from './LSD';
+import { Negative } from './Negative';
+import { Night } from './Night';
+import { Polaroid } from './Polaroid';
 import { Saturate } from './Saturate';
+import { Sepia } from './Sepia';
+import { SetColorMatrix } from './SetColorMatrix';
+import { ShiftToBGR } from './ShiftToBGR';
+import { Technicolor } from './Technicolor';
+import { VintagePinhole } from './VintagePinhole';
 
 export {
     BlackWhite,
     Brightness,
+    Brown,
+    Contrast,
+    DefaultColorMatrix,
     Desaturate,
+    DesaturateLuminance,
     Grayscale,
     Hue,
-    Saturate
+    Kodachrome,
+    LSD,
+    Negative,
+    Night,
+    Polaroid,
+    Saturate,
+    Sepia,
+    SetColorMatrix,
+    ShiftToBGR,
+    Technicolor,
+    VintagePinhole
 };
