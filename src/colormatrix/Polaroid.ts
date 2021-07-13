cdaa2a537c3485c6a2e1da6bd44acf97f15c9124
Richard Davey <rich@photonstorm.com>
2021-07-13 12:56:22 +0100

    Create Polaroid.ts

---
+++ src/colormatrix/Polaroid.ts
@@ -0,0 +1,20 @@
+import { DEFAULT_COLOR_OFFSET } from './consts';
+import { IContainer } from '../gameobjects/container/IContainer';
+import { SetColorMatrix } from './SetColorMatrix';
+
+const values = [
+    1.438, -0.062, -0.062, 0,
+    -0.122, 1.378, -0.122, 0,
+    -0.016, -0.016, 1.483, 0,
+    0, 0, 0, 1
+];
+
+export function Polaroid <T extends IContainer> (gameObject: T, multiply: boolean = false): T
+{
+    if (SetColorMatrix(gameObject.id, values, DEFAULT_COLOR_OFFSET, multiply))
+    {
+        gameObject.color.colorMatrixEnabled = true;
+    }
+
+    return gameObject;
+}
