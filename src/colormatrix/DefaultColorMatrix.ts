a60a5a50a67dce3029f39b0471e54f71b106550b
Richard Davey <rich@photonstorm.com>
2021-07-13 12:56:11 +0100

    Create DefaultColorMatrix.ts

---
+++ src/colormatrix/DefaultColorMatrix.ts
@@ -0,0 +1,14 @@
+import { DEFAULT_COLOR_MATRIX, DEFAULT_COLOR_OFFSET } from './consts';
+
+import { IContainer } from '../gameobjects/container/IContainer';
+import { SetColorMatrix } from './SetColorMatrix';
+
+export function DefaultColorMatrix <T extends IContainer> (gameObject: T): T
+{
+    if (SetColorMatrix(gameObject.id, DEFAULT_COLOR_MATRIX, DEFAULT_COLOR_OFFSET, false))
+    {
+        gameObject.color.colorMatrixEnabled = false;
+    }
+
+    return gameObject;
+}
