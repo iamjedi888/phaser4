af1dd116a4658c9a25a7f1a5758a11fb92903fb5
Richard Davey <rich@photonstorm.com>
2021-07-13 12:56:08 +0100

    Create Brown.ts

---
+++ src/colormatrix/Brown.ts
@@ -0,0 +1,26 @@
+import { IContainer } from '../gameobjects/container/IContainer';
+import { SetColorMatrix } from './SetColorMatrix';
+
+const values = [
+    0.5997023498159715, 0.34553243048391263, -0.2708298674538042, 0,
+    -0.037703249837783157, 0.8609577587992641, 0.15059552388459913, 0,
+    0.24113635128153335, -0.07441037908422492, 0.44972182064877153, 0,
+    0, 0, 0, 1
+];
+
+const offsets = [
+    47.43192855600873,
+    -36.96841498319127,
+    -7.562075277591283,
+    0
+];
+
+export function Brown <T extends IContainer> (gameObject: T, multiply: boolean = false): T
+{
+    if (SetColorMatrix(gameObject.id, values, offsets, multiply))
+    {
+        gameObject.color.colorMatrixEnabled = true;
+    }
+
+    return gameObject;
+}
