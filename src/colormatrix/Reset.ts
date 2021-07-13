0f4950c14096c90162d304ce05ec1cf995908ee7
Richard Davey <rich@photonstorm.com>
2021-07-13 16:27:54 +0100

    Renamed to Reset

--- src/colormatrix/Reset.ts
+++ src/colormatrix/Reset.ts
@@ -3,7 +3,7 @@ import { DEFAULT_COLOR_MATRIX, DEFAULT_COLOR_OFFSET } from './consts';
 import { IContainer } from '../gameobjects/container/IContainer';
 import { SetColorMatrix } from './SetColorMatrix';
 
-export function DefaultColorMatrix <T extends IContainer> (gameObject: T): T
+export function Reset <T extends IContainer> (gameObject: T): T
 {
     if (SetColorMatrix(gameObject.id, DEFAULT_COLOR_MATRIX, DEFAULT_COLOR_OFFSET, false))
     {
