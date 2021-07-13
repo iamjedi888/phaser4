071d869bbbedbc700955d542cdb52ce18102eb72
Richard Davey <rich@photonstorm.com>
2021-07-13 02:38:37 +0100

    Delete ColorMatrixComponent.ts

--- src/color/colormatrix/ColorMatrixComponent.ts
+++
@@ -1,8 +0,0 @@
-import { Types, defineComponent } from 'bitecs';
-
-const ColorMatrix = defineComponent({
-    alpha: Types.f32,
-    values: [ Types.f32, 20 ]
-});
-
-export const ColorMatrixComponent = ColorMatrix;
