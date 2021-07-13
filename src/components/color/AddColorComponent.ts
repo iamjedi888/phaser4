a5a89c0ea7cd3a17285d27db611b062d0d9923b6
Richard Davey <rich@photonstorm.com>
2021-07-13 02:41:01 +0100

    Set default color matrix (or multiply fails)

--- src/components/color/AddColorComponent.ts
+++ src/components/color/AddColorComponent.ts
@@ -6,8 +6,10 @@ export function AddColorComponent (id: number): void
 {
     addComponent(GameObjectWorld, ColorComponent, id);
 
-    ColorComponent.red[id] = 1;
-    ColorComponent.green[id] = 1;
-    ColorComponent.blue[id] = 1;
+    ColorComponent.red[id] = 255;
+    ColorComponent.green[id] = 255;
+    ColorComponent.blue[id] = 255;
     ColorComponent.alpha[id] = 1;
+
+    ColorComponent.colorMatrix[id].set([ 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1 ]);
 }
