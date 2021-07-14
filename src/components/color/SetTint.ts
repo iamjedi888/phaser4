0928e9f5fa992aee38273f638b4b331d6e798a43
Richard Davey <rich@photonstorm.com>
2021-07-14 17:01:09 +0100

    Update SetTint.ts

--- src/components/color/SetTint.ts
+++ src/components/color/SetTint.ts
@@ -3,7 +3,7 @@ import { SetDirtyParents } from '../dirty';
 
 export function SetTint (id: number, value: number): void
 {
-    ColorComponent.tint[id] = value;
+    // ColorComponent.tint[id] = value;
 
     SetDirtyParents(id);
 }
