60a46867c1f43aa8f6ea8599f587131ff85dbe24
Richard Davey <rich@photonstorm.com>
2021-07-15 18:24:37 +0100

    Removed 2 parameters

--- src/scenes/ResetRenderStats.ts
+++ src/scenes/ResetRenderStats.ts
@@ -1,15 +1,15 @@
 import { RenderStatsComponent } from './RenderStatsComponent';
 
-export function ResetRenderStats (id: number, gameFrame: number, scenes: number, worlds: number, transforms: number): void
+export function ResetRenderStats (id: number, gameFrame: number, scenes: number): void
 {
     RenderStatsComponent.gameFrame[id] = gameFrame;
     RenderStatsComponent.numScenes[id] = scenes;
-    RenderStatsComponent.numWorlds[id] = worlds;
+    RenderStatsComponent.numWorlds[id] = 0;
     RenderStatsComponent.numGameObjects[id] = 0;
     RenderStatsComponent.numGameObjectsRendered[id] = 0;
     RenderStatsComponent.numDirtyWorldLists[id] = 0;
     RenderStatsComponent.numDirtyVertices[id] = 0;
-    RenderStatsComponent.numDirtyLocalTransforms[id] = transforms;
+    RenderStatsComponent.numDirtyLocalTransforms[id] = 0;
     RenderStatsComponent.numDirtyWorldTransforms[id] = 0;
     RenderStatsComponent.numDirtyCameras[id] = 0;
 }
