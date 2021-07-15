dd05a03660630473a17ba99aa25eac62e416d678
Richard Davey <rich@photonstorm.com>
2021-07-15 18:23:27 +0100

    Allow zero sized bounds

--- src/components/bounds/BoundsIntersects.ts
+++ src/components/bounds/BoundsIntersects.ts
@@ -2,6 +2,7 @@ import { BoundsComponent } from './BoundsComponent';
 import { GameObjectWorld } from '../../GameObjectWorld';
 import { hasComponent } from 'bitecs';
 
+//  TODO - Pass in 2 ids and run the check against the two components
 export function BoundsIntersects (id: number, x: number, y: number, width: number, height: number): boolean
 {
     if (!hasComponent(GameObjectWorld, BoundsComponent, id))
@@ -9,18 +10,12 @@ export function BoundsIntersects (id: number, x: number, y: number, width: numbe
         return true;
     }
 
-    const bw = BoundsComponent.width[id];
-    const bh = BoundsComponent.height[id];
+    const [ bx, by, bw, bh, br, bb ] = BoundsComponent.global[id];
 
-    if (width <= 0 || height <= 0 || bw <= 0 || bh <= 0)
-    {
-        return false;
-    }
-
-    const bx = BoundsComponent.x[id];
-    const by = BoundsComponent.y[id];
-    const br = BoundsComponent.right[id];
-    const bb = BoundsComponent.bottom[id];
+    // const bx = BoundsComponent.x[id];
+    // const by = BoundsComponent.y[id];
+    // const br = BoundsComponent.right[id];
+    // const bb = BoundsComponent.bottom[id];
 
     const right = x + width;
     const bottom = y + height;
