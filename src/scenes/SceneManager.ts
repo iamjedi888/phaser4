26925eb05e228b32290bcb7b8efe9e6970618516
Richard Davey <rich@photonstorm.com>
2021-07-15 13:49:56 +0100

    Moving query management to the Worlds

--- src/scenes/SceneManager.ts
+++ src/scenes/SceneManager.ts
@@ -51,9 +51,6 @@ export class SceneManager
 
     update (delta: number, time: number, gameFrame: number): void
     {
-        let sceneTotal = 0;
-        let worldTotal = 0;
-
         for (const scene of this.scenes.values())
         {
             const worlds = WorldList.get(scene);
@@ -61,26 +58,31 @@ export class SceneManager
             for (const world of worlds)
             {
                 world.beforeUpdate(delta, time);
-                world.update(delta, time);
-                world.afterUpdate(delta, time);
-
-                worldTotal++;
             }
 
-            sceneTotal++;
+            for (const world of worlds)
+            {
+                world.update(delta, time);
+            }
+
+            for (const world of worlds)
+            {
+                world.afterUpdate(delta, time);
+            }
         }
 
-        //  Process all dirty 2D transforms and update the local matrix across all Worlds and Scenes
-        const localTransforms = UpdateLocalTransform2DSystem(GameObjectWorld);
-
-        ResetRenderStats(this.id, gameFrame, sceneTotal, worldTotal, localTransforms.length);
+        //  TODO - Move to start of function, remove sceneTotal var, get the worlds to update the stats directly
+        // ResetRenderStats(this.id, gameFrame, sceneTotal, worldTotal, 0);
+        ResetRenderStats(this.id, gameFrame, 1, 1, 0);
     }
 
     //  Run through all Scenes and Worlds within them, telling them to prepare to render
     //  The renderer itself tells them to actually render
     preRender (gameFrame: number): void
     {
-        const dirtyTransforms = this.changedMatrixQuery(GameObjectWorld);
+        //  TODO - This can almost certainly be avoided, as we know what changed
+        //  in the UpdateLocalTransform2DSystem, then we don't need to pass it to the world
+        // const dirtyTransforms = this.changedMatrixQuery(GameObjectWorld);
 
         let dirtyWorld = false;
 
@@ -90,24 +92,14 @@ export class SceneManager
 
             for (const world of worlds)
             {
-                if (world.preRender(gameFrame, dirtyTransforms))
+                if (world.preRender(gameFrame))
                 {
                     dirtyWorld = true;
                 }
             }
         }
 
-        //  Repack any changed vertex colors
         PackQuadColorsSystem(GameObjectWorld);
-
-        //  Update all vertices across the whole game, ready for rendering
-
-        //  TODO - This Query needs to run on a WORLD level, so same query + World Tag ID.
-        //  Then we can cull based on camera, user function, etc. because a GO in another World will
-        //  never influence the position of one in a different world.
-        // const updatedEntities = UpdateVertexPositionSystem(GameObjectWorld);
-
-        // RenderStatsComponent.numDirtyVertices[this.id] = updatedEntities.length * 4;
 
         if (dirtyWorld)
         {
