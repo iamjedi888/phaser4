728d98a0214f6d3a76114e3c06345e67c675cba6
Richard Davey <rich@photonstorm.com>
2021-07-15 18:23:42 +0100

    Removed unused properties

--- src/components/dirty/AddDirtyComponent.ts
+++ src/components/dirty/AddDirtyComponent.ts
@@ -6,14 +6,5 @@ export function AddDirtyComponent (id: number): void
 {
     addComponent(GameObjectWorld, DirtyComponent, id);
 
-    DirtyComponent.frame[id] = 0;
     DirtyComponent.transform[id] = 1;
-    DirtyComponent.update[id] = 1;
-    DirtyComponent.childCache[id] = 0;
-    DirtyComponent.postRender[id] = 0;
-    DirtyComponent.bounds[id] = 1;
-    DirtyComponent.texture[id] = 0;
-    DirtyComponent.textureFrame[id] = 0;
-    DirtyComponent.child[id] = 0;
-    DirtyComponent.displayList[id] = 0;
 }
--- src/components/dirty/DirtyComponent.ts
+++ src/components/dirty/DirtyComponent.ts
@@ -1,16 +1,10 @@
 import { Types, defineComponent } from 'bitecs';
 
 const Dirty = defineComponent({
-    frame: Types.ui32,
-    transform: Types.ui32,
-    update: Types.ui32,
-    childCache: Types.ui32,
-    postRender: Types.ui32,
-    bounds: Types.ui32,
-    texture: Types.ui32,
-    textureFrame: Types.ui32,
-    child: Types.ui32,
-    displayList: Types.ui32
+    child: Types.ui8,
+    childCache: Types.ui8,
+    displayList: Types.ui8,
+    transform: Types.ui8
 });
 
 export const DirtyComponent = Dirty;
