98cfd64b2aa74bbd315e249135146fb0b23be672
Richard Davey <rich@photonstorm.com>
2021-07-15 13:49:43 +0100

    Moving all queries to be local to the World and removing use of temp arrays

--- src/world/BaseWorld.ts
+++ src/world/BaseWorld.ts
@@ -4,7 +4,7 @@ import { Begin, Flush } from '../renderer/webgl1/renderpass';
 import { Changed, Query, defineComponent, defineQuery } from 'bitecs';
 import { ClearDirtyDisplayList, HasDirtyChildCache, HasDirtyDisplayList, SetDirtyParents } from '../components/dirty';
 import { Emit, Once } from '../events';
-import { Extent2DComponent, WorldMatrix2DComponent } from '../components/transform';
+import { Extent2DComponent, Transform2DComponent, UpdateLocalTransform2DSystem, WorldMatrix2DComponent } from '../components/transform';
 import { GameObject, GameObjectCache } from '../gameobjects';
 
 import { AddRenderDataComponent } from './AddRenderDataComponent';
@@ -19,6 +19,7 @@ import { IGameObject } from '../gameobjects/IGameObject';
 import { IRenderPass } from '../renderer/webgl1/renderpass/IRenderPass';
 import { IScene } from '../scenes/IScene';
 import { Mat2dEquals } from '../math/mat2d/Mat2dEquals';
+import { PopColor } from '../renderer/webgl1/renderpass/PopColor';
 import { RebuildWorldList } from './RebuildWorldList';
 import { RebuildWorldTransforms } from './RebuildWorldTransforms';
 import { RemoveChildren } from '../display';
@@ -26,6 +27,7 @@ import { ResetWorldRenderData } from './ResetWorldRenderData';
 import { SceneDestroyEvent } from '../scenes/events';
 import { SceneManager } from '../scenes/SceneManager';
 import { SceneManagerInstance } from '../scenes/SceneManagerInstance';
+import { SetColor } from '../renderer/webgl1/renderpass/SetColor';
 import { SetWorldID } from '../components/hierarchy';
 import { UpdateVertexPositionSystem } from '../components/vertices/UpdateVertexPositionSystem';
 import { WillUpdate } from '../components/permissions';
@@ -54,6 +56,7 @@ export class BaseWorld extends GameObject implements IBaseWorld
 
     private totalChildren: number = 0;
     private totalChildrenQuery: Query;
+    private dirtyLocalQuery: Query;
     private dirtyWorldQuery: Query;
 
     private vertexPositionQuery: Query;
@@ -66,6 +69,7 @@ export class BaseWorld extends GameObject implements IBaseWorld
         this.sceneManager = SceneManagerInstance.get();
 
         this.totalChildrenQuery = defineQuery([ this.tag ]);
+        this.dirtyLocalQuery = defineQuery([ this.tag, Changed(Transform2DComponent) ]);
         this.dirtyWorldQuery = defineQuery([ this.tag, Changed(WorldMatrix2DComponent) ]);
         this.vertexPositionQuery = defineQuery([ this.tag, Changed(WorldMatrix2DComponent), Changed(Extent2DComponent) ]);
 
@@ -105,6 +109,11 @@ export class BaseWorld extends GameObject implements IBaseWorld
     afterUpdate (delta: number, time: number): void
     {
         Emit(this, WorldEvents.WorldAfterUpdateEvent, delta, time, this);
+
+        //  Process all dirty 2D transforms and update their local matrix
+        UpdateLocalTransform2DSystem(GameObjectWorld, this.dirtyLocalQuery);
+
+        //  TODO - Return length from above function and add to RenderStats
     }
 
     //  Here we can check if the entity SHOULD be added to the render list, or not.
@@ -174,7 +183,7 @@ export class BaseWorld extends GameObject implements IBaseWorld
         return output;
     }
 
-    preRender (gameFrame: number, transformList: number[]): boolean
+    preRender (gameFrame: number): boolean
     {
         const sceneManager = this.sceneManager;
 
@@ -197,10 +206,13 @@ export class BaseWorld extends GameObject implements IBaseWorld
 
         //  Run the World Transform Update first, then Vertex Positions, finally the Render List
 
-        if (dirtyDisplayList || CheckDirtyTransforms(id, transformList))
+        //  TODO - We can SetDirtyTransform on this World just once
+        //  during the UpdateLocalTransform function
+
+        if (dirtyDisplayList || CheckDirtyTransforms(id))
         {
             //  TODO - This should only run over the branches that are dirty, not the whole World
-            RebuildWorldTransforms(this, id, transformList, false);
+            RebuildWorldTransforms(this, id, false);
 
             isDirty = true;
         }
@@ -231,7 +243,7 @@ export class BaseWorld extends GameObject implements IBaseWorld
         }
 
         //  TODO - Send total verts to RenderStats component
-        // RenderStatsComponent.numDirtyVertices[this.id] = updatedEntities.length * 4;
+        // RenderStatsComponent.numDirtyVertices[sceneManager.id] = updatedEntities.length * 4;
 
         this.runRender = (this.listLength > 0);
 
@@ -254,12 +266,7 @@ export class BaseWorld extends GameObject implements IBaseWorld
 
     renderGL <T extends IRenderPass> (renderPass: T): void
     {
-        const color = this.color;
-
-        if (color.colorMatrixEnabled && color.willColorChildren)
-        {
-            renderPass.colorMatrix.set(color);
-        }
+        SetColor(renderPass, this.color);
 
         Emit(this, WorldEvents.WorldRenderEvent, this);
 
@@ -295,12 +302,7 @@ export class BaseWorld extends GameObject implements IBaseWorld
 
     postRenderGL <T extends IRenderPass> (renderPass: T): void
     {
-        const color = this.color;
-
-        if (color.colorMatrixEnabled && color.willColorChildren)
-        {
-            renderPass.colorMatrix.pop();
-        }
+        PopColor(renderPass, this.color);
 
         if (!this.runRender)
         {
