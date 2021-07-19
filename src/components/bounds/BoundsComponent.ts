1c25815c07cce7b157c6ba5647371e60edd373fd
Richard Davey <rich@photonstorm.com>
2021-07-19 17:45:50 +0100

    Just store x, y, right and bottom

--- src/components/bounds/BoundsComponent.ts
+++ src/components/bounds/BoundsComponent.ts
@@ -1,16 +1,16 @@
 import { Types, defineComponent } from 'bitecs';
 
 //  Array structure:
-//  x, y, width, height, right, bottom
+//  x, y, right, bottom
 
 //  local = bounds of Game Object in local space (calculated dynamically, on request)
 //  global = bounds of Game Object in global space (calcuated in UpdatedVertexPositionSystem)
 //  world = bounds of Game Object, factoring in all children, in global space (calculated in BaseWorld)
 
 const Bounds = defineComponent({
-    local: [ Types.f32, 6 ],
-    global: [ Types.f32, 6 ],
-    world: [ Types.f32, 6 ]
+    local: [ Types.f32, 4 ],
+    global: [ Types.f32, 4 ],
+    world: [ Types.f32, 4 ]
 });
 
 export const BoundsComponent = Bounds;
