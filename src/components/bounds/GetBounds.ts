d0cd320552651b64e4b7c4bb7c9759769e50d15d
Richard Davey <rich@photonstorm.com>
2021-07-19 17:46:06 +0100

    Create GetBounds.ts

---
+++ src/components/bounds/GetBounds.ts
@@ -0,0 +1,37 @@
+import { BoundsComponent } from './BoundsComponent';
+import { GameObjectTree } from '../../gameobjects';
+import { GetNumChildren } from '../hierarchy';
+
+export function GetBounds (id: number): number[]
+{
+    const children = GameObjectTree.get(id);
+
+    let [ x, y, right, bottom ] = BoundsComponent.global[id];
+
+    children.forEach(childID =>
+    {
+        const [ childX, childY, childRight, childBottom ] = (GetNumChildren(childID) > 0) ? GetBounds(childID) : BoundsComponent.global[childID];
+
+        if (childX < x)
+        {
+            x = childX;
+        }
+
+        if (childY < y)
+        {
+            y = childY;
+        }
+
+        if (childRight > right)
+        {
+            right = childRight;
+        }
+
+        if (childBottom > bottom)
+        {
+            bottom = childBottom;
+        }
+    });
+
+    return [ x, y, right, bottom ];
+}
