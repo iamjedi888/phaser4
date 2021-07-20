134c663ec45a7010ead914181868e0ac998848b6
Richard Davey <rich@photonstorm.com>
2021-07-20 11:56:19 +0100

    Update HierarchyComponent.ts

--- src/components/hierarchy/HierarchyComponent.ts
+++ src/components/hierarchy/HierarchyComponent.ts
@@ -3,9 +3,9 @@ import { Types, defineComponent } from 'bitecs';
 //  worldID = The ID of the World entity the owner of this component belongs to
 //  parentID = The ID of the Parent entity. If it has no parent, will match the worldID
 //  numChildren = The number of direct descendants this entity has
-//  worldDepth = The distance of the entity from the world root (up to a maximum of 65535)
-//  depth = Reserved to allow for per-child depth sorting outside of the display list
+//  depth = Reserved to allow for per-child depth sorting outside of the display list index
 //  index = The position of this entity compared to its siblings, i.e. index 0 is at the back, index 1 above that, etc
+//  worldDepth = The distance of the entity from the world root (up to a maximum depth of 65535)
 
 const Hierarchy = defineComponent({
     worldID: Types.ui32,
