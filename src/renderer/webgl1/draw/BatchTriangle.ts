8473358ed97d7101af29bda5b4ec1ac30488807e
Richard Davey <rich@photonstorm.com>
2021-07-23 18:12:32 +0100

    Smaller set

--- src/renderer/webgl1/draw/BatchTriangle.ts
+++ src/renderer/webgl1/draw/BatchTriangle.ts
@@ -1,7 +1,3 @@
-import { GetTriBuffer } from './GetTriBuffer';
-
-const buffer = GetTriBuffer();
-
 export function BatchTriangle (
     F32: Float32Array,
     offset: number,
@@ -11,31 +7,37 @@ export function BatchTriangle (
     x3: number, y3: number,
     r: number, g: number, b: number, a: number): number
 {
-    buffer[0] = x1;
-    buffer[1] = y1;
-    buffer[4] = textureIndex;
-    buffer[5] = r;
-    buffer[6] = g;
-    buffer[7] = b;
-    buffer[8] = a;
+    F32.set([
+        x1,
+        y1,
+        0,
+        0,
+        textureIndex,
+        r,
+        g,
+        b,
+        a,
 
-    buffer[9] = x2;
-    buffer[10] = y2;
-    buffer[13] = textureIndex;
-    buffer[14] = r;
-    buffer[15] = g;
-    buffer[16] = b;
-    buffer[17] = a;
+        x2,
+        y2,
+        0,
+        1,
+        textureIndex,
+        r,
+        g,
+        b,
+        a,
 
-    buffer[18] = x3;
-    buffer[19] = y3;
-    buffer[22] = textureIndex;
-    buffer[23] = r;
-    buffer[24] = g;
-    buffer[25] = b;
-    buffer[26] = a;
-
-    F32.set(buffer, offset);
+        x3,
+        y3,
+        1,
+        1,
+        textureIndex,
+        r,
+        g,
+        b,
+        a
+    ], offset);
 
     return offset + 27;
 }
