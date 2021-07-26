6ba46a2883f5d8ab9d904b9f4cd3ea459a7b76b0
Richard Davey <rich@photonstorm.com>
2021-07-26 17:52:10 +0100

    Update Matrix4.ts

--- src/math/mat4/Matrix4.ts
+++ src/math/mat4/Matrix4.ts
@@ -1,24 +1,26 @@
+import { Types, defineComponent } from 'bitecs';
+
 import { NOOP } from '../../utils/NOOP';
 
 //  4x4 Matrix in column-major format
 
 /**
- * data[0] = m00
- * data[1] = m01
- * data[2] = m02
- * data[3] = m03
- * data[4] = m10
- * data[5] = m11
- * data[6] = m12
- * data[7] = m13
- * data[8] = m20
- * data[9] = m21
- * data[10] = m22
- * data[11] = m23
- * data[12] = m30
- * data[13] = m31
- * data[14] = m32
- * data[15] = m33
+ * [0] = m00 - 1st value of 1st row
+ * [1] = m01 - 2nd value of 1st row
+ * [2] = m02 - 3rd value of 1st row
+ * [3] = m03 - 4th value of 1st row
+ * [4] = m10 - 1st value of 2nd row
+ * [5] = m11 - 2nd value of 2nd row
+ * [6] = m12 - 3rd value of 2nd row
+ * [7] = m13 - 4th value of 2nd row
+ * [8] = m20 - 1st value of 3rd row
+ * [9] = m21 - 2nd value of 3rd row
+ * [10] = m22 - 3rd value of 3rd row
+ * [11] = m23 - 4th value of 3rd row
+ * [12] = m30 - 1st value of 4th row
+ * [13] = m31 - 2nd value of 4th row
+ * [14] = m32 - 3rd value of 4th row
+ * [15] = m33 - 4th value of 4th row
  */
 
 export class Matrix4
@@ -54,24 +56,6 @@ export class Matrix4
         }
     }
 
-    /**
-     * @param m00 - 1st value of 1st row
-     * @param m01 - 2nd value of 1st row
-     * @param m02 - 3rd value of 1st row
-     * @param m03 - 4th value of 1st row
-     * @param m10 - 1st value of 2nd row
-     * @param m11 - 2nd value of 2nd row
-     * @param m12 - 3rd value of 2nd row
-     * @param m13 - 4th value of 2nd row
-     * @param m20 - 1st value of 3rd row
-     * @param m21 - 2nd value of 3rd row
-     * @param m22 - 3rd value of 3rd row
-     * @param m23 - 4th value of 3rd row
-     * @param m30 - 1st value of 4th row
-     * @param m31 - 2nd value of 4th row
-     * @param m32 - 3rd value of 4th row
-     * @param m33 - 4th value of 4th row
-     */
     set (m00: number, m01: number, m02: number, m03: number, m10: number, m11: number, m12: number, m13: number, m20: number, m21: number, m22: number, m23: number, m30: number, m31: number, m32: number, m33: number): this
     {
         this.data.set([
