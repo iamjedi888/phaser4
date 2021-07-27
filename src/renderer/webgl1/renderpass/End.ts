f4ffa12e2dd297b5643379e4bf1a12fc30350025
Richard Davey <rich@photonstorm.com>
2021-07-27 15:13:01 +0100

    WebGL End calls PackQuads

--- src/renderer/webgl1/renderpass/End.ts
+++ src/renderer/webgl1/renderpass/End.ts
@@ -1,7 +1,11 @@
 import { Flush } from './Flush';
+import { GameObjectWorld } from '../../../GameObjectWorld';
 import { IRenderPass } from './IRenderPass';
+import { PackQuadColorsSystem } from '../../../components/color';
 
 export function End (renderPass: IRenderPass): void
 {
+    PackQuadColorsSystem(GameObjectWorld);
+
     Flush(renderPass);
 }
