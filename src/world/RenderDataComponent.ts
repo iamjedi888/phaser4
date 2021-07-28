5b968fca439456849d01ef738353e6ce962341ca
Richard Davey <rich@photonstorm.com>
2021-07-28 18:23:23 +0100

    New data components

--- src/world/RenderDataComponent.ts
+++ src/world/RenderDataComponent.ts
@@ -2,7 +2,9 @@ import { Types, defineComponent } from 'bitecs';
 
 const RenderData = defineComponent({
     gameFrame: Types.ui32,
-    dirtyFrame: Types.ui32,
+    dirtyLocal: Types.ui32,
+    dirtyVertices: Types.ui32,
+    numChildren: Types.ui32,
     numRendered: Types.ui32,
     numRenderable: Types.ui32
 });
--- src/world/ResetWorldRenderData.ts
+++ src/world/ResetWorldRenderData.ts
@@ -3,7 +3,7 @@ import { RenderDataComponent } from './RenderDataComponent';
 export function ResetWorldRenderData (id: number, gameFrame: number): void
 {
     RenderDataComponent.gameFrame[id] = gameFrame;
-    RenderDataComponent.dirtyFrame[id] = 0;
+    RenderDataComponent.dirtyLocal[id] = 0;
+    RenderDataComponent.dirtyVertices[id] = 0;
     RenderDataComponent.numRendered[id] = 0;
-    RenderDataComponent.numRenderable[id] = 0;
 }
