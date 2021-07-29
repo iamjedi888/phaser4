e84b1eea84b22c55dada398fd42734d2e30c152e
Richard Davey <rich@photonstorm.com>
2021-07-29 18:35:04 +0100

    Fast path

--- src/components/bounds/BoundsIntersects.ts
+++ src/components/bounds/BoundsIntersects.ts
@@ -4,17 +4,13 @@ import { hasComponent } from 'bitecs';
 
 export function BoundsIntersects (id: number, x: number, y: number, right: number, bottom: number): boolean
 {
-    if (!hasComponent(GameObjectWorld, BoundsComponent, id))
+    if (hasComponent(GameObjectWorld, BoundsComponent, id))
     {
-        return true;
+        const [ bx, by, br, bb ] = BoundsComponent.global[id];
+
+        return !(right < bx || bottom < by || x > br || y > bb);
     }
 
-    const global = BoundsComponent.global[id];
-
-    const bx = global[0];
-    const by = global[1];
-    const br = global[2];
-    const bb = global[3];
-
-    return !(right < bx || bottom < by || x > br || y > bb);
+    //  No bounds component? Always return true then
+    return true;
 }
