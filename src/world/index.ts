dada4a5613dfab9f78930c848139ae227f66a178
Richard Davey <rich@photonstorm.com>
2021-07-29 12:25:53 +0100

    Expose new modules

--- src/world/index.ts
+++ src/world/index.ts
@@ -1,8 +1,11 @@
 import * as Events from './events';
 
 import { AddRenderDataComponent } from './AddRenderDataComponent';
+import { AddToRenderList } from './AddToRenderList';
 import { BaseWorld } from './BaseWorld';
 import { CheckDirtyTransforms } from './CheckDirtyTransforms';
+import { FlatWorld } from './FlatWorld';
+import { GetRenderList } from './GetRenderList';
 import { RebuildWorldList } from './RebuildWorldList';
 import { RebuildWorldTransforms } from './RebuildWorldTransforms';
 import { RenderDataComponent } from './RenderDataComponent';
@@ -13,8 +16,11 @@ import { WorldList } from './WorldList';
 
 export {
     AddRenderDataComponent,
+    AddToRenderList,
     BaseWorld,
     CheckDirtyTransforms,
+    FlatWorld,
+    GetRenderList,
     Events,
     RebuildWorldList,
     RebuildWorldTransforms,
