e9085ced0201bbc17e171206b098bef3e435d000
Richard Davey <rich@photonstorm.com>
2021-07-29 12:24:51 +0100

    Support renderType 2

--- src/world/AddToRenderList.ts
+++ src/world/AddToRenderList.ts
@@ -7,10 +7,30 @@ import { IBaseWorld } from './IBaseWorld';
 
 //  0 = render
 //  1 = postRender
+//  2 = render + immediate postRender
 export function AddToRenderList (world: IBaseWorld, id: number, renderType: number): void
 {
     let len: number = world.listLength;
     const list = world.renderList;
+
+    if (len > 0)
+    {
+        //  Get the previous entity in the list, if this is the same one
+        //  we can merge it into a renderType 2.
+        //  This cuts down on the list size and iterations.
+
+        const prevEntity = list[len - 1];
+        const prevType = list[len];
+
+        if (prevEntity === id && prevType === 0 && renderType === 1)
+        {
+            //  Update the previous entry to use renderType 2
+            list[len - 1] = 2;
+
+            //  We didn't change the list size, so we can just return
+            return;
+        }
+    }
 
     list[len] = id;
     list[len + 1] = renderType;
@@ -19,9 +39,9 @@ export function AddToRenderList (world: IBaseWorld, id: number, renderType: numb
 
     len += 2;
 
-    //  Resize the render list
     if (len === list.length)
     {
+        //  Resize the render list
         const newList = new Uint32Array(len + (GetWorldSize() * 4));
 
         newList.set(list, 0);
