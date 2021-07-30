50704fe35b76718057f104b336ac11d9417bedf5
Richard Davey <rich@photonstorm.com>
2021-07-30 14:50:17 +0100

    Pass in skipCache value

--- src/loader/CreateFile.ts
+++ src/loader/CreateFile.ts
@@ -1,10 +1,10 @@
 import { IFile } from './IFile';
 
-export function CreateFile (key: string, url: string): IFile
+export function CreateFile (key: string, url: string, skipCache: boolean = false): IFile
 {
     return {
         key,
         url,
-        skipCache: false
+        skipCache
     };
 }
