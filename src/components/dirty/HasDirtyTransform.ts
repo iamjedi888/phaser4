ba4e99e980ef225c8b3ed15219b8679b6d2baad4
Richard Davey <rich@photonstorm.com>
2021-08-02 16:43:09 +0100

    Use Dirty Component

--- src/components/dirty/HasDirtyTransform.ts
+++ src/components/dirty/HasDirtyTransform.ts
@@ -1,12 +1,6 @@
 import { DirtyComponent } from './DirtyComponent';
-import { GameInstance } from '../../GameInstance';
-import { Transform2DComponent } from '../transform';
 
 export function HasDirtyTransform (id: number): boolean
 {
-    // return Boolean(Transform2DComponent.dirty[id] === GameInstance.getFrame());
-
-    // return Boolean(DirtyComponent.transform[id]);
-
-    return true;
+    return Boolean(DirtyComponent.transform[id]);
 }
