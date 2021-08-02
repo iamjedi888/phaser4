009aca3f3c8abecf1c3b5eb4daf421e7488977f5
Richard Davey <rich@photonstorm.com>
2021-08-02 16:43:17 +0100

    Set Dirty Transform too

--- src/components/dirty/SetDirtyParents.ts
+++ src/components/dirty/SetDirtyParents.ts
@@ -1,5 +1,6 @@
 import { GetParents } from '../hierarchy/GetParents';
 import { SetDirtyChildCache } from './SetDirtyChildCache';
+import { SetDirtyTransform } from './SetDirtyTransform';
 import { WillCacheChildren } from '../permissions/WillCacheChildren';
 
 export function SetDirtyParents (childID: number): void
@@ -10,6 +11,7 @@ export function SetDirtyParents (childID: number): void
     {
         if (WillCacheChildren(id))
         {
+            SetDirtyTransform(id);
             SetDirtyChildCache(id);
         }
     });
