e4a93aaacca21a576dcd00fe3623063dd3deeb5b
Richard Davey <rich@photonstorm.com>
2021-08-02 23:18:34 +0100

    Updated docs

--- src/gameobjects/layer/Layer.ts
+++ src/gameobjects/layer/Layer.ts
@@ -2,8 +2,8 @@ import { GameObject } from '../GameObject';
 import { SetWillTransformChildren } from '../../components/permissions/SetWillTransformChildren';
 
 //  A Layer is a way of grouping Game Objects together, without impacting their
-//  transforms. Children of a Layer do not inherit the layers transform, however
-//  you are able to control the visibility of the layers children in a single pass.
+//  transforms. Children of a Layer do not inherit the layers transform (as it
+//  doesn't have one), however you are able to control the visibility of the layers children in a single pass.
 
 export class Layer extends GameObject
 {
