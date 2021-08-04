ed74634a6b1be4b50aa5c97b8e2b841157cf299d
Richard Davey <rich@photonstorm.com>
2021-08-04 01:18:11 +0100

    Added comments

--- src/components/vertices/SetQuadPosition.ts
+++ src/components/vertices/SetQuadPosition.ts
@@ -4,21 +4,27 @@ export function SetQuadPosition (id: number, x0: number, y0: number, x1: number,
 {
     const data = QuadVertexComponent.values[id];
 
+    //  top left
     data[0] = x0;
     data[1] = y0;
 
+    //  bottom left
     data[9] = x1;
     data[10] = y1;
 
+    //  bottom right
     data[18] = x2;
     data[19] = y2;
 
+    //  top left
     data[27] = x0;
     data[28] = y0;
 
+    //  bottom right
     data[36] = x2;
     data[37] = y2;
 
+    //  top right
     data[45] = x3;
     data[46] = y3;
 }
