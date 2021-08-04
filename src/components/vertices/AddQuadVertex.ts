68bc52d29f6a0daf44203c14dfe9df5f645bbcd9
Richard Davey <rich@photonstorm.com>
2021-08-04 01:16:27 +0100

    Set default quad values

--- src/components/vertices/AddQuadVertex.ts
+++ src/components/vertices/AddQuadVertex.ts
@@ -1,25 +1,18 @@
 import { GameObjectWorld } from '../../GameObjectWorld';
 import { QuadVertexComponent } from './QuadVertexComponent';
+import { SetQuadColor } from './SetQuadColor';
+import { SetQuadPosition } from './SetQuadPosition';
+import { SetUV } from './SetUV';
 import { addComponent } from 'bitecs';
 
 export function AddQuadVertex (id: number, width: number = 0, height: number = 0, flipY: boolean = true): void
 {
     addComponent(GameObjectWorld, QuadVertexComponent, id);
 
-    /*
     if (width || height)
     {
-        QuadVertexComponent.tl[id] = AddVertex(0, 0, 0, 0, 1);
-        QuadVertexComponent.bl[id] = AddVertex(0, height, 0, 0, 0);
-        QuadVertexComponent.br[id] = AddVertex(width, height, 0, 1, 0);
-        QuadVertexComponent.tr[id] = AddVertex(width, 0, 0, 1, 1);
+        SetQuadColor(id, 1, 1, 1, 1);
+        SetQuadPosition(id, 0, 0, 0, height, width, height, width, 0);
+        SetUV(id, 0, 0, 1, 1);
     }
-    else
-    {
-        QuadVertexComponent.tl[id] = AddVertex();
-        QuadVertexComponent.bl[id] = AddVertex();
-        QuadVertexComponent.br[id] = AddVertex();
-        QuadVertexComponent.tr[id] = AddVertex();
-    }
-    */
 }
