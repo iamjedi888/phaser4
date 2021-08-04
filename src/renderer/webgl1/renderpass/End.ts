b3fb56d0c34f53cb73031fb4418e04ee546efdf2
Richard Davey <rich@photonstorm.com>
2021-08-04 01:17:35 +0100

    Removed color system

--- src/renderer/webgl1/renderpass/End.ts
+++ src/renderer/webgl1/renderpass/End.ts
@@ -1,11 +1,7 @@
 import { Flush } from './Flush';
-import { GameObjectWorld } from '../../../GameObjectWorld';
 import { IRenderPass } from './IRenderPass';
-import { PackQuadColorsSystem } from '../../../components/color';
 
 export function End (renderPass: IRenderPass): void
 {
-    PackQuadColorsSystem(GameObjectWorld);
-
     Flush(renderPass);
 }
