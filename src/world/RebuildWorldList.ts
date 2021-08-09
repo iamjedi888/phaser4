bee5bb21f4c56e1a864f8c54384d612807f60843
Richard Davey <rich@photonstorm.com>
2021-08-09 18:28:10 +0100

    Make use of new HasRenderableChildren function

--- src/world/RebuildWorldList.ts
+++ src/world/RebuildWorldList.ts
@@ -2,6 +2,7 @@ import { AddToRenderList } from './AddToRenderList';
 import { GameObjectTree } from '../gameobjects/GameObjectTree';
 import { GetNumChildren } from '../components/hierarchy/GetNumChildren';
 import { HasDirtyChildCache } from '../components/dirty/HasDirtyChildCache';
+import { HasRenderableChildren } from '../components/permissions';
 import { IBaseWorld } from './IBaseWorld';
 import { SetWorldDepth } from '../components/hierarchy/SetWorldDepth';
 import { WillCacheChildren } from '../components/permissions/WillCacheChildren';
@@ -16,58 +17,34 @@ import { WillRenderChildren } from '../components/permissions/WillRenderChildren
 //  This will rebuild the render list for the whole world by calling AddToRenderList
 //  for all game objects that will render (not invisible, etc)
 
-export function RebuildWorldList (world: IBaseWorld, parent: number, worldDepth: number): void
+export function RebuildWorldList (world: IBaseWorld, parent: number): void
 {
     if (WillRender(parent))
     {
-        if (world.id !== parent)
+        if (HasRenderableChildren(parent))
         {
-            AddToRenderList(world, parent, 0);
-        }
-
-        const children = GameObjectTree.get(parent);
-
-        for (let i = 0; i < children.length; i++)
-        {
-            const nodeID = children[i];
-
-            if (WillRender(nodeID))
+            if (parent !== world.id)
             {
-                if (GetNumChildren(nodeID) > 0 && WillRenderChildren(nodeID))
+                AddToRenderList(world, parent, 0);
+            }
+
+            const children = GameObjectTree.get(parent);
+
+            for (let i = 0; i < children.length; i++)
+            {
+                const childID = children[i];
+
+                if (WillRender(childID))
                 {
-                    //  A Container *won't* cache children, but a RenderLayer *will* cache children
-                    if (!WillCacheChildren(nodeID) || (WillCacheChildren(nodeID) && HasDirtyChildCache(nodeID)))
-                    {
-                        SetWorldDepth(nodeID, worldDepth);
-
-                        RebuildWorldList(world, nodeID, worldDepth + 1);
-                    }
-                     // else
-                    // {
-                    //     SetWorldDepth(nodeID, worldDepth);
-
-                    //     AddToRenderList(world, nodeID, 0);
-                    //     AddToRenderList(world, nodeID, 1);
-                    // }
-                }
-                else if (!WillCacheChildren(nodeID))
-                {
-                    SetWorldDepth(nodeID, worldDepth);
-
-                    AddToRenderList(world, nodeID, 0);
-                    AddToRenderList(world, nodeID, 1);
+                    RebuildWorldList(world, childID);
                 }
             }
-        }
 
-        if (children.length === 0)
-        {
-            SetWorldDepth(parent, worldDepth);
-        }
-
-        if (world.id !== parent)
-        {
             AddToRenderList(world, parent, 1);
+        }
+        else if (parent !== world.id)
+        {
+            AddToRenderList(world, parent, 2);
         }
     }
 }
