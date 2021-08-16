602f2372034d9ee5ebf276b1bce8c20873dd34c2
Richard Davey <rich@photonstorm.com>
2021-08-16 18:28:29 +0100

    Update DebugHierarchyComponent.ts

--- src/components/hierarchy/DebugHierarchyComponent.ts
+++ src/components/hierarchy/DebugHierarchyComponent.ts
@@ -5,13 +5,15 @@ export function DebugHierarchyComponent (id: number): void
     const hc = HierarchyComponent;
 
     console.group(`Entity ID: ${id}`);
-    console.log(`world: ${hc.world[id]}`);
-    console.log(`parent: ${hc.parent[id]}`);
-    console.log(`index: ${hc.index[id]}`);
-    console.log(`next: ${hc.next[id]}`);
-    console.log(`prev: ${hc.prev[id]}`);
-    console.log(`first: ${hc.first[id]}`);
-    console.log(`last: ${hc.last[id]}`);
-    console.log(`numChildren: ${hc.numChildren[id]}`);
+    console.log(`index: ${hc.index[id]} - parent: ${hc.parent[id]} - world: ${hc.world[id]}`);
+    console.log(`> next: ${hc.next[id]}      < prev: ${hc.prev[id]}`);
+
+    if (hc.numChildren[id] > 0)
+    {
+        console.log(`first: ${hc.first[id]}`);
+        console.log(`last: ${hc.last[id]}`);
+        console.log(`numChildren: ${hc.numChildren[id]}`);
+    }
+
     console.groupEnd();
 }
