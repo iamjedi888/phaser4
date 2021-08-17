530d42a0b4323a7e9aacd9f729537a18b5af15b5
Richard Davey <rich@photonstorm.com>
2021-08-17 17:29:57 +0100

    Testing bounds properties

--- src/components/bounds/BoundsComponent.ts
+++ src/components/bounds/BoundsComponent.ts
@@ -1,16 +1,19 @@
 import { Types, defineComponent } from 'bitecs';
 
-//  Array structure:
+//  local and world Array structure:
 //  x, y, right, bottom
 
+//  The direct properties are the global bounds values, calculated in UpdateVertexPositionSystem
+
 //  local = bounds of Game Object in local space (calculated dynamically, on request)
-//  global = bounds of Game Object in global space (calcuated in UpdatedVertexPositionSystem)
 //  world = bounds of Game Object, factoring in all children, in global space (calculated in BaseWorld)
 
-const Bounds = defineComponent({
+export const BoundsComponent = defineComponent({
+    x: Types.f32,
+    y: Types.f32,
+    right: Types.f32,
+    bottom: Types.f32,
     local: [ Types.f32, 4 ],
     global: [ Types.f32, 4 ],
     world: [ Types.f32, 4 ]
 });
-
-export const BoundsComponent = Bounds;
--- src/components/bounds/BoundsIntersects.ts
+++ src/components/bounds/BoundsIntersects.ts
@@ -6,7 +6,12 @@ export function BoundsIntersects (id: number, x: number, y: number, right: numbe
 {
     if (hasComponent(GameObjectWorld, BoundsComponent, id))
     {
-        const [ bx, by, br, bb ] = BoundsComponent.global[id];
+        const bounds = BoundsComponent.global[id];
+
+        const bx = bounds[0];
+        const by = bounds[1];
+        const br = bounds[2];
+        const bb = bounds[3];
 
         return !(right < bx || bottom < by || x > br || y > bb);
     }
