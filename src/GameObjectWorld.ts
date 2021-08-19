c401ff94c5a2bf46ad00632e0a06c818ecba20df
Richard Davey <rich@photonstorm.com>
2021-08-19 02:13:13 +0100

    Bump up size

--- src/GameObjectWorld.ts
+++ src/GameObjectWorld.ts
@@ -2,8 +2,8 @@ import { createWorld, setDefaultSize } from 'bitecs';
 
 // setDefaultSize(100000);
 // setDefaultSize(150000);
-setDefaultSize(200000);
-// setDefaultSize(250000);
+// setDefaultSize(200000);
+setDefaultSize(250000);
 // setDefaultSize(550000);
 
 const world = createWorld();
