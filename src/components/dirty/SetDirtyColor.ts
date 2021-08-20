813e6a32e474f479be797f262921dbe15c443ea5
Richard Davey <rich@photonstorm.com>
2021-08-20 15:54:54 +0100

    Update SetDirtyColor.ts

--- src/components/dirty/SetDirtyColor.ts
+++ src/components/dirty/SetDirtyColor.ts
@@ -1,6 +1,14 @@
 import { DirtyComponent } from './DirtyComponent';
+import { GetWorldID } from '../hierarchy/GetWorldID';
 
 export function SetDirtyColor (id: number): void
 {
     DirtyComponent.color[id] = 1;
+
+    const world = GetWorldID(id);
+
+    if (world)
+    {
+        DirtyComponent.color[world] = 1;
+    }
 }
