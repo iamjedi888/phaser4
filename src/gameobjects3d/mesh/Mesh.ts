70cdce618410c31ff1a7f7ca5ad7c483fd2774b1
Richard Davey <rich@photonstorm.com>
2021-08-23 17:21:03 +0100

    Added 3d libs back

---
+++ src/gameobjects3d/mesh/Mesh.ts
@@ -0,0 +1,84 @@
+import { FlushBuffer } from '../../renderer/webgl1/renderpass';
+import { Frame } from '../../textures/Frame';
+import { GameObject3D } from '../GameObject3D';
+import { Geometry } from '../geometry/Geometry';
+import { IGameObject3D } from '../IGameObject3D';
+import { IRenderPass } from '../../renderer/webgl1/renderpass/IRenderPass';
+import { Material } from '../material/Material';
+import { SetTexture as RequestTexture } from '../../renderer/webgl1/renderpass/SetTexture';
+import { SetFrame } from './SetFrame';
+import { SetTexture } from './SetTexture';
+import { Texture } from '../../textures/Texture';
+
+export class Mesh extends GameObject3D
+{
+    texture: Texture;
+    frame: Frame;
+    hasTexture: boolean = false;
+
+    geometry: Geometry;
+    material: Material;
+
+    cullFaces: boolean = true;
+
+    constructor (x: number = 0, y: number = 0, z: number = 0, geometry?: Geometry, material: Material = new Material())
+    {
+        super(x, y, z);
+
+        this.geometry = geometry;
+        this.material = material;
+
+        this.setTexture('__WHITE');
+    }
+
+    setTexture (key: string | Texture, frame?: string | number): this
+    {
+        SetTexture(key, frame, this);
+
+        return this;
+    }
+
+    setFrame (key?: string | number | Frame): this
+    {
+        SetFrame(this.texture, key, this);
+
+        return this;
+    }
+
+    setMaterial (material: Material): this
+    {
+        this.material = material;
+
+        return this;
+    }
+
+    renderGL <T extends IRenderPass> (renderPass: T): void
+    {
+        const shader = renderPass.currentShader.shader;
+
+        shader.setUniform('uModelMatrix', this.transform.local.data);
+        shader.setUniform('uNormalMatrix', this.transform.normal.data);
+
+        if (this.hasTexture)
+        {
+            const textureIndex = RequestTexture(renderPass, this.texture);
+
+            shader.setUniform('uTexture', textureIndex);
+        }
+
+        this.material.setUniforms(shader);
+
+        FlushBuffer(renderPass, this.geometry.buffer);
+    }
+
+    destroy (reparentChildren?: IGameObject3D): void
+    {
+        super.destroy(reparentChildren);
+
+        this.geometry = null;
+        this.material = null;
+        this.texture = null;
+        this.frame = null;
+        this.hasTexture = false;
+    }
+}
---
+++ src/gameobjects3d/renderlayer3d/RenderLayer3D.ts
@@ -0,0 +1,81 @@
+import { Flush, PopFramebuffer, SetFramebuffer } from '../../renderer/webgl1/renderpass';
+import { GetHeight, GetResolution, GetWidth } from '../../config/size';
+
+import { CreateDepthBuffer } from '../../renderer/webgl1/fbo/CreateDepthBuffer';
+import { CreateFramebuffer } from '../../renderer/webgl1/fbo/CreateFramebuffer';
+import { DIRTY_CONST } from '../../gameobjects/DIRTY_CONST';
+import { DrawTexturedQuad } from '../../renderer/webgl1/draw/DrawTexturedQuad';
+import { GLTextureBinding } from '../../renderer/webgl1/textures/GLTextureBinding';
+import { IRenderLayer3D } from './IRenderLayer3D';
+import { IRenderPass } from '../../renderer/webgl1/renderpass/IRenderPass';
+import { Layer } from '../../gameobjects/layer/Layer';
+import { Texture } from '../../textures/Texture';
+
+export class RenderLayer3D extends Layer implements IRenderLayer3D
+{
+    texture: Texture;
+    framebuffer: WebGLFramebuffer;
+
+    constructor ()
+    {
+        super();
+
+        this.willRender = true;
+        this.willRenderChildren = true;
+        this.willCacheChildren = true;
+
+        this.setDirty(DIRTY_CONST.CHILD_CACHE);
+
+        const width = GetWidth();
+        const height = GetHeight();
+        const resolution = GetResolution();
+
+        //  TODO: Allow them to set this via a filterArea
+        //  TODO: This code is duplicate of Shader constructor, consolidate
+        const texture = new Texture(null, width * resolution, height * resolution);
+
+        const binding = new GLTextureBinding(texture);
+
+        texture.binding = binding;
+
+        binding.framebuffer = CreateFramebuffer(binding.texture);
+        binding.depthbuffer = CreateDepthBuffer(binding.framebuffer, texture.width, texture.height);
+
+        this.texture = texture;
+        this.framebuffer = binding.framebuffer;
+    }
+
+    renderGL <T extends IRenderPass> (renderPass: T): void
+    {
+        if (this.numChildren > 0)
+        {
+            Flush(renderPass);
+
+            if (!this.willCacheChildren || this.isDirty(DIRTY_CONST.CHILD_CACHE))
+            {
+                //  This RenderLayer has dirty children
+                SetFramebuffer(renderPass, this.framebuffer, true);
+
+                this.clearDirty(DIRTY_CONST.CHILD_CACHE);
+            }
+            else
+            {
+                //  This RenderLayer doesn't have any dirty children, so we'll use the previous fbo contents
+                SetFramebuffer(renderPass, this.framebuffer, false);
+
+                this.postRenderGL(renderPass);
+            }
+        }
+    }
+
+    postRenderGL <T extends IRenderPass> (renderPass: T): void
+    {
+        Flush(renderPass);
+
+        PopFramebuffer(renderPass);
+
+        DrawTexturedQuad(renderPass, this.texture);
+
+        this.clearDirty(DIRTY_CONST.TRANSFORM);
+    }
+}
