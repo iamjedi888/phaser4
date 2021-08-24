07c7be3f00af911bae9bde66ba6ed6147ddd10a1
Richard Davey <rich@photonstorm.com>
2021-08-24 12:10:48 +0100

    Use data ref

--- src/components/bounds/BoundsIntersects.ts
+++ src/components/bounds/BoundsIntersects.ts
@@ -2,10 +2,12 @@ import { TRANSFORM, Transform2DComponent } from '../transform/Transform2DCompone
 
 export function BoundsIntersects (id: number, x: number, y: number, right: number, bottom: number): boolean
 {
-    const bx = Transform2DComponent.data[id][TRANSFORM.BOUNDS_X1];
-    const by = Transform2DComponent.data[id][TRANSFORM.BOUNDS_Y1];
-    const br = Transform2DComponent.data[id][TRANSFORM.BOUNDS_X2];
-    const bb = Transform2DComponent.data[id][TRANSFORM.BOUNDS_Y2];
+    const data = Transform2DComponent.data[id];
+
+    const bx = data[TRANSFORM.BOUNDS_X1];
+    const by = data[TRANSFORM.BOUNDS_Y1];
+    const br = data[TRANSFORM.BOUNDS_X2];
+    const bb = data[TRANSFORM.BOUNDS_Y2];
 
     return !(right < bx || bottom < by || x > br || y > bb);
 }
