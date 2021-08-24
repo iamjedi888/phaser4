067e572f5e9e0e16ea0e2d25561d06093d5b2d09
Richard Davey <rich@photonstorm.com>
2021-08-24 12:11:06 +0100

    Trying stackRender

--- src/world/StaticWorld.ts
+++ src/world/StaticWorld.ts
@@ -12,14 +12,20 @@ import { ClearDirtyChildWorldTransform } from '../components/dirty/ClearDirtyChi
 import { ClearDirtyDisplayList } from '../components/dirty/ClearDirtyDisplayList';
 import { ColorComponent } from '../components/color/ColorComponent';
 import { Emit } from '../events/Emit';
+import { GameInstance } from '../GameInstance';
 import { GameObjectCache } from '../gameobjects/GameObjectCache';
 import { GameObjectWorld } from '../GameObjectWorld';
 import { GetFirstChildID } from '../components/hierarchy/GetFirstChildID';
+import { GetNextSiblingID } from '../components/hierarchy/GetNextSiblingID';
+import { GetParentID } from '../components/hierarchy/GetParentID';
+import { GetRenderFrame } from '../components/transform/GetRenderFrame';
 import { HasDirtyChildColor } from '../components/dirty/HasDirtyChildColor';
 import { HasDirtyChildTransform } from '../components/dirty/HasDirtyChildTransform';
 import { HasDirtyChildWorldTransform } from '../components/dirty/HasDirtyChildWorldTransform';
 import { HasDirtyDisplayList } from '../components/dirty/HasDirtyDisplayList';
+import { HasRenderableChildren } from '../components/permissions/HasRenderableChildren';
 import { IBaseCamera } from '../camera/IBaseCamera';
+import { IGameObject } from '../gameobjects/IGameObject';
 import { IRenderPass } from '../renderer/webgl1/renderpass/IRenderPass';
 import { IScene } from '../scenes/IScene';
 import { IStaticCamera } from '../camera/IStaticCamera';
@@ -31,12 +37,14 @@ import { RebuildWorldList } from './RebuildWorldList';
 import { RebuildWorldTransforms } from './RebuildWorldTransforms';
 import { RendererInstance } from '../renderer/RendererInstance';
 import { SetColor } from '../renderer/webgl1/renderpass/SetColor';
+import { SetRenderFrame } from '../components/transform/SetRenderFrame';
 import { SetWillCacheChildren } from '../components/permissions/SetWillCacheChildren';
 import { SetWillTransformChildren } from '../components/permissions/SetWillTransformChildren';
 import { Transform2DComponent } from '../components/transform/Transform2DComponent';
 import { UpdateLocalTransform } from '../components/transform/UpdateLocalTransform';
 import { UpdateQuadColorSystem } from '../components/color/UpdateQuadColorSystem';
 import { UpdateVertexPositionSystem } from '../components/vertices/UpdateVertexPositionSystem';
+import { WillRender } from '../components/permissions/WillRender';
 import { WillUpdate } from '../components/permissions/WillUpdate';
 import { WorldCamera } from '../camera/WorldCamera';
 
@@ -175,6 +183,96 @@ export class StaticWorld extends BaseWorld implements IStaticWorld
         Emit(this, WorldEvents.WorldUpdateEvent, this);
     }
 
+    stackRender <T extends IRenderPass, C extends IBaseCamera> (renderPass: T, camera: C): void
+    {
+        const x = camera.getBoundsX();
+        const y = camera.getBoundsY();
+        const right = camera.getBoundsRight();
+        const bottom = camera.getBoundsBottom();
+
+        let total = 0;
+
+        const gameFrame = GameInstance.getFrame();
+
+        const stack: IGameObject[] = [];
+
+        let gameObject: IGameObject;
+
+        const worldID = this.id;
+
+        const start = performance.now();
+
+        let id = GetFirstChildID(worldID);
+
+        while (id > 0)
+        {
+            const willRender = WillRender(id) && BoundsIntersects(id, x, y, right, bottom);
+            const hasRenderableChildren = willRender && HasRenderableChildren(id);
+
+            if (willRender)
+            {
+                gameObject = GameObjectCache.get(id);
+
+                gameObject.renderGL(renderPass);
+
+                if (hasRenderableChildren)
+                {
+                    stack.push(gameObject);
+                }
+                else
+                {
+                    gameObject.postRenderGL(renderPass);
+                }
+
+                total++;
+            }
+
+            if (hasRenderableChildren)
+            {
+                id = GetFirstChildID(id);
+            }
+            else
+            {
+                const sibling = GetNextSiblingID(id);
+
+                if (sibling)
+                {
+                    //  Move to the next sibling
+                    id = sibling;
+                }
+                else
+                {
+                    //  Move back up to the parent
+                    const parent = GetParentID(id);
+
+                    if (parent === worldID)
+                    {
+                        //  End of the line - pop the stack and quit
+                        gameObject = stack.pop();
+
+                        if (gameObject)
+                        {
+                            gameObject.postRenderGL(renderPass);
+                        }
+
+                        id = 0;
+                    }
+                    else
+                    {
+                        gameObject = stack.pop();
+
+                        gameObject.postRenderGL(renderPass);
+
+                        id = GetNextSiblingID(parent);
+                    }
+                }
+            }
+        }
+
+        this.renderData.rendered = total;
+        this.renderData.renderMs = performance.now() - start;
+    }
+
     render <T extends IRenderPass, C extends IBaseCamera> (renderPass: T, camera: C): void
     {
         const x = camera.getBoundsX();
@@ -227,7 +325,9 @@ export class StaticWorld extends BaseWorld implements IStaticWorld
 
         Begin(renderPass, camera);
 
-        this.render(renderPass, camera);
+        // this.render(renderPass, camera);
+
+        this.stackRender(renderPass, camera);
 
         PopColor(renderPass, this.color);
 
