b8c111fa95ab407bd8cb4086ab47d0e3e9cf0a78
Richard Davey <rich@photonstorm.com>
2021-08-24 16:23:24 +0100

    Removed render list and related functions

--- src/world/index.ts
+++ src/world/index.ts
@@ -1,22 +1,14 @@
 import * as Events from './events';
 
-import { AddToRenderList } from './AddToRenderList';
 import { BaseWorld } from './BaseWorld';
-import { CheckDirtyTransforms } from './CheckDirtyTransforms';
-import { GetRenderList } from './GetRenderList';
-import { RebuildWorldList } from './RebuildWorldList';
 import { RebuildWorldTransforms } from './RebuildWorldTransforms';
 import { StaticWorld } from './StaticWorld';
 import { World } from './World';
 import { WorldList } from './WorldList';
 
 export {
-    AddToRenderList,
     BaseWorld,
-    CheckDirtyTransforms,
-    GetRenderList,
     Events,
-    RebuildWorldList,
     RebuildWorldTransforms,
     StaticWorld,
     World,
