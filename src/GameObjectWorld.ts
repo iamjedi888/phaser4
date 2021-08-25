d63e6a5be4239a40432cdd39e889d1a1170af3d3
Richard Davey <rich@photonstorm.com>
2021-08-25 22:15:12 +0100

    Update GameObjectWorld.ts

--- src/GameObjectWorld.ts
+++ src/GameObjectWorld.ts
@@ -8,8 +8,8 @@ if (window['defaultSize'])
 else
 {
     // setDefaultSize(10000);
-    setDefaultSize(100000);
-    // setDefaultSize(150000);
+    // setDefaultSize(100000);
+    setDefaultSize(150000);
     // setDefaultSize(200000);
     // setDefaultSize(250000);
     // setDefaultSize(300000);
