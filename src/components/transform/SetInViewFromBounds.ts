b697a43c506976975526747b28f8c16a1439aac5
Richard Davey <rich@photonstorm.com>
2021-08-25 23:48:59 +0100

    Create SetInViewFromBounds.ts

---
+++ src/components/transform/SetInViewFromBounds.ts
@@ -0,0 +1,18 @@
+import { TRANSFORM, Transform2DComponent } from '../transform/Transform2DComponent';
+
+export function SetInViewFromBounds (id: number, gameFrame: number, cx: number, cy: number, cright: number, cbottom: number): void
+{
+    const data: Float32Array = Transform2DComponent.data[id];
+
+    if (data[TRANSFORM.UPDATED] < gameFrame)
+    {
+        const bx = data[TRANSFORM.BOUNDS_X1];
+        const by = data[TRANSFORM.BOUNDS_Y1];
+        const br = data[TRANSFORM.BOUNDS_X2];
+        const bb = data[TRANSFORM.BOUNDS_Y2];
+
+        data[TRANSFORM.IN_VIEW] = Number(!(cright < bx || cbottom < by || cx > br || cy > bb));
+
+        data[TRANSFORM.UPDATED] = gameFrame;
+    }
+}
