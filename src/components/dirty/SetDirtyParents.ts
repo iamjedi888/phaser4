3884cb84c391994b93930cb1250d6a37af12bdfe
Richard Davey <rich@photonstorm.com>
2021-08-25 18:40:08 +0100

    Invalidates World Transform

--- src/components/dirty/SetDirtyParents.ts
+++ src/components/dirty/SetDirtyParents.ts
@@ -1,12 +1,15 @@
 import { GetParents } from '../hierarchy/GetParents';
+import { GetWorldID } from '../hierarchy/GetWorldID';
 import { SetDirtyChild } from './SetDirtyChild';
 import { SetDirtyChildCache } from './SetDirtyChildCache';
+import { SetDirtyChildTransform } from './SetDirtyChildTransform';
 import { SetDirtyTransform } from './SetDirtyTransform';
 import { WillCacheChildren } from '../permissions/WillCacheChildren';
 import { WillTransformChildren } from '../permissions/WillTransformChildren';
 
 export function SetDirtyParents (childID: number): void
 {
+    const worldID = GetWorldID(childID);
     const parents = GetParents(childID);
 
     for (let i = 0; i < parents.length; i++)
@@ -23,6 +26,11 @@ export function SetDirtyParents (childID: number): void
         if (WillCacheChildren(id))
         {
             SetDirtyChildCache(id);
+        }
+
+        if (id === worldID)
+        {
+            SetDirtyChildTransform(id);
         }
     }
 }
