0b8ac841295082a249e19d0eaeecb62077f2accb
Richard Davey <rich@photonstorm.com>
2021-08-26 13:25:14 +0100

    Use data const

--- src/components/transform/SetExtent.ts
+++ src/components/transform/SetExtent.ts
@@ -6,12 +6,14 @@ import { SetDirtyTransform } from '../dirty/SetDirtyTransform';
 
 export function SetExtent (id: number, x: number, y: number, width: number, height: number): void
 {
-    Transform2DComponent.data[id][TRANSFORM.FRAME_X1] = x;
-    Transform2DComponent.data[id][TRANSFORM.FRAME_Y1] = y;
-    Transform2DComponent.data[id][TRANSFORM.FRAME_X2] = x + width;
-    Transform2DComponent.data[id][TRANSFORM.FRAME_Y2] = y + height;
-    Transform2DComponent.data[id][TRANSFORM.FRAME_WIDTH] = width;
-    Transform2DComponent.data[id][TRANSFORM.FRAME_HEIGHT] = height;
+    const data = Transform2DComponent.data[id];
+
+    data[TRANSFORM.FRAME_X1] = x;
+    data[TRANSFORM.FRAME_Y1] = y;
+    data[TRANSFORM.FRAME_X2] = x + width;
+    data[TRANSFORM.FRAME_Y2] = y + height;
+    data[TRANSFORM.FRAME_WIDTH] = width;
+    data[TRANSFORM.FRAME_HEIGHT] = height;
 
     SetDirtyTransform(id);
 }
