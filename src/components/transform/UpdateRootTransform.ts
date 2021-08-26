204e5e924bc584773ca5e944a6e3926ae168059a
Richard Davey <rich@photonstorm.com>
2021-08-26 01:57:50 +0100

    Ignore world

--- src/components/transform/UpdateRootTransform.ts
+++ src/components/transform/UpdateRootTransform.ts
@@ -1,15 +1,17 @@
 import { TRANSFORM, Transform2DComponent } from './Transform2DComponent';
 
 import { GetParentID } from '../hierarchy/GetParentID';
+import { GetWorldID } from '../hierarchy/GetWorldID';
 import { WillTransformChildren } from '../permissions/WillTransformChildren';
 
 export function UpdateRootTransform (id: number): void
 {
+    const worldID = GetWorldID(id);
     let currentParent = GetParentID(id);
 
     let isRootTransform = true;
 
-    while (currentParent)
+    while (currentParent && currentParent !== worldID)
     {
         if (WillTransformChildren(currentParent))
         {
