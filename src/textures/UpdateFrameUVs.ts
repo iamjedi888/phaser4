8354239863ff2e77f85b7212907badfd66e99840
Richard Davey <rich@photonstorm.com>
2021-08-26 13:24:36 +0100

    Create UpdateFrameUVs.ts

---
+++ src/textures/UpdateFrameUVs.ts
@@ -0,0 +1,17 @@
+import { IFrame } from './IFrame';
+
+export function UpdateFrameUVs <T extends IFrame> (frame: T): T
+{
+    const { x, y, width, height } = frame;
+
+    const baseTextureWidth = frame.texture.width;
+    const baseTextureHeight = frame.texture.height;
+
+    frame.u0 = x / baseTextureWidth;
+    frame.v0 = y / baseTextureHeight;
+
+    frame.u1 = (x + width) / baseTextureWidth;
+    frame.v1 = (y + height) / baseTextureHeight;
+
+    return frame;
+}
