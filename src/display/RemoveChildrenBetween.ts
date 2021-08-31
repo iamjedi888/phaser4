fa8aa27d678c3f33628e57471000cd896e7ec9a6
Richard Davey <rich@photonstorm.com>
2021-08-31 18:10:29 +0100

    Update RemoveChildrenBetween.ts

--- src/display/RemoveChildrenBetween.ts
+++ src/display/RemoveChildrenBetween.ts
@@ -1,23 +1,25 @@
 import { ClearWorldAndParentID } from '../components/hierarchy/ClearWorldAndParentID';
 import { GameObjectCache } from '../gameobjects/GameObjectCache';
+import { GetChildIDsFromParent } from '../components/hierarchy/GetChildIDsFromParent';
+import { GetNumChildren } from '../components/hierarchy/GetNumChildren';
 import { IGameObject } from '../gameobjects/IGameObject';
-import { UpdateChildIndexes } from '../components/hierarchy/UpdateChildIndexes';
+import { RelinkChildren } from '../components/hierarchy/RelinkChildren';
 
 export function RemoveChildrenBetween <P extends IGameObject> (parent: P, beginIndex: number = 0, endIndex?: number): IGameObject[]
 {
     const parentID = parent.id;
 
-    // const children = GameObjectTree.get(parentID);
-
     if (endIndex === undefined)
     {
-        endIndex = children.length;
+        endIndex = GetNumChildren(parentID);
     }
 
     const range = endIndex - beginIndex;
 
     if (range > 0 && range <= endIndex)
     {
+        const children = GetChildIDsFromParent(parent);
+
         const removed = children.splice(beginIndex, range);
 
         removed.forEach(childID =>
@@ -25,7 +27,7 @@ export function RemoveChildrenBetween <P extends IGameObject> (parent: P, beginI
             ClearWorldAndParentID(childID);
         });
 
-        UpdateChildIndexes(parentID);
+        RelinkChildren(parentID, children);
 
         return removed.map(id => GameObjectCache.get(id));
     }
