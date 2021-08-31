ae06b6f88ecb9c667ddca8387d21e1a97dfa1c87
Richard Davey <rich@photonstorm.com>
2021-08-31 17:44:17 +0100

    Create RemoveChildID.ts

---
+++ src/components/hierarchy/RemoveChildID.ts
@@ -0,0 +1,34 @@
+import { ClearSiblings } from './ClearSiblings';
+import { GetFirstChildID } from './GetFirstChildID';
+import { GetLastChildID } from './GetLastChildID';
+import { GetNextSiblingID } from './GetNextSiblingID';
+import { GetParentID } from './GetParentID';
+import { GetPreviousSiblingID } from './GetPreviousSiblingID';
+import { LinkSiblings } from './LinkSiblings';
+import { SetFirstChildID } from './SetFirstChildID';
+import { SetLastChildID } from './SetLastChildID';
+
+export function RemoveChildID (childID: number): void
+{
+    const parentID = GetParentID(childID);
+
+    const first = GetFirstChildID(parentID);
+    const last = GetLastChildID(parentID);
+
+    const prevID = GetPreviousSiblingID(childID);
+    const nextID = GetNextSiblingID(childID);
+
+    LinkSiblings(prevID, nextID);
+
+    if (first === childID)
+    {
+        SetFirstChildID(parentID, nextID);
+    }
+
+    if (last === childID)
+    {
+        SetLastChildID(parentID, prevID);
+    }
+
+    ClearSiblings(childID);
+}
