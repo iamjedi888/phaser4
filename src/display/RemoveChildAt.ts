f1f82d3207bd9d77cbf80e3bbd21f423082eb8c2
Richard Davey <rich@photonstorm.com>
2021-08-31 17:57:11 +0100

    Update RemoveChildAt.ts

--- src/display/RemoveChildAt.ts
+++ src/display/RemoveChildAt.ts
@@ -1,27 +1,10 @@
-import { ClearWorldAndParentID } from '../components/hierarchy/ClearWorldAndParentID';
-import { GameObjectCache } from '../gameobjects/GameObjectCache';
+import { GetChildAt } from './GetChildAt';
 import { IGameObject } from '../gameobjects/IGameObject';
-import { UpdateChildIndexes } from '../components/hierarchy/UpdateChildIndexes';
+import { RemoveChild } from './RemoveChild';
 
 export function RemoveChildAt <T extends IGameObject> (parent: T, index: number): IGameObject | undefined
 {
-    const parentID = parent.id;
+    const child = GetChildAt(parent, index);
 
-    // const children = GameObjectTree.get(parentID);
-
-    if (index >= 0 && index < children.length)
-    {
-        const removedID = children.splice(index, 1)[0];
-
-        if (removedID)
-        {
-            ClearWorldAndParentID(removedID);
-
-            UpdateChildIndexes(parentID);
-
-            //  Emit remove event?
-
-            return GameObjectCache.get(removedID);
-        }
-    }
+    return RemoveChild(parent, child);
 }
