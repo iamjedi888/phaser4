ca96a1d5c0a5e831bea53119257a38a6cd61f26b
Richard Davey <rich@photonstorm.com>
2021-08-31 17:44:12 +0100

    Create RelinkChildren.ts

---
+++ src/components/hierarchy/RelinkChildren.ts
@@ -0,0 +1,44 @@
+import { LinkSiblings } from './LinkSiblings';
+import { SetFirstChildID } from './SetFirstChildID';
+import { SetLastChildID } from './SetLastChildID';
+import { SetNumChildren } from './SetNumChildren';
+
+export function RelinkChildren (parentID: number, children: number[]): void
+{
+    const len = children.length;
+
+    if (len === 0)
+    {
+        return;
+    }
+
+    let total = 1;
+
+    let childA = children[0];
+
+    SetFirstChildID(parentID, childA);
+
+    if (len === 1)
+    {
+        SetLastChildID(parentID, childA);
+
+        SetNumChildren(parentID, total);
+
+        return;
+    }
+
+    for (let i = 1; i < len; i++)
+    {
+        const childB = children[i];
+
+        LinkSiblings(childA, childB);
+
+        childA = childB;
+
+        total++;
+    }
+
+    SetLastChildID(parentID, childA);
+
+    SetNumChildren(parentID, total);
+}
