443939930ed70f595adddab2b19676bf20e0b96a
Hardy <hardylr@users.noreply.github.com>
2021-09-01 11:04:23 +0200

    Update GetTriangleCircumCircle.ts
    Fix problem: WARNING in ./node_modules/@phaserjs/phaser/geom/circle/Circle.js
    There are multiple modules with names that only differ in casing.
    Updated casing of import folder to fix warning

--- src/geom/triangle/GetTriangleCircumCircle.ts
+++ src/geom/triangle/GetTriangleCircumCircle.ts
@@ -4,7 +4,7 @@
  * @license      {@link https://opensource.org/licenses/MIT|MIT License}
  */
 
-import { Circle } from '../Circle/Circle';
+import { Circle } from '../circle/Circle';
 import { ITriangle } from './ITriangle';
 
 /**
