8e7153a6295c45b3fd6a2da0d96e936e7f511c74
Richard Davey <rich@photonstorm.com>
2021-09-01 17:23:22 +0100

    Update ColorComponent.ts

--- src/components/color/ColorComponent.ts
+++ src/components/color/ColorComponent.ts
@@ -1,6 +1,6 @@
 import { Types, defineComponent } from 'bitecs';
 
-const Color = defineComponent({
+export const ColorComponent = defineComponent({
     r: Types.ui8c,
     g: Types.ui8c,
     b: Types.ui8c,
@@ -8,5 +8,3 @@ const Color = defineComponent({
     colorMatrix: [ Types.f32, 16 ],
     colorOffset: [ Types.f32, 4 ]
 });
-
-export const ColorComponent = Color;
