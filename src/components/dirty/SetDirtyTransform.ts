79b7a1ab3f11e694cebcd18a81c079bfa4e7b493
Richard Davey <rich@photonstorm.com>
2021-09-02 18:07:35 +0100

    Don't set parents dirty

--- src/components/dirty/SetDirtyTransform.ts
+++ src/components/dirty/SetDirtyTransform.ts
@@ -1,10 +1,16 @@
 import { TRANSFORM, Transform2DComponent } from '../transform/Transform2DComponent';
 
-import { SetDirtyParents } from './SetDirtyParents';
+import { GetWorldID } from '../hierarchy/GetWorldID';
+import { SetDirtyChildTransform } from './SetDirtyChildTransform';
 
 export function SetDirtyTransform (id: number): void
 {
     Transform2DComponent.data[id][TRANSFORM.DIRTY] = 1;
 
-    SetDirtyParents(id);
+    const worldID = GetWorldID(id);
+
+    if (worldID)
+    {
+        SetDirtyChildTransform(worldID);
+    }
 }
