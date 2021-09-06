cc047d7ff21db2cb526a817ca7096480dfaf454f
Richard Davey <rich@photonstorm.com>
2021-09-07 00:16:44 +0100

    Update GameObjectWorld.ts

--- src/GameObjectWorld.ts
+++ src/GameObjectWorld.ts
@@ -6,12 +6,13 @@ import { createWorld, setDefaultSize } from 'bitecs';
 // setDefaultSize(200000);
 // setDefaultSize(250000);
 // setDefaultSize(300000);
-setDefaultSize(350000);
+// setDefaultSize(350000);
 // setDefaultSize(400000);
-// setDefaultSize(500000);
+setDefaultSize(500000);
 // setDefaultSize(550000);
 // setDefaultSize(1000000);
 // setDefaultSize(2000000);
+// setDefaultSize(4000000);
 
 const world = createWorld();
 
