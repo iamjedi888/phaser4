0c222426c04097aace5b9ff4392470d0ced8ac0f
Richard Davey <rich@photonstorm.com>
2021-09-06 23:26:27 +0100

    Update view always

--- src/components/transform/SetInViewFromBounds.ts
+++ src/components/transform/SetInViewFromBounds.ts
@@ -4,15 +4,10 @@ export function SetInViewFromBounds (id: number, gameFrame: number, cx: number, 
 {
     const data: Float32Array = Transform2DComponent.data[id];
 
-    if (data[TRANSFORM.UPDATED] < gameFrame)
-    {
-        const bx = data[TRANSFORM.BOUNDS_X1];
-        const by = data[TRANSFORM.BOUNDS_Y1];
-        const br = data[TRANSFORM.BOUNDS_X2];
-        const bb = data[TRANSFORM.BOUNDS_Y2];
+    const bx = data[TRANSFORM.BOUNDS_X1];
+    const by = data[TRANSFORM.BOUNDS_Y1];
+    const br = data[TRANSFORM.BOUNDS_X2];
+    const bb = data[TRANSFORM.BOUNDS_Y2];
 
-        data[TRANSFORM.IN_VIEW] = Number(!(cright < bx || cbottom < by || cx > br || cy > bb));
-
-        data[TRANSFORM.UPDATED] = gameFrame;
-    }
+    data[TRANSFORM.IN_VIEW] = Number(!(cright < bx || cbottom < by || cx > br || cy > bb));
 }
