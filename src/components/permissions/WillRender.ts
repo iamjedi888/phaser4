d1a22c6165b9ffedd6d27ca736f4cbf981e9c598
Richard Davey <rich@photonstorm.com>
2021-09-07 17:25:44 +0100

    Update WillRender.ts

--- src/components/permissions/WillRender.ts
+++ src/components/permissions/WillRender.ts
@@ -2,5 +2,6 @@ import { PERMISSION, PermissionsComponent } from './PermissionsComponent';
 
 export function WillRender (id: number): boolean
 {
-    return Boolean(PermissionsComponent.data[id][PERMISSION.VISIBLE]) && Boolean(PermissionsComponent.data[id][PERMISSION.WILL_RENDER]);
+    return !!(PermissionsComponent.data[id][PERMISSION.VISIBLE]) &&
+           !!(PermissionsComponent.data[id][PERMISSION.WILL_RENDER]);
 }
