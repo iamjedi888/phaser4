df022e0060863cdcfa31ea7612ce9ec48c520126
Richard Davey <rich@photonstorm.com>
2021-09-07 17:25:38 +0100

    Call SetDirtyParents

--- src/components/permissions/SetVisibleChildren.ts
+++ src/components/permissions/SetVisibleChildren.ts
@@ -1,7 +1,5 @@
 import { PERMISSION, PermissionsComponent } from './PermissionsComponent';
 
-import { GetWorldID } from '../hierarchy/GetWorldID';
-import { SetDirtyDisplayList } from '../dirty/SetDirtyDisplayList';
 import { SetDirtyParents } from '../dirty/SetDirtyParents';
 
 export function SetVisibleChildren (id: number, value: boolean): void
@@ -9,5 +7,4 @@ export function SetVisibleChildren (id: number, value: boolean): void
     PermissionsComponent.data[id][PERMISSION.VISIBLE_CHILDREN] = Number(value);
 
     SetDirtyParents(id);
-    SetDirtyDisplayList(GetWorldID(id));
 }
