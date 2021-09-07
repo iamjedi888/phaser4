ab4a1d1c1f2b034db3a60a07f8a91876dc9309e7
Richard Davey <rich@photonstorm.com>
2021-09-07 17:24:24 +0100

    Don't need WillTransform

--- src/components/dirty/SetDirtyTransform.ts
+++ src/components/dirty/SetDirtyTransform.ts
@@ -1,17 +1,10 @@
 import { DIRTY, DirtyComponent } from './DirtyComponent';
-import { TRANSFORM, Transform2DComponent } from '../transform/Transform2DComponent';
 
 import { SetDirtyParents } from './SetDirtyParents';
-import { WillTransformChildren } from '../permissions/WillTransformChildren';
 
 export function SetDirtyTransform (id: number): void
 {
     DirtyComponent.data[id][DIRTY.TRANSFORM] = 1;
-
-    // if (WillTransformChildren(id))
-    // {
-    //     SetDirtyParentTransform(id);
-    // }
 
     SetDirtyParents(id);
 }
