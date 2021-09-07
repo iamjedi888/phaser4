ef6344e67445b2840363419fec81c0c275c28a18
Richard Davey <rich@photonstorm.com>
2021-09-07 17:23:27 +0100

    Expose new DirtyWorldTransform functions

--- src/components/dirty/index.ts
+++ src/components/dirty/index.ts
@@ -4,18 +4,21 @@ import { ClearDirtyChildColor } from './ClearDirtyChildColor';
 import { ClearDirtyChildTransform } from './ClearDirtyChildTransform';
 import { ClearDirtyColor } from './ClearDirtyColor';
 import { ClearDirtyTransform } from './ClearDirtyTransform';
+import { ClearDirtyWorldTransform } from './ClearDirtyWorldTransform';
 import { DirtyComponent } from './DirtyComponent';
 import { HasDirtyChildCache } from './HasDirtyChildCache';
 import { HasDirtyChildColor } from './HasDirtyChildColor';
 import { HasDirtyChildTransform } from './HasDirtyChildTransform';
 import { HasDirtyColor } from './HasDirtyColor';
 import { HasDirtyTransform } from './HasDirtyTransform';
+import { HasDirtyWorldTransform } from './HasDirtyWorldTransform';
 import { SetDirtyChildCache } from './SetDirtyChildCache';
 import { SetDirtyChildColor } from './SetDirtyChildColor';
 import { SetDirtyChildTransform } from './SetDirtyChildTransform';
 import { SetDirtyColor } from './SetDirtyColor';
 import { SetDirtyParents } from './SetDirtyParents';
 import { SetDirtyTransform } from './SetDirtyTransform';
+import { SetDirtyWorldTransform } from './SetDirtyWorldTransform';
 
 export {
     AddDirtyComponent,
@@ -24,16 +27,19 @@ export {
     ClearDirtyChildTransform,
     ClearDirtyColor,
     ClearDirtyTransform,
+    ClearDirtyWorldTransform,
     DirtyComponent,
     HasDirtyChildCache,
     HasDirtyChildColor,
     HasDirtyChildTransform,
     HasDirtyColor,
     HasDirtyTransform,
+    HasDirtyWorldTransform,
     SetDirtyChildCache,
     SetDirtyChildColor,
     SetDirtyChildTransform,
     SetDirtyColor,
     SetDirtyParents,
-    SetDirtyTransform
+    SetDirtyTransform,
+    SetDirtyWorldTransform
 };
