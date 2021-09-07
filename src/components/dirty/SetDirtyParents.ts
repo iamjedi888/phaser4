223055d186c4f53567167787e0363e543446ad02
Richard Davey <rich@photonstorm.com>
2021-09-07 17:24:07 +0100

    Always set dirty child (WillTransform only applies to dirty parents)

--- src/components/dirty/SetDirtyParents.ts
+++ src/components/dirty/SetDirtyParents.ts
@@ -2,23 +2,14 @@ import { GetParentID } from '../hierarchy/GetParentID';
 import { SetDirtyChildCache } from './SetDirtyChildCache';
 import { SetDirtyChildTransform } from './SetDirtyChildTransform';
 import { WillCacheChildren } from '../permissions/WillCacheChildren';
-import { WillTransformChildren } from '../permissions/WillTransformChildren';
 
 export function SetDirtyParents (childID: number): void
 {
-    // if (WillTransformChildren(childID))
-    // {
-    //     SetDirtyChildTransform(childID);
-    // }
-
     let id = GetParentID(childID);
 
     while (id)
     {
-        if (WillTransformChildren(id))
-        {
-            SetDirtyChildTransform(id);
-        }
+        SetDirtyChildTransform(id);
 
         if (WillCacheChildren(id))
         {
