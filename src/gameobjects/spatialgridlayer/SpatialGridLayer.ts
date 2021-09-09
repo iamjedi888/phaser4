80aa2198fdff2db9e5aff063a10b32d0212ceac0
Richard Davey <rich@photonstorm.com>
2021-09-09 17:41:25 +0100

    Update SpatialGridLayer.ts

--- src/gameobjects/spatialgridlayer/SpatialGridLayer.ts
+++ src/gameobjects/spatialgridlayer/SpatialGridLayer.ts
@@ -1,8 +1,13 @@
 import { GameObject } from '../GameObject';
+import { GameObjectCache } from '../GameObjectCache';
+import { HasDirtyTransform } from '../../components/dirty/HasDirtyTransform';
 import { IGameObject } from '../IGameObject';
+import { IRenderPass } from '../../renderer/webgl1/renderpass/IRenderPass';
+import { SetCustomDisplayList } from '../../components/permissions/SetCustomDisplayList';
 import { SetWillCacheChildren } from '../../components/permissions/SetWillCacheChildren';
 import { SetWillTransformChildren } from '../../components/permissions/SetWillTransformChildren';
 import { SpatialHashGrid } from '../../math/spatialgrid/SpatialHashGrid';
+import { UpdateTransforms } from '../../components/transform/UpdateTransforms';
 
 export class SpatialGridLayer extends GameObject
 {
@@ -19,11 +24,40 @@ export class SpatialGridLayer extends GameObject
         const id = this.id;
 
         SetWillTransformChildren(id, false);
-        SetWillCacheChildren(id, false);
+        SetWillCacheChildren(id, true);
+        SetCustomDisplayList(id, true);
+    }
+
+    getChildren <T extends IRenderPass> (renderPass?: T): IGameObject[]
+    {
+        const camera = renderPass.current2DCamera;
+
+        const cx = camera.getBoundsX();
+        const cy = camera.getBoundsY();
+        const cright = camera.getBoundsRight();
+        const cbottom = camera.getBoundsBottom();
+
+        const childIDs = this.hash.intersects(cx, cy, cright, cbottom);
+
+        const result: IGameObject[] = [];
+
+        childIDs.forEach(id =>
+        {
+            result.push(GameObjectCache.get(id));
+        });
+
+        return result;
     }
 
     onAddChild (child: IGameObject): void
     {
+        if (HasDirtyTransform(child.id))
+        {
+            const max = Number.MAX_SAFE_INTEGER;
+
+            UpdateTransforms(child.id, 0, 0, max, max, true);
+        }
+
         this.hash.add(child.id);
     }
 
