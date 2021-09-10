e6ac6d0d514fa6e816380ba95e88b03bc834f7c7
Richard Davey <rich@photonstorm.com>
2021-09-10 15:36:53 +0100

    No index

--- src/components/hierarchy/DebugHierarchyComponent.ts
+++ src/components/hierarchy/DebugHierarchyComponent.ts
@@ -4,14 +4,13 @@ export function DebugHierarchyComponent (id: number): void
 {
     const data = HierarchyComponent.data[id];
 
-    const index = data[HIERARCHY.INDEX];
     const parent = data[HIERARCHY.PARENT];
     const world = data[HIERARCHY.WORLD];
     const next = data[HIERARCHY.NEXT];
     const prev = data[HIERARCHY.PREV];
 
     console.group(`Entity ID: ${id}`);
-    console.log(`index: ${index} - parent: ${parent} - world: ${world}`);
+    console.log(`parent: ${parent} - world: ${world}`);
     console.log(`> next: ${next}      < prev: ${prev}`);
 
     const kids = data[HIERARCHY.NUM_CHILDREN];
