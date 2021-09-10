cdd2a083a6d404ea7fec46aa4c1025f74567000b
Richard Davey <rich@photonstorm.com>
2021-09-10 15:14:09 +0100

    Update index.ts

--- src/world/index.ts
+++ src/world/index.ts
@@ -1,7 +1,6 @@
 import * as Events from './events';
 
 import { BaseWorld } from './BaseWorld';
-import { RebuildWorldTransforms } from './RebuildWorldTransforms';
 import { StaticWorld } from './StaticWorld';
 import { World } from './World';
 import { WorldList } from './WorldList';
@@ -9,7 +8,6 @@ import { WorldList } from './WorldList';
 export {
     BaseWorld,
     Events,
-    RebuildWorldTransforms,
     StaticWorld,
     World,
     WorldList
