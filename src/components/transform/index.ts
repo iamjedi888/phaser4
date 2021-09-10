453806ef22ecef80289e8f4d4ca0767025ac6d5a
Richard Davey <rich@photonstorm.com>
2021-09-10 15:14:06 +0100

    Update index.ts

--- src/components/transform/index.ts
+++ src/components/transform/index.ts
@@ -1,35 +1,71 @@
 import { AddTransform2DComponent } from './AddTransform2DComponent';
-import { GetVertices } from './GetVertices';
-import { GetVerticesFromValues } from './GetVerticesFromValues';
+import { BoundsIntersects } from './BoundsIntersects';
+import { CopyLocalToWorld } from './CopyLocalToWorld';
+import { CopyWorldToWorld } from './CopyWorldToWorld';
+import { GetLocalBounds } from './GetLocalBounds';
+import { GetLocalTransform } from './GetLocalTransform';
+import { GetRotation } from './GetRotation';
+import { GetScaleX } from './GetScaleX';
+import { GetScaleY } from './GetScaleY';
+import { GetSkewX } from './GetSkewX';
+import { GetSkewY } from './GetSkewY';
+import { GetWorldTransform } from './GetWorldTransform';
+import { GetX } from './GetX';
+import { GetY } from './GetY';
 import { GlobalToLocal } from './GlobalToLocal';
+import { IsInView } from './IsInView';
+import { IsRootTransform } from './IsRootTransform';
 import { LocalToGlobal } from './LocalToGlobal';
+import { MultiplyLocalWithWorld } from './MultiplyLocalWithWorld';
 import { Origin } from './Origin';
 import { Position } from './Position';
 import { Scale } from './Scale';
+import { SetBounds } from './SetBounds';
 import { SetExtent } from './SetExtent';
+import { SetInView } from './SetInView';
+import { SetInViewFromBounds } from './SetInViewFromBounds';
+import { SetRootTransform } from './SetRootTransform';
 import { Size } from './Size';
 import { Skew } from './Skew';
-import { SyncTransform } from './SyncTransform';
 import { Transform2DComponent } from './Transform2DComponent';
+import { UpdateAxisAligned } from './UpdateAxisAligned';
 import { UpdateExtent } from './UpdateExtent';
-import { UpdateLocalTransform } from './UpdateLocalTransform';
-import { UpdateWorldTransform } from './UpdateWorldTransform';
+import { UpdateTransforms } from './UpdateTransforms';
+import { UpdateWorldTransformSingle } from './UpdateWorldTransformSingle';
 
 export {
     AddTransform2DComponent,
-    GetVertices,
-    GetVerticesFromValues,
+    BoundsIntersects,
+    CopyLocalToWorld,
+    CopyWorldToWorld,
+    GetLocalBounds,
+    GetLocalTransform,
+    GetRotation,
+    GetScaleX,
+    GetScaleY,
+    GetSkewX,
+    GetSkewY,
+    GetWorldTransform,
+    GetX,
+    GetY,
     GlobalToLocal,
+    IsInView,
+    IsRootTransform,
     LocalToGlobal,
+    MultiplyLocalWithWorld,
     Origin,
     Position,
     Scale,
+    SetBounds,
     SetExtent,
+    SetInView,
+    SetInViewFromBounds,
+    SetRootTransform,
     Size,
     Skew,
-    SyncTransform,
     Transform2DComponent,
+    UpdateAxisAligned,
     UpdateExtent,
-    UpdateLocalTransform,
-    UpdateWorldTransform
+    UpdateTransforms,
+    UpdateWorldTransformSingle
 };
