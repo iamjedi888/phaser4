7392068340b9738cd8f8ef9e2a4672d6efd8961e
Richard Davey <rich@photonstorm.com>
2021-09-10 16:16:25 +0100

    Expose all the new functions

--- src/world/index.ts
+++ src/world/index.ts
@@ -1,14 +1,24 @@
 import * as Events from './events';
 
 import { BaseWorld } from './BaseWorld';
+import { CreateWorldRenderData } from './CreateWorldRenderData';
+import { ProcessNode } from './ProcessNode';
+import { RenderNode } from './RenderNode';
+import { ResetWorldRenderData } from './ResetWorldRenderData';
 import { StaticWorld } from './StaticWorld';
+import { UpdateNode } from './UpdateNode';
 import { World } from './World';
 import { WorldList } from './WorldList';
 
 export {
     BaseWorld,
+    CreateWorldRenderData,
     Events,
+    ProcessNode,
+    RenderNode,
+    ResetWorldRenderData,
     StaticWorld,
+    UpdateNode,
     World,
     WorldList
 };
