94d2d0ed8cfe9ba96f45f6e3207b8b91383ec551
Richard Davey <rich@photonstorm.com>
2021-09-10 15:14:00 +0100

    Use ITransform2D interface, not Matrix class

--- src/components/transform/GlobalToLocal.ts
+++ src/components/transform/GlobalToLocal.ts
@@ -1,8 +1,8 @@
+import { ITransform2D } from './ITransform2D';
 import { IVec2Like } from '../../math/vec2/IVec2Like';
-import { Matrix2D } from '../../math/mat2d/Matrix2D';
 import { Vec2 } from '../../math/vec2/Vec2';
 
-export function GlobalToLocal (worldTransform: Matrix2D, x: number, y: number, out: IVec2Like = new Vec2()): IVec2Like
+export function GlobalToLocal (worldTransform: ITransform2D, x: number, y: number, out: IVec2Like = new Vec2()): IVec2Like
 {
     const { a, b, c, d, tx, ty } = worldTransform;
 
