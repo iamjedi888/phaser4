5c5c780a5b36d42da712e5847939727c1fad3d77
Richard Davey <rich@photonstorm.com>
2021-09-10 15:13:09 +0100

    !!

--- src/components/transform/IsInView.ts
+++ src/components/transform/IsInView.ts
@@ -2,5 +2,5 @@ import { TRANSFORM, Transform2DComponent } from './Transform2DComponent';
 
 export function IsInView (id: number): boolean
 {
-    return Boolean(Transform2DComponent.data[id][TRANSFORM.IN_VIEW]);
+    return !!(Transform2DComponent.data[id][TRANSFORM.IN_VIEW]);
 }
--- src/components/transform/IsRootTransform.ts
+++ src/components/transform/IsRootTransform.ts
@@ -2,5 +2,5 @@ import { TRANSFORM, Transform2DComponent } from './Transform2DComponent';
 
 export function IsRootTransform (id: number): boolean
 {
-    return Boolean(Transform2DComponent.data[id][TRANSFORM.IS_ROOT]);
+    return !!(Transform2DComponent.data[id][TRANSFORM.IS_ROOT]);
 }
