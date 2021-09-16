76469c9b40e5f80bee850415d6cd3b357baf75c1
Richard Davey <rich@photonstorm.com>
2021-09-16 12:10:17 +0100

    Remove flipY parameter

--- src/components/vertices/AddQuadVertex.ts
+++ src/components/vertices/AddQuadVertex.ts
@@ -5,21 +5,13 @@ import { SetQuadPosition } from './SetQuadPosition';
 import { SetUV } from './SetUV';
 import { addComponent } from 'bitecs';
 
-export function AddQuadVertex (id: number, width: number = 0, height: number = 0, flipY: boolean = false): void
+export function AddQuadVertex (id: number, width: number = 0, height: number = 0): void
 {
     addComponent(GameObjectWorld, QuadVertexComponent, id);
 
     if (width || height)
     {
-        if (flipY)
-        {
-            SetUV(id, 0, 1, 1, 0);
-        }
-        else
-        {
-            SetUV(id, 0, 0, 1, 1);
-        }
-
+        SetUV(id, 0, 0, 1, 1);
         SetQuadColor(id, 1, 1, 1, 1);
         SetQuadPosition(id, 0, 0, 0, height, width, height, width, 0);
     }
