3a816047d111e7ae5ca31fe2a2308e31ce0d019a
Richard Davey <rich@photonstorm.com>
2021-09-16 18:00:25 +0100

    Accept x and y params

--- src/components/vertices/SetInversedQuadFromCamera.ts
+++ src/components/vertices/SetInversedQuadFromCamera.ts
@@ -1,16 +1,16 @@
 import { IBaseCamera } from '../../camera/IBaseCamera';
 import { SetQuadPosition } from './SetQuadPosition';
 
-export function SetInversedQuadFromCamera <T extends IBaseCamera> (id: number, camera: T, width: number, height: number): void
+export function SetInversedQuadFromCamera <T extends IBaseCamera> (id: number, camera: T, x: number, y: number, width: number, height: number): void
 {
-    const x = camera.getBoundsX();
-    const y = camera.getBoundsY();
+    const cx = camera.getBoundsX() + x;
+    const cy = camera.getBoundsY() + y;
 
     //  Adjust for the camera position (still inverted scale Y)
     SetQuadPosition(id,
-        x, y + height,
-        x, y,
-        x + width, y,
-        x + width, y + height
+        cx, cy + height,
+        cx, cy,
+        cx + width, cy,
+        cx + width, cy + height
     );
 }
