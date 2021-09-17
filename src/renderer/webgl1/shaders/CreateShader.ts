2a071bec22b02a749be487bc8f8bd38465a5702a
Richard Davey <rich@photonstorm.com>
2021-09-17 13:43:33 +0100

    CreateShader is now CompileShader and this function actually creates the Shader object

--- src/renderer/webgl1/shaders/CreateShader.ts
+++ src/renderer/webgl1/shaders/CreateShader.ts
@@ -1,31 +1,51 @@
+import { CompileShader } from './CompileShader';
+import { CreateAttributes } from './CreateAttributes';
+import { CreateProgram } from './CreateProgram';
+import { CreateUniforms } from './CreateUniforms';
+import { IShader } from './IShader';
 import { gl } from '../GL';
 
-export function CreateShader (source: string, type: GLenum): WebGLShader | null
+export function CreateShader <T extends IShader> (shader: T, fragmentShaderSource: string, vertexShaderSource: string, uniforms: {}, attribs: {}): T
 {
-    const shader = gl.createShader(type);
+    const fragmentShader = CompileShader(fragmentShaderSource, gl.FRAGMENT_SHADER);
+    const vertexShader = CompileShader(vertexShaderSource, gl.VERTEX_SHADER);
 
-    gl.shaderSource(shader, source);
-
-    gl.compileShader(shader);
-
-    const status = gl.getShaderParameter(shader, gl.COMPILE_STATUS);
-
-    if (!status)
+    if (!fragmentShader || !vertexShader)
     {
-        const info = gl.getShaderInfoLog(shader);
-
-        //  Adding line numbers idea borrowed from Gregg Tavares!
-        const sourceLines = source.split('\n').map((line, index) =>
-        {
-            return `${index}: ${line}`;
-        });
-
-        console.error(`Error compiling shader: ${info}`, sourceLines.join('\n'));
-
-        gl.deleteShader(shader);
-
-        return null;
+        return;
     }
+
+    const program = CreateProgram(fragmentShader, vertexShader);
+
+    if (!program)
+    {
+        return;
+    }
+
+    const currentProgram = gl.getParameter(gl.CURRENT_PROGRAM);
+
+    gl.useProgram(program);
+
+    shader.program = program;
+
+    shader.uniformSetters = CreateUniforms(program);
+
+    shader.uniforms = new Map();
+
+    //  Copy starting values from the config object to the uniforms map but only if a setter exists
+    for (const [ key, value ] of Object.entries(uniforms))
+    {
+        if (shader.uniformSetters.has(key))
+        {
+            shader.uniforms.set(key, value);
+        }
+    }
+
+    shader.attributes = CreateAttributes(program, attribs);
+
+    gl.useProgram(currentProgram);
+
+    shader.isActive = false;
 
     return shader;
 }
