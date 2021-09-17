ee99cbed9dd08a44a538d9bc0cfe8cdffb2de931
Richard Davey <rich@photonstorm.com>
2021-09-17 18:02:14 +0100

    Removed create function

--- src/renderer/webgl1/shaders/MultiTextureQuadShader.ts
+++ src/renderer/webgl1/shaders/MultiTextureQuadShader.ts
@@ -1,4 +1,4 @@
-import { GetMaxTextures } from '../../../config/maxtextures/GetMaxTextures';
+import { BindShader } from './BindShader';
 import { IRenderPass } from '../renderpass/IRenderPass';
 import { IShaderConfig } from './IShaderConfig';
 import { MULTI_QUAD_FRAG } from '../glsl/MULTI_QUAD_FRAG';
@@ -13,19 +13,10 @@ export class MultiTextureQuadShader extends Shader
         super(config);
     }
 
-    create (fragmentShaderSource: string, vertexShaderSource: string, uniforms: {}, attribs: {}): void
-    {
-        const maxTextures = GetMaxTextures();
-
-        fragmentShaderSource = fragmentShaderSource.replace(/%count%/gi, `${maxTextures}`);
-
-        super.create(fragmentShaderSource, vertexShaderSource, uniforms, attribs);
-    }
-
     bind (renderPass: IRenderPass): boolean
     {
         this.uniforms.set('uTexture', renderPass.textures.textureIndex);
 
-        return super.bind(renderPass);
+        return BindShader(this, renderPass);
     }
 }
