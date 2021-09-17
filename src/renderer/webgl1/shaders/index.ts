8975309a13103c2c565e0c39b9ca488693bc6346
Richard Davey <rich@photonstorm.com>
2021-09-17 13:43:15 +0100

    Expose new functions

--- src/renderer/webgl1/shaders/index.ts
+++ src/renderer/webgl1/shaders/index.ts
@@ -1,3 +1,5 @@
+import { BindShader } from './BindShader';
+import { CompileShader } from './CompileShader';
 import { CreateAttributes } from './CreateAttributes';
 import { CreateProgram } from './CreateProgram';
 import { CreateShader } from './CreateShader';
@@ -6,20 +8,30 @@ import { CreateUniforms } from './CreateUniforms';
 import { DefaultQuadAttributes } from './DefaultQuadAttributes';
 import { DefaultQuadUniforms } from './DefaultQuadUniforms';
 import { DeleteShaders } from './DeleteShaders';
+import { DestroyShader } from './DestroyShader';
 import { FXShader } from './FXShader';
 import { MultiTextureQuadShader } from './MultiTextureQuadShader';
+import { SetAttributes } from './SetAttributes';
+import { SetShaderFromConfig } from './SetShaderFromConfig';
+import { SetUniform } from './SetUniform';
 import { Shader } from './Shader';
 
 export {
+    BindShader,
+    CompileShader,
     CreateAttributes,
     CreateProgram,
     CreateShader,
-    CreateUniforms,
     CreateUniformSetter,
+    CreateUniforms,
     DefaultQuadAttributes,
     DefaultQuadUniforms,
     DeleteShaders,
+    DestroyShader,
     FXShader,
     MultiTextureQuadShader,
+    SetAttributes,
+    SetShaderFromConfig,
+    SetUniform,
     Shader
 };
