8c6fdd1b8ff634ce3c83bf6aed24e63720c9a024
Richard Davey <rich@photonstorm.com>
2021-09-17 18:02:03 +0100

    Added viewport property

--- src/renderer/webgl1/shaders/IShader.ts
+++ src/renderer/webgl1/shaders/IShader.ts
@@ -1,3 +1,4 @@
+import { IRectangle } from '../../../geom/rectangle/IRectangle';
 import { IRenderPass } from '../renderpass/IRenderPass';
 import { IVertexAttribPointer } from './IVertexAttribPointer';
 import { Texture } from '../../../textures/Texture';
@@ -6,13 +7,16 @@ export interface IShader
 {
     attributes: Map<string, IVertexAttribPointer>;
     framebuffer: WebGLFramebuffer;
-    program: WebGLProgram;
-    renderToFramebuffer: boolean;
-    renderToDepthbuffer: boolean;
     isActive: boolean;
+    program: WebGLProgram;
+    renderToDepthbuffer: boolean;
+    renderToFramebuffer: boolean;
     texture: Texture;
     uniforms: Map<string, unknown>;
     uniformSetters: Map<string, Function>;
+    viewport?: IRectangle;
+
     bind (renderPass: IRenderPass, uTexture?: number): boolean;
+
     updateUniforms (renderPass: IRenderPass): void;
 }
