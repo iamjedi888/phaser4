2c21c1a5b82f7c30b039dfe2114c92a3e9f377f3
Richard Davey <rich@photonstorm.com>
2021-09-20 16:32:20 +0100

    Update VertexBufferStack.ts

--- src/renderer/webgl1/renderpass/VertexBufferStack.ts
+++ src/renderer/webgl1/renderpass/VertexBufferStack.ts
@@ -1,8 +1,7 @@
 import { IRenderPass } from './IRenderPass';
 import { IVertexBuffer } from '../buffers/IVertexBuffer';
-import { gl } from '../GL';
 
-export class VertexBufferStack
+export type IVertexBufferStack =
 {
     renderPass: IRenderPass;
 
@@ -10,89 +9,19 @@ export class VertexBufferStack
     active: IVertexBuffer;
     default: IVertexBuffer;
     index: number;
+    init: <T extends IRenderPass> (renderPass: T) => void;
+};
 
-    constructor (renderPass: IRenderPass)
+export const VertexBufferStack: IVertexBufferStack =
+{
+    renderPass: null,
+    stack: [],
+    active: null,
+    default: null,
+    index: 0,
+
+    init: <T extends IRenderPass> (renderPass: T): void =>
     {
-        this.renderPass = renderPass;
-        this.stack = [];
+        VertexBufferStack.renderPass = renderPass;
     }
-
-    get current (): IVertexBuffer
-    {
-        return this.stack[this.index];
-    }
-
-    add (buffer: IVertexBuffer): IVertexBuffer
-    {
-        this.index++;
-
-        //  cursor already at the end of the stack, so we need to grow it
-        if (this.index === this.stack.length)
-        {
-            this.stack.push(buffer);
-        }
-        else
-        {
-            this.stack[this.index] = buffer;
-        }
-
-        return buffer;
-    }
-
-    bindDefault (): void
-    {
-        this.index = 0;
-
-        this.bind(this.default);
-    }
-
-    bind (buffer?: IVertexBuffer): void
-    {
-        if (!buffer)
-        {
-            buffer = this.current;
-        }
-
-        if (!buffer.isBound)
-        {
-            const indexBuffer = (buffer.indexed) ? buffer.indexBuffer : null;
-
-            gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
-
-            gl.bindBuffer(gl.ARRAY_BUFFER, buffer.vertexBuffer);
-
-            buffer.isBound = true;
-
-            if (this.active && this.active !== buffer)
-            {
-                this.active.isBound = false;
-            }
-
-            this.active = buffer;
-        }
-    }
-
-    pop (): void
-    {
-        this.index--;
-
-        this.bind();
-    }
-
-    set (buffer: IVertexBuffer): void
-    {
-        const entry = this.add(buffer);
-
-        this.bind(entry);
-    }
-
-    setDefault (buffer: IVertexBuffer): void
-    {
-        //  The default entry always goes into index zero
-        this.stack[0] = buffer;
-
-        this.index = 0;
-
-        this.default = buffer;
-    }
-}
+};
