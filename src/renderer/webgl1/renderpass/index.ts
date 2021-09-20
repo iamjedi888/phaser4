3d6ac52dac89b88b108da9adba78ce376d0317bb
Richard Davey <rich@photonstorm.com>
2021-09-20 16:19:49 +0100

    Expose funcs

--- src/renderer/webgl1/renderpass/index.ts
+++ src/renderer/webgl1/renderpass/index.ts
@@ -1,7 +1,12 @@
+import { AddBlendMode } from './AddBlendMode';
 import { AddFramebuffer } from './AddFramebuffer';
 import { Begin } from './Begin';
+import { BindBlendMode } from './BindBlendMode';
+import { BindDefaultBlendMode } from './BindDefaultBlendMode';
 import { BindDefaultFramebuffer } from './BindDefaultFramebuffer';
 import { BindFramebuffer } from './BindFramebuffer';
+import { BlendModeStack } from './BlendModeStack';
+import { CurrentBlendMode } from './CurrentBlendMode';
 import { CurrentFramebuffer } from './CurrentFramebuffer';
 import { Draw } from './Draw';
 import { End } from './End';
@@ -9,25 +14,34 @@ import { Flush } from './Flush';
 import { FlushBuffer } from './FlushBuffer';
 import { FramebufferStack } from './FramebufferStack';
 import { GetVertexBufferEntry } from './GetVertexBufferEntry';
+import { PopBlendMode } from './PopBlendMode';
 import { PopFramebuffer } from './PopFramebuffer';
 import { ProcessBindingQueue } from './ProcessBindingQueue';
 import { RenderPass } from './RenderPass';
+import { SetDefaultBlendMode } from './SetDefaultBlendMode';
 import { SetDefaultFramebuffer } from './SetDefaultFramebuffer';
 import { SetFramebuffer } from './SetFramebuffer';
 import { Start } from './Start';
 
 export {
+    AddBlendMode,
     AddFramebuffer,
     Begin,
+    BindBlendMode,
+    BindDefaultBlendMode,
     BindDefaultFramebuffer,
     BindFramebuffer,
+    BlendModeStack,
+    CurrentBlendMode,
     CurrentFramebuffer,
     Draw,
     End,
     Flush,
     FlushBuffer,
     FramebufferStack,
+    PopBlendMode,
     PopFramebuffer,
+    SetDefaultBlendMode,
     SetDefaultFramebuffer,
     SetFramebuffer,
     GetVertexBufferEntry,
