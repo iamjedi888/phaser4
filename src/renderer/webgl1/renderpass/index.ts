42d25b3cab15534a49c47d9a19c9bc983b552555
Richard Davey <rich@photonstorm.com>
2021-09-20 18:14:08 +0100

    Expose all funcs

--- src/renderer/webgl1/renderpass/index.ts
+++ src/renderer/webgl1/renderpass/index.ts
@@ -1,6 +1,7 @@
 import { AddBlendMode } from './AddBlendMode';
 import { AddColorMatrix } from './AddColorMatrix';
 import { AddFramebuffer } from './AddFramebuffer';
+import { AddShader } from './AddShader';
 import { AddVertexBuffer } from './AddVertexBuffer';
 import { AddViewport } from './AddViewport';
 import { Begin } from './Begin';
@@ -9,6 +10,7 @@ import { BindColorMatrix } from './BindColorMatrix';
 import { BindDefaultBlendMode } from './BindDefaultBlendMode';
 import { BindDefaultColorMatrix } from './BindDefaultColorMatrix';
 import { BindDefaultFramebuffer } from './BindDefaultFramebuffer';
+import { BindDefaultShader } from './BindDefaultShader';
 import { BindDefaultVertexBuffer } from './BindDefaultVertexBuffer';
 import { BindDefaultViewport } from './BindDefaultViewport';
 import { BindFramebuffer } from './BindFramebuffer';
@@ -17,11 +19,15 @@ import { BindTexture } from './BindTexture';
 import { BindVertexBuffer } from './BindVertexBuffer';
 import { BindViewport } from './BindViewport';
 import { BlendModeStack } from './BlendModeStack';
+import { BlendModeStackEntry } from './BlendModeStackEntry';
 import { ClearTextures } from './ClearTextures';
 import { ColorMatrixStack } from './ColorMatrixStack';
+import { ColorMatrixStackEntry } from './ColorMatrixStackEntry';
+import { CreateTempTextures } from './CreateTempTextures';
 import { CurrentBlendMode } from './CurrentBlendMode';
 import { CurrentColorMatrix } from './CurrentColorMatrix';
 import { CurrentFramebuffer } from './CurrentFramebuffer';
+import { CurrentShader } from './CurrentShader';
 import { CurrentVertexBuffer } from './CurrentVertexBuffer';
 import { CurrentViewport } from './CurrentViewport';
 import { Draw } from './Draw';
@@ -29,21 +35,41 @@ import { End } from './End';
 import { Flush } from './Flush';
 import { FlushBuffer } from './FlushBuffer';
 import { FramebufferStack } from './FramebufferStack';
+import { FramebufferStackEntry } from './FramebufferStackEntry';
 import { GetVertexBufferEntry } from './GetVertexBufferEntry';
 import { PopBlendMode } from './PopBlendMode';
+import { PopColor } from './PopColor';
 import { PopColorMatrix } from './PopColorMatrix';
 import { PopFramebuffer } from './PopFramebuffer';
+import { PopShader } from './PopShader';
 import { PopVertexBuffer } from './PopVertexBuffer';
 import { PopViewport } from './PopViewport';
 import { ProcessBindingQueue } from './ProcessBindingQueue';
 import { RenderPass } from './RenderPass';
+import { ResetTextures } from './ResetTextures';
+import { SetBlendMode } from './SetBlendMode';
+import { SetCamera } from './SetCamera';
+import { SetColor } from './SetColor';
+import { SetColorMatrix } from './SetColorMatrix';
 import { SetDefaultBlendMode } from './SetDefaultBlendMode';
 import { SetDefaultColorMatrix } from './SetDefaultColorMatrix';
 import { SetDefaultFramebuffer } from './SetDefaultFramebuffer';
+import { SetDefaultShader } from './SetDefaultShader';
+import { SetDefaultTextures } from './SetDefaultTextures';
 import { SetDefaultVertexBuffer } from './SetDefaultVertexBuffer';
 import { SetDefaultViewport } from './SetDefaultViewport';
 import { SetFramebuffer } from './SetFramebuffer';
+import { SetShader } from './SetShader';
+import { SetTexture } from './SetTexture';
+import { SetVertexBuffer } from './SetVertexBuffer';
+import { SetViewport } from './SetViewport';
+import { SetWhiteTexture } from './SetWhiteTexture';
+import { ShaderStack } from './ShaderStack';
+import { ShaderStackEntry } from './ShaderStackEntry';
 import { Start } from './Start';
+import { TextureStack } from './TextureStack';
+import { UnbindTexture } from './UnbindTexture';
+import { UnbindTextureFromSlot } from './UnbindTextureFromSlot';
 import { VertexBufferStack } from './VertexBufferStack';
 import { ViewportStack } from './ViewportStack';
 
@@ -51,6 +77,7 @@ export {
     AddBlendMode,
     AddColorMatrix,
     AddFramebuffer,
+    AddShader,
     AddVertexBuffer,
     AddViewport,
     Begin,
@@ -59,6 +86,7 @@ export {
     BindDefaultBlendMode,
     BindDefaultColorMatrix,
     BindDefaultFramebuffer,
+    BindDefaultShader,
     BindDefaultVertexBuffer,
     BindDefaultViewport,
     BindFramebuffer,
@@ -67,11 +95,15 @@ export {
     BindVertexBuffer,
     BindViewport,
     BlendModeStack,
+    BlendModeStackEntry,
     ClearTextures,
     ColorMatrixStack,
-    CurrentColorMatrix,
+    ColorMatrixStackEntry,
+    CreateTempTextures,
     CurrentBlendMode,
+    CurrentColorMatrix,
     CurrentFramebuffer,
+    CurrentShader,
     CurrentVertexBuffer,
     CurrentViewport,
     Draw,
@@ -79,21 +111,41 @@ export {
     Flush,
     FlushBuffer,
     FramebufferStack,
+    FramebufferStackEntry,
     GetVertexBufferEntry,
     PopBlendMode,
+    PopColor,
     PopColorMatrix,
     PopFramebuffer,
+    PopShader,
     PopVertexBuffer,
     PopViewport,
     ProcessBindingQueue,
     RenderPass,
+    ResetTextures,
+    SetBlendMode,
+    SetCamera,
+    SetColor,
+    SetColorMatrix,
     SetDefaultBlendMode,
     SetDefaultColorMatrix,
     SetDefaultFramebuffer,
+    SetDefaultShader,
+    SetDefaultTextures,
     SetDefaultVertexBuffer,
     SetDefaultViewport,
     SetFramebuffer,
+    SetShader,
+    SetTexture,
+    SetVertexBuffer,
+    SetViewport,
+    SetWhiteTexture,
+    ShaderStack,
+    ShaderStackEntry,
     Start,
+    TextureStack,
+    UnbindTexture,
+    UnbindTextureFromSlot,
     VertexBufferStack,
     ViewportStack
 };
