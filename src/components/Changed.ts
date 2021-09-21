7ceec5c2a7237a6f9f8eb905db203ea0de74937d
Richard Davey <rich@photonstorm.com>
2021-09-21 17:32:38 +0100

    Removing bitecs proxies

--- src/components/Changed.ts
+++
@@ -1,6 +0,0 @@
-import { Changed as BITECSChanged, ComponentType, IComponent, ISchema, QueryModifier } from 'bitecs';
-
-export function Changed (c: ComponentType<ISchema>): IComponent | QueryModifier
-{
-    return BITECSChanged(c);
-}
