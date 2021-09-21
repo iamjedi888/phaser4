6063e512fccc670856fc8de58cde7a1cd3afae31
Richard Davey <rich@photonstorm.com>
2021-09-21 17:33:53 +0100

    Removed 3D from index

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,58 @@
 import * as Animation from './animation/';
 import * as Camera from './camera/';
-import * as Camera3D from './camera3d/';
 import * as Color from './color/';
 import * as Config from './config/';
 import * as DOM from './dom/';
 import * as Device from './device/';
 import * as Display from './display/';
-import * as Display3D from './display3d/';
 import * as Events from './events/';
 import * as GameObjects from './gameobjects/';
-import * as GameObjects3D from './gameobjects3d/';
 import * as Geom from './geom/';
-import * as Geom3D from './geom3d/';
 import * as Input from './input/';
 import * as Loader from './loader/';
-import * as Materials3D from './materials3d/';
 import * as Math from './math/';
 import * as Scenes from './scenes/';
 import * as Textures from './textures/';
 import * as Time from './time/';
 import * as WebGL1 from './renderer/webgl1/';
 import * as World from './world/';
-import * as World3D from './world3d/';
 
 import { CreateGame } from './CreateGame';
 import { Game } from './Game';
 import { Scene } from './scenes/Scene';
 
+// import * as Camera3D from './camera3d/';
+// import * as Display3D from './display3d/';
+// import * as GameObjects3D from './gameobjects3d/';
+// import * as Geom3D from './geom3d/';
+// import * as Materials3D from './materials3d/';
+// import * as World3D from './world3d/';
+
+export {
+    Animation,
+    Camera,
+    Color,
+    Config,
+    Device,
+    DOM,
+    Display,
+    Events,
+    CreateGame,
+    Game,
+    Scene,
+    GameObjects,
+    Geom,
+    Input,
+    Loader,
+    Math,
+    Scenes,
+    Textures,
+    Time,
+    WebGL1,
+    World
+};
+
+/*
 export {
     Animation,
     Camera,
@@ -56,3 +82,4 @@ export {
     World,
     World3D
 };
+*/
