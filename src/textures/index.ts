5a8bf20a5b2b7b2e5774b499b2c65a3a438ec948
Richard Davey <rich@photonstorm.com>
2021-09-23 18:22:41 +0100

    Expose new functions

--- src/textures/index.ts
+++ src/textures/index.ts
@@ -5,28 +5,58 @@ import * as Types from './types';
 import { AlphaTexture } from './AlphaTexture';
 import { CreateCanvas } from './CreateCanvas';
 import { CreateTextureManager } from './CreateTextureManager';
+import { FlipFrameUVs } from './FlipFrameUVs';
 import { Frame } from './Frame';
+import { GetExtentFromFrame } from './GetExtentFromFrame';
 import { GetFrames } from './GetFrames';
 import { GetFramesInRange } from './GetFramesInRange';
+import { GetSpritesWithTexture } from './GetSpritesWithTexture';
 import { GetTexture } from './GetTexture';
+import { RemoveTexture } from './RemoveTexture';
+import { RemoveTextureFromGameObject } from './RemoveTextureFromGameObject';
+import { ReturnTexture } from './ReturnTexture';
+import { SetExtentFromFrame } from './SetExtentFromFrame';
 import { SetFilter } from './SetFilter';
+import { SetFrame } from './SetFrame';
+import { SetFramePivot } from './SetFramePivot';
+import { SetFrameSize } from './SetFrameSize';
+import { SetFrameSourceSize } from './SetFrameSourceSize';
+import { SetFrameTrim } from './SetFrameTrim';
+import { SetTexture } from './SetTexture';
+import { SetVertexUVsFromFrame } from './SetVertexUVsFromFrame';
 import { Texture } from './Texture';
 import { TextureManager } from './TextureManager';
+import { UpdateFrameUVs } from './UpdateFrameUVs';
 import { WhiteTexture } from './WhiteTexture';
 
 export {
     AlphaTexture,
     CreateCanvas,
     CreateTextureManager,
+    FlipFrameUVs,
     Frame,
+    GetExtentFromFrame,
     GetFrames,
     GetFramesInRange,
+    GetSpritesWithTexture,
     GetTexture,
-    SetFilter,
     Palettes,
     Parsers,
-    Types,
+    RemoveTexture,
+    RemoveTextureFromGameObject,
+    ReturnTexture,
+    SetExtentFromFrame,
+    SetFilter,
+    SetFrame,
+    SetFramePivot,
+    SetFrameSize,
+    SetFrameSourceSize,
+    SetFrameTrim,
+    SetTexture,
+    SetVertexUVsFromFrame,
     Texture,
     TextureManager,
+    Types,
+    UpdateFrameUVs,
     WhiteTexture
 };
