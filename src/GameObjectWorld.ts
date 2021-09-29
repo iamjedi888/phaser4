c26820f2e8f5776cd1b6403c27377466b16c983d
Richard Davey <rich@photonstorm.com>
2021-09-29 12:17:00 +0100

    bitecs can now resize itself

--- src/GameObjectWorld.ts
+++ src/GameObjectWorld.ts
@@ -1,20 +1,4 @@
-import { createWorld, setDefaultSize } from 'bitecs';
-
-// setDefaultSize(10000);
-// setDefaultSize(100000);
-// setDefaultSize(150000);
-// setDefaultSize(200000);
-// setDefaultSize(250000);
-// setDefaultSize(300000);
-// setDefaultSize(350000);
-// setDefaultSize(400000);
-// setDefaultSize(500000);
-// setDefaultSize(550000);
-// setDefaultSize(1000000);
-// setDefaultSize(1500000);
-// setDefaultSize(2000000);
-// setDefaultSize(2500000);
-// setDefaultSize(4000000);
+import { createWorld } from 'bitecs';
 
 const world = createWorld();
 
