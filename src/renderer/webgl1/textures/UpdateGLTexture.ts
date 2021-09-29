8b475aec8855fdbe72016f665513cb50c632e3ae
Richard Davey <rich@photonstorm.com>
2021-09-29 17:47:25 +0100

    Unbind when done

--- src/renderer/webgl1/textures/UpdateGLTexture.ts
+++ src/renderer/webgl1/textures/UpdateGLTexture.ts
@@ -1,19 +1,23 @@
 import { IGLTextureBinding } from './IGLTextureBinding';
+import { SetWebGLTexture } from '../renderpass/SetWebGLTexture';
+import { UnbindTexture } from '../renderpass/UnbindTexture';
 import { gl } from '../GL';
 
 export function UpdateGLTexture <T extends IGLTextureBinding> (binding: T): WebGLTexture
 {
-    const source = binding.parent.image;
+    const parent = binding.parent;
+    const source = parent.image;
     const width = source.width;
     const height = source.height;
 
     if (width > 0 && height > 0)
     {
-        gl.activeTexture(gl.TEXTURE0);
-        gl.bindTexture(gl.TEXTURE_2D, binding.texture);
+        SetWebGLTexture(parent);
 
         gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, binding.flipY);
         gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, source);
+
+        UnbindTexture(parent);
     }
 
     return binding.texture;
