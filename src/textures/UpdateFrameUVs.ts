f0095583c169fbd37eef91cdc37fe1e8c0e3ab80
Richard Davey <rich@photonstorm.com>
2021-09-30 18:39:48 +0100

    Set the fractional width and height

--- src/textures/UpdateFrameUVs.ts
+++ src/textures/UpdateFrameUVs.ts
@@ -13,5 +13,8 @@ export function UpdateFrameUVs <T extends IFrame> (frame: T): T
     frame.u1 = (x + width) / baseTextureWidth;
     frame.v1 = (y + height) / baseTextureHeight;
 
+    frame.fWidth = width / baseTextureWidth;
+    frame.fHeight = height / baseTextureHeight;
+
     return frame;
 }
