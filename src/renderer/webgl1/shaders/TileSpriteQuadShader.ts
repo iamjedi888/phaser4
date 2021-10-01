ebf7168ed360a1c502107f3d6d58f78946088e15
Richard Davey <rich@photonstorm.com>
2021-10-01 02:03:58 +0100

    Correct uniforms

--- src/renderer/webgl1/shaders/TileSpriteQuadShader.ts
+++ src/renderer/webgl1/shaders/TileSpriteQuadShader.ts
@@ -12,11 +12,13 @@ export class TileSpriteQuadShader extends Shader
     constructor (tileSprite: ITileSprite, config: IShaderConfig = {})
     {
         config.fragmentShader = config?.fragmentShader || TILESPRITE_FRAG;
-        config.uniforms = config?.uniforms || DefaultTileSpriteUniforms;
+        // config.uniforms = config?.uniforms || DefaultTileSpriteUniforms;
 
         super(config);
 
         this.tileSprite = tileSprite;
+
+        console.log(this);
     }
 
     updateUniforms (): void
@@ -24,10 +26,14 @@ export class TileSpriteQuadShader extends Shader
         const sprite = this.tileSprite;
         const frame = sprite.frame;
 
-        this.uniforms.set('uTexture', TextureStack.textureIndex);
-        this.uniforms.set('uTextureSize', [ frame.fWidth, frame.fHeight ]);
-        this.uniforms.set('uTexturePosition', [ frame.u0, frame.v0 ]);
-        this.uniforms.set('uScale', [ sprite.tileScale.x, sprite.tileScale.y ]);
-        this.uniforms.set('uOffset', [ sprite.tilePosition.x, sprite.tilePosition.y ]);
+        this.uniforms.set('uTime', performance.now() / 1000);
+
+        this.uniforms.set('uTexture', frame.texture.binding.textureUnit);
+        this.uniforms.set('uTileSize', [ frame.fWidth, frame.fHeight ]);
+        this.uniforms.set('uTilePosition', [ frame.u0, frame.v0 ]);
+        this.uniforms.set('uTileScale', [ sprite.frameScale.x * sprite.tileScale.x, sprite.frameScale.y * sprite.tileScale.y ]);
+        this.uniforms.set('uTileOffset', [ sprite.tilePosition.x, sprite.tilePosition.y ]);
+        this.uniforms.set('uTileRotationOrigin', [ sprite.tileScale.x * sprite.tileRotationOrigin.x, sprite.tileScale.y * sprite.tileRotationOrigin.y ]);
+        this.uniforms.set('uTileAngle', sprite.tileAngle);
     }
 }
