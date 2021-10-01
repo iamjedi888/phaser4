5d0f844e48b1665ec213fcfb2e1b4bec8fc400d8
Richard Davey <rich@photonstorm.com>
2021-10-01 18:09:40 +0100

    Directly modify the typed arrays

--- src/renderer/webgl1/shaders/TileSpriteQuadShader.ts
+++ src/renderer/webgl1/shaders/TileSpriteQuadShader.ts
@@ -3,22 +3,31 @@ import { IShaderConfig } from './IShaderConfig';
 import { ITileSprite } from '../../../gameobjects/tilesprite/ITileSprite';
 import { Shader } from './Shader';
 import { TILESPRITE_FRAG } from '../glsl/TILESPRITE_FRAG';
-import { TextureStack } from '../renderpass/TextureStack';
 
 export class TileSpriteQuadShader extends Shader
 {
     tileSprite: ITileSprite;
 
+    uTileSize: Float32Array;
+    uTilePosition: Float32Array;
+    uTileScale: Float32Array;
+    uTileOffset: Float32Array;
+    uTileRotationOrigin: Float32Array;
+
     constructor (tileSprite: ITileSprite, config: IShaderConfig = {})
     {
         config.fragmentShader = config?.fragmentShader || TILESPRITE_FRAG;
-        // config.uniforms = config?.uniforms || DefaultTileSpriteUniforms;
+        config.uniforms = config?.uniforms || DefaultTileSpriteUniforms;
 
         super(config);
 
         this.tileSprite = tileSprite;
 
-        console.log(this);
+        this.uTileSize = this.uniforms.get('uTileSize') as Float32Array;
+        this.uTilePosition = this.uniforms.get('uTilePosition') as Float32Array;
+        this.uTileScale = this.uniforms.get('uTileScale') as Float32Array;
+        this.uTileOffset = this.uniforms.get('uTileOffset') as Float32Array;
+        this.uTileRotationOrigin = this.uniforms.get('uTileRotationOrigin') as Float32Array;
     }
 
     updateUniforms (): void
@@ -27,13 +36,26 @@ export class TileSpriteQuadShader extends Shader
         const frame = sprite.frame;
 
         this.uniforms.set('uTime', performance.now() / 1000);
-
         this.uniforms.set('uTexture', frame.texture.binding.textureUnit);
-        this.uniforms.set('uTileSize', [ frame.fWidth, frame.fHeight ]);
-        this.uniforms.set('uTilePosition', [ frame.u0, frame.v0 ]);
-        this.uniforms.set('uTileScale', [ sprite.frameScale.x * sprite.tileScale.x, sprite.frameScale.y * sprite.tileScale.y ]);
-        this.uniforms.set('uTileOffset', [ sprite.tilePosition.x, sprite.tilePosition.y ]);
-        this.uniforms.set('uTileRotationOrigin', [ sprite.tileScale.x * sprite.tileRotationOrigin.x, sprite.tileScale.y * sprite.tileRotationOrigin.y ]);
+
+        this.uTileSize[0] = frame.fWidth;
+        this.uTileSize[1] = frame.fWidth;
+
+        this.uTilePosition[0] = frame.u0;
+        this.uTilePosition[1] = frame.v0;
+
+        this.uTileScale[0] = sprite.frameScale.x * sprite.tileScale.x;
+        this.uTileScale[1] = sprite.frameScale.y * sprite.tileScale.y;
+
+        this.uTileOffset[0] = sprite.tilePosition.x;
+        this.uTileOffset[1] = sprite.tilePosition.y;
+
+        this.uTileRotationOrigin[0] = this.uTileScale[0] * sprite.tileRotationOrigin.x;
+        this.uTileRotationOrigin[1] = this.uTileScale[1] * sprite.tileRotationOrigin.y;
+
         this.uniforms.set('uTileAngle', sprite.tileAngle);
+        this.uniforms.set('uTileDistortion', sprite.tileDistortion);
+        this.uniforms.set('uTileSway', sprite.tileSway);
+        this.uniforms.set('uTileSpeed', sprite.tileSpeed);
     }
 }
