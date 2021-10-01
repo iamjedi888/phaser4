e469a80eedf321e901998d44a2df4b5665ceb99f
Richard Davey <rich@photonstorm.com>
2021-10-01 18:09:11 +0100

    Invoke onAddedToWorld

--- src/components/hierarchy/SetWorldID.ts
+++ src/components/hierarchy/SetWorldID.ts
@@ -1,6 +1,21 @@
 import { HIERARCHY, HierarchyComponent } from './HierarchyComponent';
 
+import { GameObjectCache } from '../../gameobjects/GameObjectCache';
+import { IBaseWorld } from '../../world/IBaseWorld';
+
 export function SetWorldID (id: number, worldID: number): void
 {
     HierarchyComponent.data[id][HIERARCHY.WORLD] = worldID;
+
+    const gameObject = GameObjectCache.get(id);
+
+    if (gameObject)
+    {
+        const world = GameObjectCache.get(worldID) as IBaseWorld;
+
+        if (world)
+        {
+            gameObject.onAddedToWorld(world);
+        }
+    }
 }
